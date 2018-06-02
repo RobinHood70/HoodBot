1c737bc0f94fe5d6edd7053c5b44d5a3455734c7
Robin Hood <robinhood70@live.ca>
2018-06-02 16:51:05 -0400

    Ignore CA warning

--- Robby/Design/IMessageSource.cs
+++ Robby/Design/IMessageSource.cs
@@ -1,6 +1,7 @@
 ﻿namespace RobinHood70.Robby
 {
 	/// <summary>Flags a class as sending warnings via the Site object's PublishWarning method.</summary>
+	[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1040:AvoidEmptyInterfaces", Justification = "Identifies a compile-time set of types.")]
 	public interface IMessageSource
 	{
 	}
