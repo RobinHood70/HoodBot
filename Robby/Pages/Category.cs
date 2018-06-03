4a3fb5f7031be2ec0da9bfda57701bd8a963b15d
Robin Hood <robinhood70@live.ca>
2018-06-03 19:41:18 -0400

    Documentation

--- Robby/Pages/Category.cs
+++ Robby/Pages/Category.cs
@@ -3,23 +3,40 @@
 	using WallE.Base;
 	using static WikiCommon.Globals;
 
+	/// <summary>In addition to regular page data, stores information about a category.</summary>
+	/// <seealso cref="RobinHood70.Robby.Pages.Page" />
 	public class Category : Page
 	{
+		/// <summary>Initializes a new instance of the <see cref="Category" /> class.</summary>
+		/// <param name="site">The site the category is from.</param>
+		/// <param name="fullPageName">Full name of the page.</param>
 		public Category(Site site, string fullPageName)
 			: base(site, fullPageName)
 		{
 		}
 
+		/// <summary>Gets the number of files in the category.</summary>
+		/// <value>The file count.</value>
 		public int FileCount { get; private set; }
 
+		/// <summary>Gets the total number of entries in the category, regardless of type.</summary>
+		/// <value>The full count.</value>
 		public int FullCount { get; private set; }
 
+		/// <summary>Gets a value indicating whether this <see cref="Category" /> is hidden.</summary>
+		/// <value><see langword="true" /> if hidden; otherwise, <see langword="false" />.</value>
 		public bool Hidden { get; private set; }
 
+		/// <summary>Gets the number of pages in the category. Files and subcategories are not included in the count.</summary>
+		/// <value>The page count.</value>
 		public int PageCount { get; private set; }
 
+		/// <summary>Gets the subcategory count.</summary>
+		/// <value>The subcategory count.</value>
 		public int SubcategoryCount { get; private set; }
 
+		/// <summary>When overridden in a derived class, populates custom page properties with custom data from the WallE PageItem.</summary>
+		/// <param name="pageItem">The page item.</param>
 		protected override void PopulateCustomResults(PageItem pageItem)
 		{
 			ThrowNull(pageItem, nameof(pageItem));
--- Robby/Pages/CategoryTitle.cs
+++ Robby/Pages/CategoryTitle.cs
@@ -1,8 +1,16 @@
 ﻿namespace RobinHood70.Robby.Pages
 {
+	/// <summary>Stores information about a category link. This includes the sort key and whether or not the category is hidden.</summary>
+	/// <seealso cref="RobinHood70.Robby.Title" />
 	public class CategoryTitle : Title
 	{
 		#region Constructors
+
+		/// <summary>Initializes a new instance of the <see cref="CategoryTitle"/> class.</summary>
+		/// <param name="site">The site the category is from.</param>
+		/// <param name="fullCategoryName">Full name of the category.</param>
+		/// <param name="sortKey">The sort key.</param>
+		/// <param name="hidden">if set to <see langword="true" /> if the category is hidden.</param>
 		internal CategoryTitle(Site site, string fullCategoryName, string sortKey, bool hidden)
 			: base(site, fullCategoryName)
 		{
@@ -12,8 +20,13 @@
 		#endregion
 
 		#region Public Properties
+
+		/// <summary>Gets a value indicating whether this <see cref="CategoryTitle"/> is hidden.</summary>
+		/// <value><see langword="true" /> if hidden; otherwise, <see langword="false" />.</value>
 		public bool Hidden { get; }
 
+		/// <summary>Gets the sort key.</summary>
+		/// <value>The sort key.</value>
 		public string SortKey { get; }
 		#endregion
 	}
