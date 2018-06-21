f9d7bcfc6e5dbe24457f2a0f4397b310fbbafb9e
Robin Hood <robinhood70@live.ca>
2018-06-20 22:35:18 -0400

    Revert DefaultPageCreator to separate file

--- Robby/Design/PageCreator.cs
+++ Robby/Design/PageCreator.cs
@@ -3,7 +3,6 @@
 	using System.Collections.Generic;
 	using Pages;
 	using WallE.Base;
-	using WikiCommon;
 	using static WikiCommon.Globals;
 
 	/// <summary>Provides a base class for creating Page objects. This serves as a go-between for customized page extensions in WallE and Robby.</summary>
@@ -123,50 +122,6 @@
 		/// <summary>Adds any custom property inputs.</summary>
 		/// <param name="propertyInputs">The property inputs.</param>
 		protected abstract void AddCustomPropertyInputs(IList<IPropertyInput> propertyInputs);
-		#endregion
-
-		#region Private Classes
-
-		/// <summary>The default page creation mechanism.</summary>
-		/// <seealso cref="RobinHood70.Robby.Design.PageCreator" />
-		public class DefaultPageCreator : PageCreator
-		{
-			#region Public Override Methods
-
-			/// <summary>Creates a page.</summary>
-			/// <param name="wikiTitle">The <see cref="IWikiTitle" /> object that represents the page to create.</param>
-			/// <returns>A fully populated Page object.</returns>
-			public override Page CreatePage(IWikiTitle wikiTitle)
-			{
-				ThrowNull(wikiTitle, nameof(wikiTitle));
-				switch (wikiTitle.Namespace.Id)
-				{
-					case MediaWikiNamespaces.MediaWiki:
-						return new Message(wikiTitle.Namespace.Site, wikiTitle.PageName);
-					case MediaWikiNamespaces.File:
-						return new FilePage(wikiTitle.Namespace.Site, wikiTitle.PageName);
-					case MediaWikiNamespaces.Category:
-						return new Category(wikiTitle.Namespace.Site, wikiTitle.PageName);
-				}
-
-				return new Page(wikiTitle);
-			}
-
-			/// <summary>Creates a page item.</summary>
-			/// <returns>A new PageItem for use by WallE.</returns>
-			public override PageItem CreatePageItem() => new PageItem();
-			#endregion
-
-			#region Protected Override Methods
-
-			/// <summary>Adds any custom property inputs.</summary>
-			/// <param name="propertyInputs">The property inputs.</param>
-			protected override void AddCustomPropertyInputs(IList<IPropertyInput> propertyInputs)
-			{
-				// The default creator requires no custom inputs, by defnition, so this is a null function.
-			}
-			#endregion
-		}
 		#endregion
 	}
 }
