d31e2d7baf2c4c81acfa92833ba986b0721eb722
Robin Hood <robinhood70@live.ca>
2018-07-14 15:31:52 -0400

    Add yield-like functionality for rare cases where WPF must be refreshed in a given order

--- HoodBot/App.xaml.cs
+++ HoodBot/App.xaml.cs
@@ -1,14 +1,19 @@
 ﻿namespace RobinHood70.HoodBot
 {
+	using System;
 	using System.Globalization;
 	using System.Threading;
 	using System.Windows;
+	using System.Windows.Threading;
 
 	/// <summary>
 	/// Interaction logic for App.xaml.
 	/// </summary>
 	public partial class App : Application
 	{
+		// Ensure everything is updated on the UI end before continuing.
+		public static void WpfYield() => Current.Dispatcher.Invoke(new Action(() => { }), DispatcherPriority.ContextIdle, null);
+
 		private void Application_Startup(object sender, StartupEventArgs e) => Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-CA");
 	}
 }
