842eed33c293c2fa77e0053205d0479d21b5e834
Robin Hood <robinhood70@live.ca>
2018-07-14 15:33:16 -0400

    Remove method that will never be needed

---
+++ HoodBot/Jobs/VoidJob.cs
@@ -0,0 +1,28 @@
+﻿namespace RobinHood70.HoodBot.Jobs
+{
+	using System.Threading;
+	using RobinHood70.HoodBot.Jobs.Design;
+	using RobinHood70.Robby;
+
+	public class VoidJob : WikiJob
+	{
+		[JobInfo("Do Nothing")]
+		public VoidJob(Site site, AsyncInfo asyncInfo)
+			: base(site, asyncInfo)
+		{
+		}
+
+		protected override void Main()
+		{
+			var progressMax = 5;
+			this.ProgressMaximum = progressMax;
+			for (var i = 1; i <= progressMax; i++)
+			{
+				this.StatusWrite($"Job {i}: Start...");
+				Thread.Sleep(199);
+				this.StatusWriteLine("End");
+				this.IncrementProgress();
+			}
+		}
+	}
+}
