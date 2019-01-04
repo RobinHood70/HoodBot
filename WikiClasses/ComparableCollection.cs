bc40e707e20956b9d0ba447c55499b28a40e1478
Robin Hood <robinhood70@live.ca>
2019-01-03 21:59:06 -0500

    Documentation

--- WikiClasses/ComparableCollection.cs
+++ WikiClasses/ComparableCollection.cs
@@ -3,6 +3,9 @@
 	using System.Collections;
 	using System.Collections.Generic;
 
+	/// <summary>A list collection that provides a Comparer.</summary>
+	/// <typeparam name="T">The type of list.</typeparam>
+	/// <seealso cref="System.Collections.Generic.IList{T}" />
 	public class ComparableCollection<T> : IList<T>
 	{
 		#region Fields
@@ -10,18 +13,27 @@
 		#endregion
 
 		#region Constructors
+
+		/// <summary>Initializes a new instance of the <see cref="ComparableCollection{T}"/> class.</summary>
+		/// <param name="comparer">The comparer.</param>
 		public ComparableCollection(IEqualityComparer<T> comparer)
 		{
 			this.list = new List<T>();
 			this.Comparer = comparer;
 		}
 
+		/// <summary>Initializes a new instance of the <see cref="ComparableCollection{T}"/> class.</summary>
+		/// <param name="capacity">The collection capacity.</param>
+		/// <param name="comparer">The comparer.</param>
 		public ComparableCollection(int capacity, IEqualityComparer<T> comparer)
 		{
 			this.list = new List<T>(capacity);
 			this.Comparer = comparer;
 		}
 
+		/// <summary>Initializes a new instance of the <see cref="ComparableCollection{T}"/> class.</summary>
+		/// <param name="collection">The collection to initialize from.</param>
+		/// <param name="comparer">The comparer.</param>
 		public ComparableCollection(IEnumerable<T> collection, IEqualityComparer<T> comparer)
 		{
 			this.list = new List<T>(collection);
@@ -30,14 +42,23 @@
 		#endregion
 
 		#region Public Properties
+
+		/// <summary>Gets the comparer for the collection.</summary>
 		public IEqualityComparer<T> Comparer { get; }
 
+		/// <summary>Gets the number of elements contained in the <see cref="T:System.Collections.Generic.ICollection`1"/>.</summary>
 		public int Count => this.list.Count;
 
+		/// <summary>Gets a value indicating whether the <see cref="T:System.Collections.Generic.ICollection`1"/> is read-only.</summary>
 		public bool IsReadOnly { get; } = false;
 		#endregion
 
 		#region Indexers
+
+		/// <summary>Gets or sets the item at the specified index.</summary>
+		/// <param name="index">The index.</param>
+		/// <value>The item.</value>
+		/// <returns>The item at the specified index.</returns>
 		public T this[int index]
 		{
 			get => this.list[index];
@@ -47,18 +68,44 @@
 		#endregion
 
 		#region Public Methods
+
+		/// <summary>Adds an item to the <see cref="T:System.Collections.Generic.ICollection`1"/>.</summary>
+		/// <param name="item">The object to add to the <see cref="T:System.Collections.Generic.ICollection`1"/>.</param>
 		public void Add(T item) => this.list.Add(item);
 
+		/// <summary>Removes all items from the <see cref="T:System.Collections.Generic.ICollection`1"/>.</summary>
 		public void Clear() => this.list.Clear();
 
+		/// <summary>Determines whether this instance contains the object.</summary>
+		/// <param name="item">The object to locate in the <see cref="T:System.Collections.Generic.ICollection`1"/>.</param>
+		/// <returns>
+		///   <span class="keyword">
+		///     <span class="languageSpecificText">
+		///       <span class="cs">true</span>
+		///       <span class="vb">True</span>
+		///       <span class="cpp">true</span>
+		///     </span>
+		///   </span>
+		///   <span class="nu">
+		///     <span class="keyword">true</span> (<span class="keyword">True</span> in Visual Basic)</span> if <paramref name="item" /> is found in the <see cref="T:System.Collections.Generic.ICollection`1"/>; otherwise, <span class="keyword"><span class="languageSpecificText"><span class="cs">false</span><span class="vb">False</span><span class="cpp">false</span></span></span><span class="nu"><span class="keyword">false</span> (<span class="keyword">False</span> in Visual Basic)</span>.</returns>
 		public bool Contains(T item) => this.IndexOf(item) >= 0;
 
+		/// <summary>Copies the elements of the <see cref="T:System.Collections.Generic.ICollection`1"/> to an <see cref="T:System.Array"/>, starting at a particular <see cref="T:System.Array"/> index.</summary>
+		/// <param name="array">The one-dimensional <see cref="T:System.Array"/> that is the destination of the elements copied from <see cref="T:System.Collections.Generic.ICollection`1"/>. The <see cref="T:System.Array"/> must have zero-based indexing.</param>
+		/// <param name="arrayIndex">The zero-based index in <paramref name="array" /> at which copying begins.</param>
 		public void CopyTo(T[] array, int arrayIndex) => this.list.CopyTo(array, arrayIndex);
 
+		/// <summary>Returns an enumerator that iterates through the collection.</summary>
+		/// <returns>An enumerator that can be used to iterate through the collection.</returns>
 		public IEnumerator<T> GetEnumerator() => this.list.GetEnumerator();
 
+		/// <summary>Returns an enumerator that iterates through a collection.</summary>
+		/// <returns>An <see cref="T:System.Collections.IEnumerator"/> object that can be used to iterate through the collection.</returns>
 		IEnumerator IEnumerable.GetEnumerator() => this.list.GetEnumerator();
 
+		/// <summary>Determines the index of a specific item in the <see cref="T:System.Collections.Generic.IList`1"/>.</summary>
+		/// <param name="item">The object to locate in the <see cref="T:System.Collections.Generic.IList`1"/>.</param>
+		/// <returns>The index of <paramref name="item" /> if found in the list; otherwise, -1.</returns>
 		public int IndexOf(T item)
 		{
 			var comparer = this.Comparer;
@@ -73,8 +120,24 @@
 			return -1;
 		}
 
+		/// <summary>Inserts an item to the <see cref="T:System.Collections.Generic.IList`1"/> at the specified index.</summary>
+		/// <param name="index">The zero-based index at which <paramref name="item" /> should be inserted.</param>
+		/// <param name="item">The object to insert into the <see cref="T:System.Collections.Generic.IList`1"/>.</param>
 		public void Insert(int index, T item) => this.list.Insert(index, item);
 
+		/// <summary>Removes the first occurrence of a specific object from the <see cref="T:System.Collections.Generic.ICollection`1"/>.</summary>
+		/// <param name="item">The object to remove from the <see cref="T:System.Collections.Generic.ICollection`1"/>.</param>
+		/// <returns>
+		///   <span class="keyword">
+		///     <span class="languageSpecificText">
+		///       <span class="cs">true</span>
+		///       <span class="vb">True</span>
+		///       <span class="cpp">true</span>
+		///     </span>
+		///   </span>
+		///   <span class="nu">
+		///     <span class="keyword">true</span> (<span class="keyword">True</span> in Visual Basic)</span> if <paramref name="item" /> was successfully removed from the <see cref="T:System.Collections.Generic.ICollection`1"/>; otherwise, <span class="keyword"><span class="languageSpecificText"><span class="cs">false</span><span class="vb">False</span><span class="cpp">false</span></span></span><span class="nu"><span class="keyword">false</span> (<span class="keyword">False</span> in Visual Basic)</span>. This method also returns <span class="keyword"><span class="languageSpecificText"><span class="cs">false</span><span class="vb">False</span><span class="cpp">false</span></span></span><span class="nu"><span class="keyword">false</span> (<span class="keyword">False</span> in Visual Basic)</span> if <paramref name="item" /> is not found in the original <see cref="T:System.Collections.Generic.ICollection`1"/>.
+		/// </returns>
 		public bool Remove(T item)
 		{
 			// Behaviour here mirrors that of List<T>, which only removes the first instance of item, not all instances.
@@ -90,6 +153,8 @@
 			return false;
 		}
 
+		/// <summary>Removes the <see cref="T:System.Collections.Generic.IList`1"/> item at the specified index.</summary>
+		/// <param name="index">The zero-based index of the item to remove.</param>
 		public void RemoveAt(int index) => this.list.RemoveAt(index);
 		#endregion
 	}
