cf0c5c065c92df79dacc885349ac476398c7dd88
Robin Hood <robinhood70@live.ca>
2019-02-25 02:12:52 -0500

    Reformat and add C-like constants

--- HoodBot/DiffViewers/MessageFilter.cs
+++ HoodBot/DiffViewers/MessageFilter.cs
@@ -2,43 +2,37 @@
 {
 	using System.Runtime.InteropServices;
 
+	// This code taken from https://docs.microsoft.com/en-us/previous-versions/ms228772(v=vs.140). It is intended to handle ComException that arise when trying to automate Visual Studio.
+	// Class containing the IOleMessageFilter thread error-handling functions.
 	internal class MessageFilter : IOleMessageFilter
 	{
-		// Class containing the IOleMessageFilter thread error-handling functions.
+		internal const int ServerCallIsHandled = 0;
+		internal const int ServerCallRetryLater = 2;
+		internal const int PendingMessageWaitDeferProcess = 2;
+
+#pragma warning disable CA1806 // Do not ignore method results
 
 		// Start the filter.
 		public static void Register()
 		{
 			IOleMessageFilter newFilter = new MessageFilter();
-#pragma warning disable CA1806 // Do not ignore method results
-			CoRegisterMessageFilter(newFilter, out var _);
-#pragma warning restore CA1806 // Do not ignore method results
+			CoRegisterMessageFilter(newFilter, out _);
 		}
 
 		// Done with the filter, close it.
-#pragma warning disable CA1806 // Do not ignore method results
-		public static void Revoke() => CoRegisterMessageFilter(null, out var _);
+		public static void Revoke() => CoRegisterMessageFilter(null, out _);
 #pragma warning restore CA1806 // Do not ignore method results
 
 		// IOleMessageFilter functions.
 		// Handle incoming thread requests.
-		int IOleMessageFilter.HandleInComingCall(int dwCallType, System.IntPtr hTaskCaller, int dwTickCount, System.IntPtr lpInterfaceInfo) => 0; // Return the flag SERVERCALL_ISHANDLED.
+		int IOleMessageFilter.HandleInComingCall(int dwCallType, System.IntPtr hTaskCaller, int dwTickCount, System.IntPtr lpInterfaceInfo) => ServerCallIsHandled;
 
 		// Thread call was rejected, so try again.
-		int IOleMessageFilter.RetryRejectedCall(System.IntPtr hTaskCallee, int dwTickCount, int dwRejectType)
-		{
-			// flag = SERVERCALL_RETRYLATER.
-			if (dwRejectType == 2)
-			{
-				// Retry the thread call immediately if return >=0 & < 100.
-				return 99;
-			}
+		// RetryLater = Retry the thread call immediately if return >=0 & < 100.
+		// Otherwise: Too busy; cancel call.
+		int IOleMessageFilter.RetryRejectedCall(System.IntPtr hTaskCallee, int dwTickCount, int dwRejectType) => dwRejectType == ServerCallRetryLater ? 99 : -1;
 
-			// Too busy; cancel call.
-			return -1;
-		}
-
-		int IOleMessageFilter.MessagePending(System.IntPtr hTaskCallee, int dwTickCount, int dwPendingType) => 2; // Return the flag PENDINGMSG_WAITDEFPROCESS.
+		int IOleMessageFilter.MessagePending(System.IntPtr hTaskCallee, int dwTickCount, int dwPendingType) => PendingMessageWaitDeferProcess;
 
 		// Implement the IOleMessageFilter interface.
 		[DllImport("Ole32.dll")]
