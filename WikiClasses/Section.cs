93fcc3a11856a4803c1a6424d032c0f2f9b3d2b7
Robin Hood <robinhood70@live.ca>
2019-03-16 01:49:00 -0400

    Import SectionedPage object

---
+++ WikiClasses/Section.cs
@@ -0,0 +1,157 @@
+﻿namespace RobinHood70.WikiClasses
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Text;
+	using System.Text.RegularExpressions;
+
+	public class Section
+	{
+		#region Constructors
+		public Section()
+			: this(null, 0, null)
+		{
+		}
+
+		public Section(string title, int level)
+			: this(title, level, null)
+		{
+		}
+
+		public Section(string title, int level, string text)
+		{
+			this.Title = title;
+			this.Level = level;
+			this.Text = text;
+		}
+		#endregion
+
+		#region Public Static Properties
+		public static Regex SectionFinder { get; } = new Regex(@"^(?<addbefore>\<!--\ *)?(?<levelopen>={1,6})(?<wslead>\ *)(?<title>.*?)(?<wstrail>\s*)(?<levelclose>={1,6})(?<addafter>\ *--\>)?\ *\r?\n", RegexOptions.Multiline | RegexOptions.Compiled);
+		#endregion
+
+		#region Public Properties
+		public string AddAfterTitle { get; set; }
+
+		public string AddBeforeTitle { get; set; }
+
+		public string FormattedTitle
+		{
+			get
+			{
+				if (string.IsNullOrWhiteSpace(this.Title))
+				{
+					return null;
+				}
+
+				var equals = "======".Substring(0, this.Level);
+				return string.Concat(
+					this.AddBeforeTitle,
+					equals,
+					this.TitleLeadingWhiteSpace,
+					this.Title,
+					this.TitleTrailingWhiteSpace,
+					equals,
+					this.AddAfterTitle);
+			}
+		}
+
+		public int Level { get; set; }
+
+		public IList<Section> Subsections { get; } = new List<Section>();
+
+		public string Text { get; set; }
+
+		public string Title { get; set; }
+
+		public string TitleLeadingWhiteSpace { get; set; }
+
+		public string TitleTrailingWhiteSpace { get; set; }
+		#endregion
+
+		#region Public Methods
+		public string Build() => this.Build(new StringBuilder()).ToString();
+
+		public Section Find(string title)
+		{
+			foreach (var section in this.Subsections)
+			{
+				if (section.Title == title)
+				{
+					return section;
+				}
+			}
+
+			// Separate loops so that all of current level is checked before trying subtitles.
+			foreach (var section in this.Subsections)
+			{
+				var subTitle = section.Find(title);
+				if (subTitle != null)
+				{
+					return subTitle;
+				}
+			}
+
+			return null;
+		}
+		#endregion
+
+		#region Public Override Methods
+		public override string ToString() => this.Title;
+		#endregion
+
+		#region Internal Static Methods
+		internal static Section Parse(MatchCollection matches, string text, ref int offset)
+		{
+			var retval = new Section();
+			Match match = matches[offset];
+			var groups = match.Groups;
+			var level = groups["levelopen"].Value.Length;
+			if (level != groups["levelclose"].Value.Length)
+			{
+				throw new InvalidOperationException("Different numbers of '=' in Section header.");
+			}
+
+			var textStart = match.Index + match.Length;
+			retval.AddAfterTitle = groups["addafter"].Value;
+			retval.AddBeforeTitle = groups["addbefore"].Value;
+			retval.Level = level;
+			retval.Text = (offset == matches.Count - 1) ? text.Substring(textStart) : text.Substring(textStart, matches[offset + 1].Index - textStart);
+			retval.Title = groups["title"].Value;
+			retval.TitleLeadingWhiteSpace = groups["wslead"].Value;
+			retval.TitleTrailingWhiteSpace = groups["wstrail"].Value;
+			offset++;
+
+			while (offset < matches.Count && matches[offset].Groups["levelopen"].Value.Length > level)
+			{
+				retval.Subsections.Add(Parse(matches, text, ref offset));
+			}
+
+			return retval;
+		}
+		#endregion
+
+		#region Internal Methods
+		internal StringBuilder Build(StringBuilder sb)
+		{
+			if (this.Title != null)
+			{
+				sb.Append(this.FormattedTitle + '\n');
+			}
+
+			sb.Append(this.Text);
+			foreach (var section in this.Subsections)
+			{
+				if (sb[sb.Length - 1] != '\n')
+				{
+					sb.Append('\n');
+				}
+
+				sb.Append(section.Build());
+			}
+
+			return sb;
+		}
+		#endregion
+	}
+}
---
+++ WikiClasses/SectionedPage.cs
@@ -0,0 +1,208 @@
+﻿namespace RobinHood70.WikiClasses
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Text;
+	using System.Text.RegularExpressions;
+	using static RobinHood70.WikiCommon.Globals;
+
+	// TODO: This has had a re-think. Needs testing.
+	public class SectionedPage
+	{
+		#region Constructors
+		public SectionedPage(string pageText)
+		{
+			ThrowNull(pageText, nameof(pageText));
+			var titles = Section.SectionFinder.Matches(pageText);
+			this.Lead = titles.Count == 0 ? pageText : pageText.Substring(0, titles[0].Index);
+
+			var offset = 0;
+			while (offset < titles.Count)
+			{
+				this.Sections.Add(Section.Parse(titles, pageText, ref offset));
+			}
+		}
+		#endregion
+
+		#region Public Properties
+
+		/// <summary>Gets or sets the page footer.</summary>
+		/// <remarks>This is a convenience property that allows certain trailing text to be handled separately, if desired. Use one of the ExtractFooter() methods or specify your own text. Any ExtractFooter methods will automatically remove the relevant text from the last section.</remarks>
+		public string Footer { get; set; } = string.Empty;
+
+		public string Lead { get; set; } = string.Empty;
+
+		public IList<Section> Sections { get; } = new List<Section>();
+		#endregion
+
+		#region Public Methods
+		public string Build()
+		{
+			var sb = new StringBuilder();
+			sb.Append(this.Lead);
+			foreach (var section in this.Sections)
+			{
+				if (sb[sb.Length - 1] != '\n')
+				{
+					sb.Append('\n');
+				}
+
+				section.Build(sb);
+			}
+
+			if (!string.IsNullOrWhiteSpace(this.Footer))
+			{
+				if (sb[sb.Length - 1] != '\n')
+				{
+					sb.Append('\n');
+				}
+
+				sb.Append(this.Footer);
+			}
+
+			return sb.ToString().Trim();
+		}
+
+		public void ExtractFooter()
+		{
+			// Any lines that start with "[[Category" or "{{" (with some exceptions) will be split off from the last section and put into the Footer property.
+			// TODO: This could use a re-think, and being UESP-specific, should be removed from this class.
+			if (this.Sections.Count == 0)
+			{
+				return;
+			}
+
+			var lastSection = this.LastSection();
+			var lines = lastSection.Text.Split('\n');
+			var i = lines.Length - 1;
+			var line = lines[i].TrimEnd();
+			var lineLower = line.ToLowerInvariant();
+			while (i >= 0
+					&& (line.Length == 0
+						|| lineLower.StartsWith("[[category", StringComparison.Ordinal)
+						|| (lineLower.StartsWith("{{", StringComparison.Ordinal)
+							&& !lineLower.Contains(":")
+							&& !lineLower.StartsWith("{{bug", StringComparison.Ordinal)
+							&& !lineLower.StartsWith("{{newleft", StringComparison.Ordinal)
+							&& !lineLower.StartsWith("{{newright", StringComparison.Ordinal)
+							&& !lineLower.StartsWith("{{newline", StringComparison.Ordinal)
+							&& !lineLower.StartsWith("{{quest link", StringComparison.Ordinal))))
+			{
+				this.Footer = "\n" + line + this.Footer;
+				i--;
+				if (i >= 0)
+				{
+					line = lines[i].TrimEnd();
+					lineLower = line.ToLowerInvariant();
+				}
+			}
+
+			if (i < lines.Length - 1)
+			{
+				lastSection.Text = string.Join("\n", lines, 0, i + 1);
+			}
+
+			if (i == -1 && lastSection.AddBeforeTitle.Length == 0 && lastSection.AddAfterTitle.Length == 0)
+			{
+				throw new InvalidOperationException("Entire section appears to be a footer. This is rather unlikely.");
+			}
+		}
+
+		public void ExtractFooter(IEnumerable<string> lastTexts)
+		{
+			// Any text coming after the earliest of the lastTexts will be split off from the last section and put into the Footer property.
+			ThrowNull(lastTexts, nameof(lastTexts));
+			var lastSection = this.LastSection();
+			var pos = int.MaxValue;
+			foreach (var text in lastTexts)
+			{
+				var textPos = lastSection.Text.IndexOf(text, StringComparison.Ordinal);
+				if (textPos >= 0 && textPos < pos)
+				{
+					pos = textPos;
+				}
+			}
+
+			if (pos < int.MaxValue)
+			{
+				this.Footer = lastSection.Text.Substring(pos);
+				lastSection.Text = lastSection.Text.Substring(0, pos);
+			}
+		}
+
+		public void ExtractFooter(Regex lastRegex)
+		{
+			// Any text that matches lastRegex will be split off from the last section and put into the Footer property.
+			ThrowNull(lastRegex, nameof(lastRegex));
+			var lastSection = this.LastSection();
+			var lastMatch = lastRegex.Match(lastSection.Text);
+			if (lastMatch.Success)
+			{
+				this.Footer = lastSection.Text.Substring(lastMatch.Index);
+				lastSection.Text = lastSection.Text.Substring(0, lastMatch.Index);
+			}
+		}
+
+		public Section FindFirstSection(string title)
+		{
+			foreach (var section in this.FindSection(title))
+			{
+				return section;
+			}
+
+			return null;
+		}
+
+		public Section FindLastSection(string title)
+		{
+			Section foundSection = null;
+			foreach (var section in this.FindSection(title))
+			{
+				foundSection = section;
+			}
+
+			return foundSection;
+		}
+
+		public IEnumerable<Section> FindSection(string title)
+		{
+			// Primitive search that assumes you want the first title with the matching name.
+			// TODO: If needed, build other FindSections that look for titles with specific levels or level ranges.
+			foreach (var section in this.Sections)
+			{
+				if (section.Title == title)
+				{
+					yield return section;
+				}
+			}
+
+			foreach (var section in this.Sections)
+			{
+				var found = section.Find(title);
+				if (found != null)
+				{
+					yield return found;
+				}
+			}
+		}
+		#endregion
+
+		#region Private Methods
+		private Section LastSection()
+		{
+			if (this.Sections.Count == 0)
+			{
+				return null;
+			}
+
+			var section = this.Sections[this.Sections.Count - 1];
+			while (section.Subsections.Count > 0)
+			{
+				section = section.Subsections[section.Subsections.Count - 1];
+			}
+
+			return section;
+		}
+		#endregion
+	}
+}
