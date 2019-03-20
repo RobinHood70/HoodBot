07be0af0f0d7a284e0411ea32ba8e05a32baa73b
Robin Hood <robinhood70@live.ca>
2019-03-19 23:57:40 -0400

    Abandon EqualityComparer in favour of IEqualityComparer due to unwanted implementation issues

--- Robby/Design/SimpleTitleEqualityComparer.cs
+++ Robby/Design/SimpleTitleEqualityComparer.cs
@@ -1,11 +1,12 @@
 ﻿namespace RobinHood70.Robby.Design
 {
+	using System.Collections;
 	using System.Collections.Generic;
 	using static RobinHood70.WikiCommon.Globals;
 
 	/// <summary>An ISimpleTitle equality comparer to determine equality based on the Namespace and PageName only.</summary>
 	/// <seealso cref="System.Collections.Generic.EqualityComparer{T}" />
-	public class SimpleTitleEqualityComparer : EqualityComparer<ISimpleTitle>
+	public class SimpleTitleEqualityComparer : IEqualityComparer<ISimpleTitle>, IEqualityComparer
 	{
 		#region Constructors
 		private SimpleTitleEqualityComparer()
@@ -26,15 +27,19 @@
 		/// <param name="x">The first object of type <see cref="ISimpleTitle" /> to compare.</param>
 		/// <param name="y">The second object of type <see cref="ISimpleTitle" /> to compare.</param>
 		/// <returns><see langword="true" /> if the specified objects are equal; otherwise, <see langword="false" />.</returns>
-		public override bool Equals(ISimpleTitle x, ISimpleTitle y) =>
-			x == null ? x == y :
+		public bool Equals(ISimpleTitle x, ISimpleTitle y) =>
+			x == null ? y == null :
 			y == null ? false :
-			x.Namespace.Equals(y.Namespace) && x.Namespace.PageNameEquals(x.PageName, y.PageName);
+			x.Namespace == y.Namespace && x.Namespace.PageNameEquals(x.PageName, y.PageName);
 
 		/// <summary>Returns a hash code for this instance.</summary>
 		/// <param name="obj">The object.</param>
 		/// <returns>A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table.</returns>
-		public override int GetHashCode(ISimpleTitle obj) => obj == null ? 0 : CompositeHashCode(obj.Namespace, obj.PageName);
+		public int GetHashCode(ISimpleTitle obj) => obj == null ? 0 : CompositeHashCode(obj.Namespace, obj.PageName);
+
+		bool IEqualityComparer.Equals(object x, object y) => x == y || (x is ISimpleTitle newX && y is ISimpleTitle newY ? this.Equals(newX, newY) : false);
+
+		int IEqualityComparer.GetHashCode(object obj) => this.GetHashCode(obj as ISimpleTitle);
 		#endregion
 	}
 }
