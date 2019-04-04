a8f5e994943a15e5eb7f72cfefba8ef9aced94ac
Robin Hood <robinhood70@live.ca>
2019-04-04 00:50:40 -0400

    Add framework for specific job

---
+++ HoodBot/Jobs/PageMover.cs
@@ -0,0 +1,22 @@
+﻿namespace RobinHood70.HoodBot.Jobs
+{
+	using System.Collections.Generic;
+	using RobinHood70.HoodBot.Jobs.Design;
+	using RobinHood70.Robby;
+
+	public class PageMover : PageMoverJob
+	{
+		[JobInfo("Page Mover")]
+		public PageMover(Site site, AsyncInfo asyncInfo, TaskActions taskActions)
+			: base(site, asyncInfo, taskActions)
+		{
+		}
+
+		protected override IList<Replacement> GetReplacements()
+		{
+			var retval = new List<Replacement>();
+
+			return retval;
+		}
+	}
+}
