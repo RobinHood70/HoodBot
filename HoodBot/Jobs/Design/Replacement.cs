5a5a420c93abec716546c61831aa0f6c67c23e50
Robin Hood <robinhood70@live.ca>
2019-04-06 03:10:37 -0400

    Expand functionality and seal

--- HoodBot/Jobs/Design/Replacement.cs
+++ HoodBot/Jobs/Design/Replacement.cs
@@ -1,6 +1,9 @@
 ﻿namespace RobinHood70.HoodBot.Jobs.Design
 {
+	using System;
+	using Newtonsoft.Json;
 	using RobinHood70.Robby;
+	using RobinHood70.Robby.Design;
 	using static RobinHood70.WikiCommon.Globals;
 
 	public enum ReplacementAction
@@ -11,31 +14,72 @@
 		ProposeForDeletion,
 	}
 
-	public class Replacement
+	public sealed class Replacement : IComparable<Replacement>, IEquatable<Replacement>
 	{
+		#region Constructors
 		public Replacement(Site site, string from, string to)
+			: this(new Title(site, from), new Title(site, to))
 		{
-			ThrowNull(site, nameof(site));
-			ThrowNull(from, nameof(from));
-			ThrowNull(to, nameof(to));
-			this.From = new Title(site, from);
-			this.To = new Title(site, to);
 		}
 
 		public Replacement(Title from, Title to)
 		{
 			ThrowNull(from, nameof(from));
 			ThrowNull(to, nameof(to));
+
+			if (from == to)
+			{
+				throw new ArgumentException($"From and to pages cannot be the same: {from.FullPageName} == {to.FullPageName}");
+			}
+
 			this.From = from;
 			this.To = to;
 		}
 
+		[JsonConstructor]
+		private Replacement()
+		{
+		}
+		#endregion
+
+		#region Public Properties
 		public ReplacementAction Action { get; set; } = ReplacementAction.Unknown;
 
 		public string ActionReason { get; set; }
 
-		public Title From { get; }
+		public string DeleteReason { get; set; }
 
-		public Title To { get; }
+		public Title From { get; set; }
+
+		public Title To { get; set; }
+		#endregion
+
+		#region Operators
+		public static bool operator ==(Replacement left, Replacement right) => left is null ? right is null : left.Equals(right);
+
+		public static bool operator !=(Replacement left, Replacement right) => !(left == right);
+
+		public static bool operator <(Replacement left, Replacement right) => left is null ? !(right is null) : left.CompareTo(right) < 0;
+
+		public static bool operator <=(Replacement left, Replacement right) => left is null || left.CompareTo(right) <= 0;
+
+		public static bool operator >(Replacement left, Replacement right) => !(left is null) && left.CompareTo(right) > 0;
+
+		public static bool operator >=(Replacement left, Replacement right) => left is null ? right is null : left.CompareTo(right) >= 0;
+		#endregion
+
+		#region Public Methods
+		public int CompareTo(Replacement other) => TitleComparer<Title>.Instance.Compare(this.From, other.From);
+
+		public bool Equals(Replacement other) => !(other is null) && this.From == other.From; // Nothing else is checked for equality, as multiple values for the same From are invalid.
+		#endregion
+
+		#region Public Override Methods
+		public override bool Equals(object obj) => this.Equals(obj as Replacement);
+
+		public override int GetHashCode() => this.From.GetHashCode();
+
+		public override string ToString() => $"{this.Action}: {this.From.FullPageName} → {this.To.FullPageName}";
+		#endregion
 	}
 }
