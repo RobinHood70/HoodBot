feea95ed59266ae73783dbbc3bbd724837958660
Robin Hood <robinhood70@live.ca>
2019-04-06 03:13:36 -0400

    Update template lists

--- HoodBot/Uesp/HoodBotFunctions.cs
+++ HoodBot/Uesp/HoodBotFunctions.cs
@@ -45,9 +45,9 @@
 		#endregion
 
 		#region Public Override Properties
-		public override IReadOnlyList<string> DeleteTemplates { get; } = new[] { "Proposeddeletion", "Prod", "Proposeddeletion-bot", "Speed", "Speedydeletion" };
+		public override IReadOnlyList<string> DeleteTemplates { get; } = new[] { "Proposeddeletion", "Prod", "Speed", "Speedydeletion" };
 
-		public override IReadOnlyList<string> DoNotDeleteTemplates { get; } = new[] { "Empty category", "Linked images" };
+		public override IReadOnlyList<string> DoNotDeleteTemplates { get; } = new[] { "Empty category", "Linked image" };
 
 		public override LogJobTypes LogJobTypes => LogJobTypes.Write;
 		#endregion
