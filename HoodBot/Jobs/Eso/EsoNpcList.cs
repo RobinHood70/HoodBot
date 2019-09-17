13c48e48b8b3649c14d8daa70ac58481c1ab92d1
Robin Hood <robinhood70@live.ca>
2019-09-17 19:46:02 -0400

    Customary regions and TryGetValue method

--- HoodBot/Jobs/Eso/EsoNpcList.cs
+++ HoodBot/Jobs/Eso/EsoNpcList.cs
@@ -6,12 +6,36 @@
 
 	internal class EsoNpcList : KeyedCollection<long, NPCData>
 	{
+		#region Public Methods
 		public void SortByPageName()
 		{
 			var list = this.Items as List<NPCData>;
 			list.Sort((x, y) => string.Compare(x.PageName, y.PageName, StringComparison.Ordinal));
 		}
 
+		public bool TryGetValue(long key, out NPCData item)
+		{
+			if (this.Dictionary != null)
+			{
+				return this.Dictionary.TryGetValue(key, out item);
+			}
+
+			foreach (var testItem in this)
+			{
+				if (this.GetKeyForItem(testItem) == key)
+				{
+					item = testItem;
+					return true;
+				}
+			}
+
+			item = null;
+			return false;
+		}
+		#endregion
+
+		#region Protected Override Methods
 		protected override long GetKeyForItem(NPCData item) => item?.Id ?? -1;
+		#endregion
 	}
 }
