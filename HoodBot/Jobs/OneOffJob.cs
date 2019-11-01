24be362559aa49b92cfeeadae9425b522fb20b69
Robin Hood <robinhood70@live.ca>
2019-11-01 18:51:58 -0400

    Reset to empty

--- HoodBot/Jobs/OneOffJob.cs
+++ HoodBot/Jobs/OneOffJob.cs
@@ -1,104 +1,45 @@
 ﻿namespace RobinHood70.HoodBot.Jobs
 {
+	using System;
 	using System.Collections.Generic;
-	using System.Text.RegularExpressions;
 	using RobinHood70.HoodBot.Jobs.Design;
-	using RobinHood70.HoodBot.Uesp;
 	using RobinHood70.Robby;
-	using RobinHood70.Robby.Design;
-	using RobinHood70.WikiClasses;
 	using RobinHood70.WikiCommon;
 
-	public class OneOffJob : EditJob
+	public class OneOffJob : WikiJob
 	{
-		#region Static Fields
-		public static readonly bool UseFastMethod = true;
-		#endregion
-
 		#region Constructors
 		[JobInfo("One-Off Job")]
 		public OneOffJob([ValidatedNotNull] Site site, AsyncInfo asyncInfo)
 			: base(site, asyncInfo)
 		{
 		}
+		#endregion
 
-		public override string LogName => "Update Lore Transclusions Page";
+		#region Public Override Properties
+		//// public override string LogName => "Check for Jeancey";
 		#endregion
 
 		#region Protected Override Methods
-		protected override void PrepareJob()
-		{
-			this.Site.UserFunctions.ResultsPage = new Page(this.Site, this.Site.User.FullPageName + "/Lore Transclusions");
-			this.Site.UserFunctions.SetResultTitle(ResultDestination.ResultsPage, "Lore Transclusions (minus all Game Book pages, even if they have a Manual Lore transclusion)");
-			var allTransclusions = GetLoreTransclusions(this.Site);
-
-			Namespace lastNamespace = null;
-			foreach (var page in allTransclusions)
-			{
-				var links = UseFastMethod ? FilterTemplates(page) : FindLoreTransclusions(page);
-				if (links.Count > 0)
-				{
-					if (page.Namespace != lastNamespace)
-					{
-						lastNamespace = page.Namespace;
-						this.WriteLine();
-						this.WriteLine($"== {lastNamespace.Name} ==");
-					}
-
-					this.WriteLine($"* [[{page.FullPageName}]] transcludes: {string.Join(", ", links)}");
-				}
-			}
-		}
-
 		protected override void Main()
 		{
-		}
-		#endregion
-
-		#region Private Static Methods
-		private static ICollection<string> FilterTemplates(Page page)
-		{
-			var retval = new SortedSet<string>();
-			foreach (var template in page.Templates)
+			var titles = new TitleCollection(this.Site);
+			titles.GetQueryPage("Wantedpages");
+			var sorted = new List<string>();
+			foreach (var title in titles)
 			{
-				if (template.FullPageName == "Template:Game Book")
+				if (title.Namespace == MediaWikiNamespaces.Main)
 				{
-					return retval;
-				}
-
-				if (template.Namespace == UespNamespaces.Lore)
-				{
-					retval.Add($"[[{template.FullPageName}|]]");
+					var uri = Uri.EscapeUriString(title.FullPageName).Replace("?", "%3F");
+					sorted.Add($"* [https://en.uesp.net/wiki/Special:WhatLinksHere/{uri} {title.FullPageName}]");
 				}
 			}
 
-			return retval;
-		}
-
-		private static ICollection<string> FindLoreTransclusions(Page page)
-		{
-			var search = Template.FindRaw(null, @"\s*:?\s*(?i:l)ore:[^#\|}]+?", null, RegexOptions.None, 10);
-			var matches = search.Matches(page.Text);
-			var links = new SortedSet<string>();
-			foreach (Match match in matches)
+			sorted.Sort();
+			foreach (var item in sorted)
 			{
-				var template = Template.Parse(match.Value);
-				links.Add($"[[{template.Name}|]]");
+				this.WriteLine(item);
 			}
-
-			return links;
-		}
-
-		private static PageCollection GetLoreTransclusions(Site site)
-		{
-			var modules = PageModules.Info | (UseFastMethod ? PageModules.Templates : PageModules.Revisions);
-			var allTransclusions = PageCollection.Unlimited(site, new PageLoadOptions(modules));
-			allTransclusions.GetTransclusions(UespNamespaces.Lore);
-			//// allTransclusions.GetTitles("Arena:Ria Silmane");
-			allTransclusions.RemoveNamespaces(true, UespNamespaces.User, UespNamespaces.Lore);
-			allTransclusions.Sort();
-
-			return allTransclusions;
 		}
 		#endregion
 	}
