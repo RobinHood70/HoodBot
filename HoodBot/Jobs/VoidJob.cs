47acf449a597c3b37a5afb6f10632690bb9ef068
Robin Hood <robinhood70@live.ca>
2019-11-13 19:15:28 -0500

    Simplify void job

--- HoodBot/Jobs/VoidJob.cs
+++ HoodBot/Jobs/VoidJob.cs
@@ -4,18 +4,16 @@
 	using RobinHood70.HoodBot.Jobs.Design;
 	using RobinHood70.Robby;
 
-	public class VoidJob : EditJob
+	public class VoidJob : WikiJob
 	{
 		#region Constructors
-		[JobInfo("Do Nothing")]
+		// We want to keep this job around for testing, but don't need it to clutter up the main list, so comment out the JobInfo.
+		// [JobInfo("Do Nothing")]
 		public VoidJob(Site site, AsyncInfo asyncInfo)
 			: base(site, asyncInfo) => this.ProgressMaximum = 5;
 		#endregion
 
-		#region Public Override Properties
-		public override string LogName => "Do Nothing";
-		#endregion
-
+		#region Protected Override Methods
 		protected override void Main()
 		{
 			for (var i = 1; i <= this.ProgressMaximum; i++)
@@ -26,5 +24,6 @@
 				this.Progress++;
 			}
 		}
+		#endregion
 	}
 }
