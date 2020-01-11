def5d7309fcb29ab3109f0d803852d9c3c7a2ad5
Robin Hood <robinhood70@live.ca>
2020-01-11 14:17:25 -0500

    One-off job

---
+++ HoodBot/Jobs/JobModels/Extensions.cs
@@ -0,0 +1,19 @@
+﻿namespace RobinHood70.HoodBot.Jobs.JobModels
+{
+	using RobinHood70.Robby;
+	using RobinHood70.Robby.Design;
+	using RobinHood70.WikiClasses.Parser;
+	using RobinHood70.WikiCommon;
+	using static RobinHood70.WikiCommon.Globals;
+
+	public static class Extensions
+	{
+		public static TitleParts BacklinkTitleToParts(this IBacklinkNode node, Site site)
+		{
+			ThrowNull(node, nameof(node));
+			ThrowNull(site, nameof(site));
+			var text = WikiTextVisitor.Value(node.Title);
+			return node is TemplateNode ? new TitleParts(site, MediaWikiNamespaces.Template, text) : new TitleParts(site, text);
+		}
+	}
+}
--- HoodBot/Jobs/OneOffJob.cs
+++ HoodBot/Jobs/OneOffJob.cs
@@ -1,36 +1,64 @@
 ﻿namespace RobinHood70.HoodBot.Jobs
 {
 	using System;
+	using System.Diagnostics;
 	using RobinHood70.HoodBot.Jobs.Design;
+	using RobinHood70.HoodBot.Jobs.JobModels;
 	using RobinHood70.Robby;
+	using RobinHood70.WikiClasses.Parser;
 	using RobinHood70.WikiCommon;
 
-	public class OneOffJob : WikiJob
+	public class OneOffJob : EditJob
 	{
 		#region Constructors
 		[JobInfo("One-Off Job")]
 		public OneOffJob([ValidatedNotNull] Site site, AsyncInfo asyncInfo)
-			: base(site, asyncInfo)
-		{
-		}
+			: base(site, asyncInfo) => this.Pages.SetLimitations(LimitationType.Remove, MediaWikiNamespaces.User);
 		#endregion
 
 		#region Public Override Properties
-		//// public override string LogName => "Check for Jeancey";
+		public override string LogName => "Update Navboxes";
 		#endregion
 
 		#region Protected Override Methods
-		protected override void Main()
+		protected override void BeforeLogging()
 		{
-			var results = PageCollection.Unlimited(this.Site);
-			results.GetSearchResults("Riddle'thar", this.Site.Namespaces.RegularIds);
-			foreach (var page in results)
+			this.Pages.PageLoaded += this.Results_PageLoaded;
+			this.Pages.GetCategoryMembers("Navbox Templates");
+			this.Pages.Sort();
+			foreach (var page in this.Pages)
 			{
-				if (page.Text.IndexOf("Riddle'thar", StringComparison.CurrentCulture) >= 0)
+				Debug.WriteLine(page.FullPageName);
+			}
+
+			this.Pages.PageLoaded -= this.Results_PageLoaded;
+		}
+
+		protected override void Main() => this.SavePages("Remove name parameter");
+		#endregion
+
+		#region Private Methods
+		private void Results_PageLoaded(object sender, Page page)
+		{
+			var parsedText = WikiTextParser.Parse(page.Text);
+			var navboxes = parsedText.FindAll<TemplateNode>(item => item.BacklinkTitleToParts(this.Site).PageName == "Navbox");
+			foreach (var navbox in navboxes)
+			{
+				navbox.RemoveParameter("name");
+				var navbars = navbox.Parameters.FindAllLinked<ParameterNode>(parameter => parameter.NameToText() == "name");
+				foreach (var navbar in navbars)
 				{
-					this.StatusWriteLine(page.FullPageName);
+					var navbarText = WikiTextVisitor.Raw(navbar.Value);
+					if (navbar.Previous?.Value is ParameterNode paramNode && navbarText.Contains("\n\n", StringComparison.Ordinal))
+					{
+						paramNode.Value.AddText("\n");
+					}
+
+					navbox.Parameters.Remove(navbar);
 				}
 			}
+
+			page.Text = WikiTextVisitor.Raw(parsedText);
 		}
 		#endregion
 	}
