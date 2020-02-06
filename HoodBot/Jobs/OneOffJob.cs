5779e3b642f05236104b69de809de3707cfb23b4
Robin Hood <robinhood70@live.ca>
2020-02-05 22:46:38 -0500

    Work around mod header save conflict

--- HoodBot/Jobs/OneOffJob.cs
+++ HoodBot/Jobs/OneOffJob.cs
@@ -1,7 +1,5 @@
 ﻿namespace RobinHood70.HoodBot.Jobs
 {
-	using System;
-	using System.Diagnostics;
 	using RobinHood70.HoodBot.Jobs.Design;
 	using RobinHood70.HoodBot.Jobs.JobModels;
 	using RobinHood70.Robby;
@@ -17,48 +15,60 @@
 		#endregion
 
 		#region Public Override Properties
-		public override string LogName => "Update Navboxes";
+		public override string LogName => "One-Off Job";
 		#endregion
 
 		#region Protected Override Methods
 		protected override void BeforeLogging()
 		{
 			this.Pages.PageLoaded += this.Results_PageLoaded;
-			this.Pages.GetCategoryMembers("Navbox Templates");
+			this.Pages.GetBacklinks("Template:Mod Icon Data", BacklinksTypes.EmbeddedIn);
 			this.Pages.Sort();
-			foreach (var page in this.Pages)
-			{
-				Debug.WriteLine(page.FullPageName);
-			}
-
 			this.Pages.PageLoaded -= this.Results_PageLoaded;
 		}
 
-		protected override void Main() => this.SavePages("Remove name parameter");
+		protected override void Main() => this.SavePages("Work around mod header issue on save");
 		#endregion
 
 		#region Private Methods
 		private void Results_PageLoaded(object sender, Page page)
 		{
 			var parsedText = WikiTextParser.Parse(page.Text);
-			var navboxes = parsedText.FindAll<TemplateNode>(item => item.BacklinkTitleToParts(this.Site).PageName == "Navbox");
-			foreach (var navbox in navboxes)
+			var modIconData = parsedText.FindFirstLinked<TemplateNode>(item => item.BacklinkTitleToParts(this.Site).PageName == "Mod Icon Data");
+			var modHeader = parsedText.FindFirst<TemplateNode>(item => item.BacklinkTitleToParts(this.Site).PageName == "Mod Header");
+			if (modIconData != null && modHeader != null)
 			{
-				navbox.RemoveParameter("name");
-				var navbars = navbox.Parameters.FindAllLinked<ParameterNode>(parameter => parameter.NameToText() == "name");
-				foreach (var navbar in navbars)
+				var modHeaderNameRaw = modHeader.FindParameter("1");
+				var modHeaderName = modHeaderNameRaw == null ? null : WikiTextVisitor.Value(modHeaderNameRaw.Value);
+				if (modIconData.Value is TemplateNode modIconTemplate && modHeaderName == page.PageName)
 				{
-					var navbarText = WikiTextVisitor.Raw(navbar.Value);
-					if (navbar.Previous?.Value is ParameterNode paramNode && navbarText.Contains("\n\n", StringComparison.Ordinal))
+					if (modIconData.Previous?.Value is TextNode textNode && textNode.Text[^1] == '\n')
 					{
-						paramNode.Value.AddText("\n");
+						textNode.Text = textNode.Text[0..^1].TrimEnd(' ');
 					}
 
-					navbox.Parameters.Remove(navbar);
-				}
-			}
+					parsedText.Remove(modIconData);
+					var icon = modIconTemplate.FindParameter("1");
+					if (icon != null)
+					{
+						icon.SetName("icon");
+						modHeader.Parameters.AddLast(icon);
+					}
 
-			page.Text = WikiTextVisitor.Raw(parsedText);
+					var iconSize = modIconTemplate.FindParameter("2");
+					if (iconSize != null)
+					{
+						var sizeValue = WikiTextVisitor.Raw(iconSize.Value);
+						if (sizeValue != "32" && sizeValue != "x32")
+						{
+							iconSize.SetName("iconSize");
+							modHeader.Parameters.AddLast(iconSize);
+						}
+					}
+				}
+
+				page.Text = WikiTextVisitor.Raw(parsedText);
+			}
 		}
 		#endregion
 	}
