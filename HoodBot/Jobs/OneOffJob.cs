2486587824b4f108ea58770080507a90cd4a7b6d
Robin Hood <robinhood70@live.ca>
2020-02-23 16:04:36 -0500

    Set mod to Dragonborn for Game Books

--- HoodBot/Jobs/OneOffJob.cs
+++ HoodBot/Jobs/OneOffJob.cs
@@ -1,73 +1,61 @@
 ﻿namespace RobinHood70.HoodBot.Jobs
 {
+	using System;
+	using System.Collections.Generic;
 	using RobinHood70.HoodBot.Jobs.Design;
-	using RobinHood70.HoodBot.Jobs.JobModels;
+	using RobinHood70.HoodBot.Parser;
+	using RobinHood70.HoodBot.Uesp;
 	using RobinHood70.Robby;
+	using RobinHood70.Robby.Design;
 	using RobinHood70.WikiClasses.Parser;
 	using RobinHood70.WikiCommon;
+	using static WikiCommon.Globals;
 
-	public class OneOffJob : EditJob
+	public class OneOffJob : ParsedPageJob
 	{
 		#region Constructors
 		[JobInfo("One-Off Job")]
 		public OneOffJob([ValidatedNotNull] Site site, AsyncInfo asyncInfo)
-			: base(site, asyncInfo) => this.Pages.SetLimitations(LimitationType.Remove, MediaWikiNamespaces.User);
+			: base(site, asyncInfo)
+		{
+		}
 		#endregion
 
-		#region Public Override Properties
-		public override string LogName => "One-Off Job";
+		#region Protected Override Properties
+		protected override string EditSummary => "Add mod parameter";
+
+		protected override void LoadPages()
+		{
+			var srGameBooks = new TitleCollection(this.Site);
+			srGameBooks.GetBacklinks("Template:Game Book", BacklinksTypes.EmbeddedIn, false, Filter.Exclude, UespNamespaces.Skyrim);
+			srGameBooks.GetBacklinks("Template:Game Book Compilation", BacklinksTypes.EmbeddedIn, false, Filter.Exclude, UespNamespaces.Skyrim);
+
+			var mhTitles = new TitleCollection(this.Site);
+			mhTitles.GetBacklinks("Template:Mod Header", BacklinksTypes.EmbeddedIn, false, Filter.Exclude, UespNamespaces.Skyrim);
+
+			var combined = new HashSet<Title>(mhTitles, SimpleTitleEqualityComparer.Instance);
+			combined.IntersectWith(srGameBooks);
+			this.Pages.GetTitles(combined);
+		}
 		#endregion
 
 		#region Protected Override Methods
-		protected override void BeforeLogging()
+		protected override void ParseText(object sender, Page page, ContextualParser parsedPage)
 		{
-			this.Pages.PageLoaded += this.Results_PageLoaded;
-			this.Pages.GetBacklinks("Template:Mod Icon Data", BacklinksTypes.EmbeddedIn);
-			this.Pages.Sort();
-			this.Pages.PageLoaded -= this.Results_PageLoaded;
-		}
-
-		protected override void Main() => this.SavePages("Work around mod header issue on save");
-		#endregion
-
-		#region Private Methods
-		private void Results_PageLoaded(object sender, Page page)
-		{
-			var parsedText = WikiTextParser.Parse(page.Text);
-			var modIconData = parsedText.FindFirstLinked<TemplateNode>(item => item.BacklinkTitleToParts(this.Site).PageName == "Mod Icon Data");
-			var modHeader = parsedText.FindFirst<TemplateNode>(item => item.BacklinkTitleToParts(this.Site).PageName == "Mod Header");
-			if (modIconData != null && modHeader != null)
+			ThrowNull(page, nameof(page));
+			ThrowNull(parsedPage, nameof(parsedPage));
+			if ((parsedPage.FindFirst<TemplateNode>(template => template.GetTitleValue() == "Mod Header") is TemplateNode modHeader) && (modHeader.FindNumberedParameter(1)?.ValueToText() == "Dragonborn"))
 			{
-				var modHeaderNameRaw = modHeader.FindNumberedParameter(1);
-				var modHeaderName = modHeaderNameRaw == null ? null : WikiTextVisitor.Value(modHeaderNameRaw.Value);
-				if (modIconData.Value is TemplateNode modIconTemplate && modHeaderName == page.PageName)
+				var gameBookTemplate = parsedPage.FindFirst<TemplateNode>(template =>
 				{
-					if (modIconData.Previous?.Value is TextNode textNode && textNode.Text[^1] == '\n')
-					{
-						textNode.Text = textNode.Text[0..^1].TrimEnd(' ');
-					}
-
-					parsedText.Remove(modIconData);
-					var icon = modIconTemplate.FindNumberedParameter(1);
-					if (icon != null)
-					{
-						icon.SetName("icon");
-						modHeader.Parameters.AddLast(icon);
-					}
-
-					var iconSize = modIconTemplate.FindNumberedParameter(2);
-					if (iconSize != null)
-					{
-						var sizeValue = WikiTextVisitor.Raw(iconSize.Value);
-						if (sizeValue != "32" && sizeValue != "x32")
-						{
-							iconSize.SetName("iconSize");
-							modHeader.Parameters.AddLast(iconSize);
-						}
-					}
+					var name = template.GetTitleValue();
+					return name.StartsWith("Game Book", StringComparison.OrdinalIgnoreCase);
+				});
+				if (gameBookTemplate != null)
+				{
+					gameBookTemplate.Parameters.AddFirst(ParameterNode.FromParts("smod", "DB\n"));
+					gameBookTemplate.Parameters.AddFirst(ParameterNode.FromParts("mod", "[[Skyrim:Dragonborn|Dragonborn]]\n"));
 				}
-
-				page.Text = WikiTextVisitor.Raw(parsedText);
 			}
 		}
 		#endregion
