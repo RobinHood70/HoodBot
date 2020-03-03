bd9d8d0f09fdbfe8d15df4867445e9b993661a8b
Robin Hood <robinhood70@live.ca>
2020-03-02 19:29:03 -0500

    Add mod parameter to NPCs

--- HoodBot/Jobs/OneOffJob.cs
+++ HoodBot/Jobs/OneOffJob.cs
@@ -1,8 +1,11 @@
 ﻿namespace RobinHood70.HoodBot.Jobs
 {
+	using System.Collections.Generic;
 	using RobinHood70.HoodBot.Jobs.Design;
 	using RobinHood70.HoodBot.Parser;
+	using RobinHood70.HoodBot.Uesp;
 	using RobinHood70.Robby;
+	using RobinHood70.Robby.Design;
 	using RobinHood70.WikiClasses.Parser;
 	using RobinHood70.WikiCommon;
 	using static WikiCommon.Globals;
@@ -17,29 +20,46 @@
 		}
 		#endregion
 
-		#region Public Override Properties
-		public override string? LogDetails => "Map Link update for Dragonborn links";
-		#endregion
-
 		#region Protected Override Properties
-		protected override string EditSummary => "Rename parameter for changes in template";
+		protected override string EditSummary => "Add mod parameter";
+
+		protected override void LoadPages()
+		{
+			var srGameBooks = new TitleCollection(this.Site);
+			srGameBooks.GetBacklinks("Template:NPC Summary", BacklinksTypes.EmbeddedIn, false, Filter.Exclude, UespNamespaces.Skyrim);
+
+			var mhTitles = new TitleCollection(this.Site);
+			mhTitles.GetBacklinks("Template:Mod Header", BacklinksTypes.EmbeddedIn, false, Filter.Exclude, UespNamespaces.Skyrim);
+
+			var combined = new HashSet<Title>(mhTitles, SimpleTitleEqualityComparer.Instance);
+			combined.IntersectWith(srGameBooks);
+			this.Pages.GetTitles(combined);
+		}
 		#endregion
 
 		#region Protected Override Methods
-		protected override void LoadPages() => this.Pages.GetBacklinks("Template:Map Link", BacklinksTypes.EmbeddedIn, false, Filter.Any);
-
 		protected override void ParseText(object sender, Page page, ContextualParser parsedPage)
 		{
 			ThrowNull(page, nameof(page));
 			ThrowNull(parsedPage, nameof(parsedPage));
-			foreach (var node in parsedPage.FindAllRecursive<TemplateNode>(template => template.GetTitleValue() == "Map Link"))
+			if (parsedPage.FindFirst<TemplateNode>(template => template.GetTitleValue() == "Mod Header") is TemplateNode modHeader)
 			{
-				if (node.FindParameter("ns_base") is ParameterNode parameter)
+				foreach (var npcSummary in parsedPage.FindAll<TemplateNode>(template => template.GetTitleValue() == "NPC Summary"))
 				{
-					var value = parameter.ValueToText()?.Trim();
-					if (value == "DB" || value == "Dragonborn")
+					if (npcSummary.FindParameter("mod") == null)
 					{
-						parameter.SetName("map");
+						switch (modHeader.FindNumberedParameter(1)?.ValueToText())
+						{
+							case "Dragonborn":
+								npcSummary.AddParameter("mod", "[[Skyrim:Dragonborn|Dragonborn]]", true);
+								break;
+							case "Dawnguard":
+								npcSummary.AddParameter("mod", "[[Skyrim:Dawnguard|Dawnguard]]", true);
+								break;
+							case "Hearthfire":
+								npcSummary.AddParameter("mod", "[[Skyrim:Hearthfire|Hearthfire]]", true);
+								break;
+						}
 					}
 				}
 			}
