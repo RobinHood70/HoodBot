e713c1430932931ebcce70f27b00eab3b5c1bd00
Robin Hood <robinhood70@live.ca>
2020-04-10 16:31:06 -0400

    New jobs

---
+++ HoodBot/Jobs/DBMergeSectionLinks.cs
@@ -0,0 +1,66 @@
+﻿namespace RobinHood70.HoodBot.Jobs
+{
+	using System;
+	using System.Collections.Generic;
+	using System.IO;
+	using Newtonsoft.Json;
+	using RobinHood70.HoodBot.Jobs.Design;
+	using RobinHood70.HoodBot.Jobs.JobModels;
+	using RobinHood70.Robby;
+	using RobinHood70.Robby.Design;
+	using RobinHood70.WikiCommon.Parser;
+
+	public class DBMergeSectionLinks : WikiJob
+	{
+		[JobInfo("Check Dragonborn Section Links")]
+		public DBMergeSectionLinks(Site site, AsyncInfo asyncInfo)
+			: base(site, asyncInfo)
+		{
+		}
+
+		protected override void Main()
+		{
+			var titles = new TitleCollection(this.Site);
+			var titleConverter = new ISimpleTitleJsonConverter(this.Site);
+			var repFile = File.ReadAllText(Environment.ExpandEnvironmentVariables(@"%BotData%\Replacements - Merge.json"));
+			var reps = JsonConvert.DeserializeObject<IEnumerable<Replacement>>(repFile, titleConverter) ?? throw new InvalidOperationException();
+			foreach (var rep in reps)
+			{
+				titles.Add(rep.To);
+			}
+
+			var pages = new PageCollection(this.Site, PageModules.LinksHere);
+			pages.GetTitles(titles);
+
+			var backlinks = new TitleCollection(this.Site);
+			foreach (var page in pages)
+			{
+				foreach (var item in page.Backlinks)
+				{
+					backlinks.Add(item.Key);
+				}
+			}
+
+			backlinks.Sort();
+			backlinks.Remove("User:HoodBot/Results");
+
+			pages = PageCollection.Unlimited(this.Site);
+			//// pages.GetTitles("Category:Dragonborn-Factions-DLC2AshSpawnFaction");
+			pages.GetTitles(backlinks);
+			pages.Sort();
+			foreach (var page in pages)
+			{
+				var parser = WikiTextParser.Parse(page.Text);
+				foreach (var node in parser.FindAllRecursive<LinkNode>())
+				{
+					var linkTitle = WikiTextVisitor.Value(node.Title);
+					var link = new FullTitle(this.Site, linkTitle);
+					if (link.Fragment == "Dragonborn" && titles.Contains(link))
+					{
+						this.WriteLine($"* {page.AsLink(false)}: {WikiTextVisitor.Raw(node)}");
+					}
+				}
+			}
+		}
+	}
+}
---
+++ HoodBot/Jobs/SmallImages.cs
@@ -0,0 +1,28 @@
+﻿namespace RobinHood70.HoodBot.Jobs
+{
+	using RobinHood70.HoodBot.Jobs.Design;
+	using RobinHood70.Robby;
+	using RobinHood70.Robby.Design;
+
+	public class SmallImages : WikiJob
+	{
+		[JobInfo("Find Small Images")]
+		public SmallImages(Site site, AsyncInfo asyncInfo)
+			: base(site, asyncInfo)
+		{
+		}
+
+		protected override void Main()
+		{
+			var files = new PageCollection(this.Site, PageModules.FileInfo);
+			files.GetCategoryMembers("Category:Legends-Art", true);
+			foreach (var page in files)
+			{
+				if (page is FilePage file && file.LatestFileRevision is FileRevision rev && rev.Height <= 512 && rev.Width <= 512)
+				{
+					this.WriteLine($"* {file.AsLink(true)}: {rev.Width} × {rev.Height}");
+				}
+			}
+		}
+	}
+}
