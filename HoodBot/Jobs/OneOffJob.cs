1b7a2657f6fe275dd3c1276b8aa1229e6ce8716c
Robin Hood <robinhood70@live.ca>
2020-05-31 20:14:12 -0400

    Date retcon job

--- HoodBot/Jobs/OneOffJob.cs
+++ HoodBot/Jobs/OneOffJob.cs
@@ -1,54 +1,32 @@
 ﻿namespace RobinHood70.HoodBot.Jobs
 {
 	using System;
-	using System.Collections.Generic;
 	using System.Diagnostics.CodeAnalysis;
 	using RobinHood70.CommonCode;
 	using RobinHood70.HoodBot.Jobs.Design;
-	using RobinHood70.HoodBot.Parser;
+	using RobinHood70.HoodBot.Uesp;
 	using RobinHood70.Robby;
-	using RobinHood70.Robby.Design;
-	using RobinHood70.WikiCommon;
-	using RobinHood70.WikiCommon.Parser;
-	using static RobinHood70.CommonCode.Globals;
 
-	public class OneOffJob : ParsedPageJob
+	public class OneOffJob : EditJob
 	{
-		#region Fields
-		private readonly Dictionary<string, bool> headers = new Dictionary<string, bool>();
-		#endregion
-
 		#region Constructors
 		[JobInfo("One-Off Job")]
 		public OneOffJob([NotNull, ValidatedNotNull] Site site, AsyncInfo asyncInfo)
-			: base(site, asyncInfo)
-		{
-		}
-		#endregion
-
-		#region Protected Override Properties
-		protected override string EditSummary => "Remove redundant parameters";
+			: base(site, asyncInfo) => this.LogDetails = "Date retcon";
 		#endregion
 
 		#region Protected Override Methods
-		protected override void LoadPages() => this.Pages.GetBacklinks("Template:Quest Link", BacklinksTypes.EmbeddedIn);
-
-		protected override void ParseText(object sender, ContextualParser parsedPage)
+		protected override void BeforeLogging()
 		{
-			ThrowNull(parsedPage, nameof(parsedPage));
-			foreach (var questLink in parsedPage.FindAll<TemplateNode>(node => node.GetTitleValue() == "Quest Link"))
+			this.Pages.GetNamespace(UespNamespaces.Lore, Filter.Exclude);
+			this.Pages.Remove("Lore:Birds of Wrothgar (old)");
+			foreach (var page in this.Pages)
 			{
-				if (questLink.FindParameter("ns_base") is ParameterNode nsBase && parsedPage.Title is ISimpleTitle title && title.Namespace.Contains(nsBase.ValueToText() ?? throw new InvalidOperationException()))
-				{
-					questLink.RemoveParameter("ns_base");
-				}
-
-				if (questLink.FindParameter("mod")?.ValueToText() == "CC")
-				{
-					questLink.RemoveParameter("mod");
-				}
+				page.Text = page.Text.Replace("2E 583", "2E 582", StringComparison.OrdinalIgnoreCase);
 			}
 		}
+
+		protected override void Main() => this.SavePages("Date retcon");
 		#endregion
 	}
 }
