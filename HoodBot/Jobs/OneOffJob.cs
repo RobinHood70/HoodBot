e00a36df3c49ffa8ea5d5c9a208d7df71c56eb20
Robin Hood <robinhood70@live.ca>
2020-09-23 11:09:44 -0400

    Merge icon data into mod header where possible

--- HoodBot/Jobs/OneOffJob.cs
+++ HoodBot/Jobs/OneOffJob.cs
@@ -1,7 +1,11 @@
 ﻿namespace RobinHood70.HoodBot.Jobs
 {
+	using System.Collections.Generic;
 	using RobinHood70.HoodBot.Jobs.Design;
+	using RobinHood70.Robby;
+	using RobinHood70.Robby.Design;
 	using RobinHood70.Robby.Parser;
+	using RobinHood70.WikiCommon;
 	using RobinHood70.WikiCommon.Parser;
 	using static RobinHood70.CommonCode.Globals;
 
@@ -14,16 +18,30 @@
 		{
 		}
 
-		protected override string EditSummary => "Remove end parameter";
+		protected override string EditSummary => "Fix issue with Mod Header and Mod Icon Data on same page";
 
-		protected override void LoadPages() => this.Pages.GetBacklinks("Template:ESO Antiquity", WikiCommon.BacklinksTypes.EmbeddedIn);
+		protected override void LoadPages()
+		{
+			var iconData = new TitleCollection(this.Site);
+			iconData.GetBacklinks("Template:Mod Icon Data", BacklinksTypes.EmbeddedIn);
+			var modHeader = new TitleCollection(this.Site);
+			modHeader.GetBacklinks("Template:Mod Header", BacklinksTypes.EmbeddedIn);
+
+			var hashSet = new HashSet<ISimpleTitle>(iconData, SimpleTitleEqualityComparer.Instance);
+			hashSet.IntersectWith(modHeader);
+
+			this.Pages.GetTitles(hashSet);
+		}
 
 		protected override void ParseText(object sender, ContextualParser parsedPage)
 		{
 			ThrowNull(parsedPage, nameof(parsedPage));
-			foreach (var template in parsedPage.FindTemplates("ESO Antiquity"))
+			if (parsedPage.FindTemplate("Mod Icon Data") is ITemplateNode iconTemplate &&
+				parsedPage.FindTemplate("Mod Header") is ITemplateNode headerTemplate &&
+				iconTemplate.Find(1) is IParameterNode icon)
 			{
-				template.Remove("end");
+				headerTemplate.Add("icon", icon.ValueToText());
+				parsedPage.Nodes.Remove(iconTemplate);
 			}
 		}
 		#endregion
