1205f0566d1c69a8d7ecad92049e3698029eae43
Robin Hood <robinhood70@live.ca>
2020-12-13 03:16:54 -0500

    Remove unused JobType property

--- HoodBot/Jobs/Loggers/LogInfo.cs
+++ HoodBot/Jobs/Loggers/LogInfo.cs
@@ -1,28 +1,21 @@
 ﻿namespace RobinHood70.HoodBot.Jobs.Loggers
 {
-	using RobinHood70.HoodBot.Jobs;
-
 	/// <summary>This provides a basic log information class to be derived from and customized as needed by custom JobLogger implementations.</summary>
 	public class LogInfo
 	{
 		/// <summary>Initializes a new instance of the <see cref="LogInfo"/> class.</summary>
 		/// <param name="title">The title for the log entry.</param>
 		/// <param name="details">The details for the log entry (typically, job parameters or notes).</param>
-		/// <param name="readOnly">If set to <see langword="true"/>, indicates that the job is read-only.</param>
-		public LogInfo(string title, string? details, JobTypes jobType)
+		/// <param name="jobType">What type of job this represents.</param>
+		public LogInfo(string title, string? details)
 		{
 			this.Title = title;
 			this.Details = details;
-			this.JobType = jobType;
 		}
 
 		/// <summary>Gets any additional details about the log entry.</summary>
 		/// <value>The additional details.</value>
 		public string? Details { get; }
-
-		/// <summary>Gets a value indicating the attributes of the job.</summary>
-		/// <value>The .</value>
-		public JobTypes JobType { get; }
 
 		/// <summary>Gets the log title.</summary>
 		/// <value>The title.</value>
