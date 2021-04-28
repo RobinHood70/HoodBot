4bed401ce43a0c4cd70a496d4dd17a4d57992217
Robin Hood <robinhood70@live.ca>
2021-04-28 00:43:24 -0400

    Add regions; code tweak

--- Robby/Parser/SiteNodeFactory.cs
+++ Robby/Parser/SiteNodeFactory.cs
@@ -2,18 +2,27 @@
 {
 	using System.Collections.Generic;
 	using RobinHood70.WikiCommon.Parser;
+	using RobinHood70.WikiCommon.Parser.Basic;
 
 	/// <summary>A concrete factory for creating <see cref="IWikiNode"/>s to be added to a <see cref="NodeCollection"/>.</summary>
 	/// <seealso cref="IWikiNodeFactory" />
-	public class SiteNodeFactory : WikiCommon.Parser.Basic.WikiNodeFactory
+	public class SiteNodeFactory : WikiNodeFactory
 	{
+		#region Constructors
+
 		/// <summary>Initializes a new instance of the <see cref="SiteNodeFactory"/> class.</summary>
 		/// <param name="site">The <see cref="Site"/> value to be passed to those nodes that require it.</param>
 		public SiteNodeFactory(Site site) => this.Site = site;
+		#endregion
+
+		#region Public Properties
 
 		/// <summary>Gets the site to use for site-specific operations like title resolution.</summary>
 		/// <value>The site.</value>
 		public Site Site { get; }
+		#endregion
+
+		#region Public Override Methods
 
 		/// <inheritdoc/>
 		public override IArgumentNode ArgumentNode(IEnumerable<IWikiNode> name, IList<IParameterNode> defaultValue) =>
@@ -30,5 +39,6 @@
 		/// <inheritdoc/>
 		public override ITemplateNode TemplateNode(IEnumerable<IWikiNode> title, IList<IParameterNode> parameters) =>
 			new SiteTemplateNode(this, title, parameters);
+		#endregion
 	}
 }
