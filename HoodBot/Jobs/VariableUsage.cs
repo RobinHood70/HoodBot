16f733b08df57b722db0252b1407df562977cdc3
Robin Hood <robinhood70@live.ca>
2021-05-04 14:11:17 -0400

    New job to find all loads/saves

---
+++ HoodBot/Jobs/VariableUsage.cs
@@ -0,0 +1,199 @@
+﻿namespace RobinHood70.HoodBot.Jobs
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Diagnostics;
+	using RobinHood70.CommonCode;
+	using RobinHood70.HoodBot.Uesp;
+	using RobinHood70.Robby;
+	using RobinHood70.Robby.Design;
+	using RobinHood70.WikiCommon;
+	using RobinHood70.WikiCommon.Parser;
+	using RobinHood70.WikiCommon.Parser.Basic;
+
+	public class VariableUsage : WikiJob
+	{
+		#region Fields
+		private readonly List<LoadSaveCall> loadCalls = new();
+		private readonly List<LoadSaveCall> saveCalls = new();
+		#endregion
+
+		#region Constructors
+		[JobInfo("Variable Usage")]
+		public VariableUsage(JobManager jobManager)
+			: base(jobManager)
+		{
+		}
+		#endregion
+
+		#region Protected Override Methods
+		protected override void Main()
+		{
+			this.StatusWriteLine("Loading pages");
+			this.ProgressMaximum = 2;
+			var results = PageCollection.Unlimited(this.Site);
+			results.GetNamespace(MediaWikiNamespaces.Template);
+			this.Progress++;
+			this.StatusWriteLine("Exporting");
+			this.GetCalls(results);
+			this.ExportResults();
+			this.Progress++;
+		}
+		#endregion
+
+		#region Private Methods
+		private void ExportResults()
+		{
+			try
+			{
+				this.WriteFile(true);
+				this.WriteFile(false);
+				this.StatusWriteLine("Files saved to bot folder.");
+			}
+			catch (System.IO.IOException e)
+			{
+				this.StatusWriteLine("Couldn't save files to bot folder.");
+				this.StatusWriteLine(e.Message);
+			}
+		}
+
+		private void GetCalls(PageCollection pages)
+		{
+			var paramTranslator = new Dictionary<string, string>(StringComparer.Ordinal); // TODO: Empty dictionary for now, but could be pre-populated to translate synonyms to a consistent name. Similarly, name comparison can be case-sensitive or not. Need to find a useful way to do those.
+			foreach (var page in pages)
+			{
+				var nodes = new WikiNodeFactory().Parse(page.Text);
+				foreach (var template in nodes.FindAll<ITemplateNode>())
+				{
+					var name = template.GetTitleText();
+					if (name.StartsWith("#load", StringComparison.OrdinalIgnoreCase))
+					{
+						this.loadCalls.Add(new LoadSaveCall(page, template));
+					}
+					else if (name.StartsWith("#save", StringComparison.OrdinalIgnoreCase))
+					{
+						this.saveCalls.Add(new LoadSaveCall(page, template));
+					}
+				}
+			}
+
+			var comparer = SimpleTitleComparer.Instance;
+			this.loadCalls.Sort((x, y) => comparer.Compare(x.Page, y.Page));
+			this.saveCalls.Sort((x, y) => comparer.Compare(x.Page, y.Page));
+		}
+
+		private void WriteFile(bool isLoadCall)
+		{
+			var csvFile = new CsvFile();
+			//// csvFile.EmptyFieldText = " ";
+
+			var list = isLoadCall ? this.loadCalls : this.saveCalls;
+			var maxCount = 0;
+			foreach (var call in list)
+			{
+				if (call.Variables.Count > maxCount)
+				{
+					maxCount = call.Variables.Count;
+				}
+			}
+
+			var header = new List<string>(maxCount + 3)
+			{
+				"Template Name"
+			};
+
+			if (isLoadCall)
+			{
+				header.Add("Load Page");
+			}
+
+			header.Add("Subset");
+
+			for (var i = 1; i <= maxCount; i++)
+			{
+				header.Add("Variable " + i.ToStringInvariant());
+			}
+
+			csvFile.Header = header;
+
+			foreach (var call in list)
+			{
+				var cells = new List<string>(call.Variables.Count + 3)
+				{
+					call.Page.PageName
+				};
+
+				if (isLoadCall)
+				{
+					cells.Add(call.LoadPage);
+				}
+
+				cells.Add(call.Subset);
+				cells.AddRange(call.Variables);
+				csvFile.Add(cells);
+			}
+
+			var type = isLoadCall ? "Loaded" : "Saved";
+			Debug.WriteLine($"{type}: {csvFile.Count} rows");
+			csvFile.WriteFile(UespSite.GetBotDataFolder(type + " Variables.txt"));
+		}
+		#endregion
+
+		#region Private Classes
+		private sealed class LoadSaveCall
+		{
+			public LoadSaveCall(ISimpleTitle page, ITemplateNode loadSave)
+			{
+				var variables = new List<string>();
+				this.Page = page;
+				var title = loadSave.Title.ToRaw();
+				var split = title.Split(TextArrays.Colon, 2);
+				var name = split[0];
+				var first = split[1];
+				if (string.Equals(name, "#load", StringComparison.OrdinalIgnoreCase))
+				{
+					this.LoadPage = first;
+				}
+				else
+				{
+					variables.Add(first);
+				}
+
+				foreach (var param in loadSave.Parameters)
+				{
+					switch (param.Name?.ToValue())
+					{
+						case null:
+							variables.Add(param.Value.ToRaw());
+							break;
+						case "subset":
+							this.Subset = param.Value.ToRaw();
+							break;
+						case "if":
+						case "ifnot":
+							break;
+						default:
+							var fullParam = WikiTextVisitor.Raw(param);
+							if (fullParam.Length > 0)
+							{
+								variables.Add(fullParam[1..]);
+							}
+
+							break;
+					}
+				}
+
+				this.Variables = variables;
+			}
+
+			public string LoadPage { get; } = string.Empty;
+
+			public ISimpleTitle Page { get; }
+
+			public string Subset { get; } = string.Empty;
+
+			public IReadOnlyList<string> Variables { get; }
+		}
+		#endregion
+	}
+}
