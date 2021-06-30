1d75e5ff73f874869b1578f82c1cabb0e478a8ab
Robin Hood <robinhood70@live.ca>
2021-06-30 10:29:29 -0400

    Make LinkFinder a job type

---
+++ HoodBot/Jobs/LinkFinderEsoCollections.cs
@@ -0,0 +1,25 @@
+﻿namespace RobinHood70.HoodBot.Jobs
+{
+	using RobinHood70.HoodBot.Uesp;
+	using RobinHood70.Robby;
+
+	public class LinkFinderEsoCollections : LinkFinderJob
+	{
+		#region Constructors
+		[JobInfo("Collections Link Finder", "ESO")]
+		public LinkFinderEsoCollections(JobManager jobManager)
+			: base(jobManager, true)
+		{
+		}
+		#endregion
+
+		#region Protected Override Methods
+		protected override void LoadPages()
+		{
+			var titles = new TitleCollection(this.Site, UespNamespaces.Online, "Assistants", "Body Markings", "Collectible Emotes", "Collectible Furnishings", "Costumes", "Facial Hair", "Fragments", "Hair Styles", "Hats(collectible)", "Head Markings", "Houseguests", "Major Adornments", "Mementos (collection)", "Minor Adornments", "Mounts", "Personalities", "Pets", "Polymorphs", "Skins");
+			this.SetTitlesFromSubpages(titles);
+			base.LoadPages();
+		}
+		#endregion
+	}
+}
