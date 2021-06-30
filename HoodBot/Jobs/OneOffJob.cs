3294a74f491ca93837f6e862673ef1069332cbed
Robin Hood <robinhood70@live.ca>
2021-06-30 10:58:31 -0400

    One-offs

--- HoodBot/Jobs/OneOffJob.cs
+++ HoodBot/Jobs/OneOffJob.cs
@@ -3,58 +3,16 @@
 	using System;
 	using System.Collections.Generic;
 	using System.Diagnostics;
-	using RobinHood70.CommonCode;
+	using System.Text.RegularExpressions;
+	using RobinHood70.HoodBot.Uesp;
 	using RobinHood70.Robby;
-	using RobinHood70.Robby.Design;
-	using RobinHood70.Robby.Parser;
-	using RobinHood70.WikiCommon;
-	using RobinHood70.WikiCommon.Parser;
+	using static RobinHood70.CommonCode.Globals;
 
 	public class OneOffJob : EditJob
 	{
-		private static readonly HashSet<string> TestFiles = new(StringComparer.Ordinal)
-		{
-			"File:ON-creature-Blue Oasis Dragon Frog.jpg",
-			"File:ON-creature-Cockroach.jpg",
-			"File:ON-creature-Dread Lich.jpg",
-			"File:ON-creature-Spider Hatchling.jpg",
-			"File:ON-creature-Tangerine Dragon Frog.jpg",
-			"File:ON-creature-Thodundor of the Hill 02.jpg",
-			"File:ON-creature-Winter Moth.jpg",
-			"File:ON-interior-Arbogasque's Home 02.jpg",
-			"File:ON-interior-Arbogasque's Home.jpg",
-			"File:ON-interior-Enthonor's House.jpg",
-			"File:ON-interior-Ramimilk 02.jpg",
-			"File:ON-interior-Ramimilk 03.jpg",
-			"File:ON-interior-Uveran Bank.jpg",
-			"File:ON-interior-West Tower.jpg",
-			"File:ON-npc-Conjured Reflection.jpg",
-			"File:ON-npc-Constable Ebarah.jpg",
-			"File:ON-npc-Crimson Knight.jpg",
-			"File:ON-npc-Dremora Harstryl 02.jpg",
-			"File:ON-npc-Fenteladir.jpg",
-			"File:ON-npc-Guild Mage.jpg",
-			"File:ON-npc-Highland Enforcer.jpg",
-			"File:ON-npc-Imbued Corpse.jpg",
-			"File:ON-npc-Jackdaw Bravo.jpg",
-			"File:ON-npc-Lady Minara.jpg",
-			"File:ON-npc-Manor Guest 03.jpg",
-			"File:ON-npc-Moon-Sentinel Guardian.jpg",
-			"File:ON-npc-Plague of Crows.jpg",
-			"File:ON-npc-Root Guard.jpg",
-			"File:ON-npc-Ruddy Fang Swashbuckler.jpg",
-			"File:ON-npc-Sahban 02.jpg",
-			"File:ON-npc-Scavenger Thunder-Smith.jpg",
-			"File:ON-npc-Sharp Stick Ravager.jpg",
-			"File:ON-npc-Sleeps-Beneath-Himself.jpg",
-			"File:ON-npc-Worm Cult Assassin 02.jpg",
-			"File:ON-npc-Zadazi 02.jpg",
-			"File:ON-place-Dead-Water Wayshrine.jpg",
-			"File:ON-place-Hatching Pools 02.jpg",
-			"File:ON-place-Skeleton Camps.jpg",
-			"File:ON-place-White Fall Giant Camp.jpg",
-			"File:ON-quest-Death Among the Dead-Water 04.jpg"
-		};
+		#region Static Fields
+		private static readonly Regex RowFinder = new(@"\|-\ *\n[!\|]\ *\[\[(?<icon>.*?)(?<iconSize>\|.*?)?\]\]\ *\n[!\|]\ *\{\{Anchor\|(?<itemName>.*?)\}\}\ *(\|\||<br>)\ *(\{\{Small\|)?(?<id>.*)(\}\})?\n\|\ *(?<weight>.*?)\ *\|\|\ *(?<value>.*?)\ *\n\|(\{\{AL\|L\}\}\|)?(?<desc>.*?)\n(\|\ *\[\[(?<image>.*?)(?<imageSize>\|.*?)?\]\]\ *\n)?", RegexOptions.ExplicitCapture, DefaultRegexTimeout);
+		#endregion
 
 		#region Constructors
 		[JobInfo("One-Off Job")]
@@ -67,93 +25,48 @@
 		#region Protected Override Methods
 		protected override void BeforeLogging()
 		{
-			var loadOptions = new PageLoadOptions(PageModules.FileInfo)
+			var pageText = this.Site.LoadPageText("Morrowind:Miscellaneous Items");
+			if (pageText != null)
 			{
-				FileRevisionCount = 1000
-			};
-
-			var pages = new PageCollection(this.Site, loadOptions);
-			pages.GetNamespace(MediaWikiNamespaces.File, Filter.Any, "ON-place");
-			pages.GetNamespace(MediaWikiNamespaces.File, Filter.Any, "ON-npc");
-			pages.GetNamespace(MediaWikiNamespaces.File, Filter.Any, "ON-creature");
-			pages.GetNamespace(MediaWikiNamespaces.File, Filter.Any, "ON-interior");
-			pages.GetNamespace(MediaWikiNamespaces.File, Filter.Any, "ON-misc");
-			pages.GetNamespace(MediaWikiNamespaces.File, Filter.Any, "ON-item");
-			pages.GetNamespace(MediaWikiNamespaces.File, Filter.Any, "ON-object");
-			pages.GetNamespace(MediaWikiNamespaces.File, Filter.Any, "ON-quest");
-
-			var august2020 = new DateTime(2020, 8, 1);
-			var sept2020 = new DateTime(2020, 9, 1);
-			var titles = new TitleCollection(this.Site);
-			foreach (var page in pages)
-			{
-				if (page is FilePage filePage &&
-					filePage.FileRevisions.Count > 0 &&
-					filePage.FileRevisions[filePage.FileRevisions.Count - 1] is var firstRev &&
-					string.Equals(firstRev.User, "MolagBallet", StringComparison.Ordinal) &&
-					firstRev.Timestamp < august2020)
+				pageText = pageText
+					.Replace("{{sic|Dewmer|Dwemer|nolink=1}}", "Dewmer", StringComparison.OrdinalIgnoreCase)
+					.Replace("{{Anchor|The Head Of Scourge|The Head of Scourge}}", "{{Anchor|The Head Of Scourge}}", StringComparison.OrdinalIgnoreCase);
+				var matches = (IReadOnlyCollection<Match>)RowFinder.Matches(pageText);
+				Debug.WriteLine(matches.Count);
+				foreach (var match in matches)
 				{
-					var add = true;
-					if (filePage.FileRevisions.Count > 1)
+					var groups = match.Groups;
+					var pageName = groups["itemName"].Value;
+					Debug.WriteLine("Match: " + match.Value);
+					var title = new Title(this.Site.Namespaces[UespNamespaces.Morrowind], pageName);
+					if (!this.Pages.TryGetValue(title, out var page))
 					{
-						foreach (var rev in filePage.FileRevisions)
-						{
-							bool isMolags = string.Equals(rev.User, "MolagBallet", StringComparison.Ordinal);
-							if (rev.Timestamp >= sept2020 && isMolags)
-							{
-								add = false;
-							}
-							else if (!isMolags)
-							{
-								this.StatusWriteLine($"{filePage.FullPageName} was updated by {rev.User} on {rev.Timestamp}, size: {rev.Width} x {rev.Height}");
-							}
-						}
+						page = new Page(title);
+						this.Pages.Add(page);
+					}
+					else
+					{
+						page.Text += "\n{{NewLine}}\n";
 					}
 
-					if (add)
-					{
-						titles.Add(filePage);
-					}
+					page.Text += "{{Item Summary" +
+						"\n|objectid=" + groups["id"].Value +
+						"\n|icon=" + Title.FromName(this.Site, groups["icon"].Value).PageName +
+						"\n|image=" + Title.FromName(this.Site, groups["image"].Value).PageName +
+						"\n|weight=" + groups["weight"].Value +
+						"\n|value=" + groups["value"].Value +
+						"\n}}" +
+						"\n" + groups["desc"].Value;
 				}
 			}
 
-			this.Pages.GetTitles(titles);
 			foreach (var page in this.Pages)
 			{
-				var parser = new ContextualParser(page);
-				if ((parser.FindTemplate("CleanImage") ?? parser.FindTemplate("Cleanimage")) is not SiteTemplateNode templateNode)
-				{
-					var first = parser.Nodes[0];
-					templateNode = (SiteTemplateNode)parser.Nodes.Factory.TemplateNodeFromParts("CleanImage");
-					parser.Nodes.Insert(0, templateNode);
-					if (first is IHeaderNode)
-					{
-						parser.Nodes.Insert(1, parser.Nodes.Factory.TextNode("\n"));
-					}
-					else if (first is not null)
-					{
-						Debug.WriteLine(page.FullPageName);
-					}
-				}
-
-				if (templateNode.Find("res") is not IParameterNode resParam)
-				{
-					resParam = templateNode.Add("res", string.Empty);
-				}
-
-				var value = resParam.Value.ToValue();
-				if (!value.Contains("too low", StringComparison.OrdinalIgnoreCase))
-				{
-					if (value.Length == 0)
-					{
-						resParam.Value.AddText("too low");
-						page.Text = parser.ToRaw();
-					}
-				}
+				page.Text = "{{Trail|Items}}{{Minimal}}\n" + page.Text;
 			}
 		}
 
-		protected override void Main() => this.SavePages("Add low-resolution tag, per MolagBallet");
+		protected override void Main() => this.SavePages("Create item page", false);
 		#endregion
 	}
 }
--- HoodBot/Jobs/OneOffParseJob.cs
+++ HoodBot/Jobs/OneOffParseJob.cs
@@ -1,71 +1,35 @@
 ﻿namespace RobinHood70.HoodBot.Jobs
 {
 	using System;
-	using System.Text.RegularExpressions;
-	using RobinHood70.CommonCode;
-	using RobinHood70.HoodBot.Uesp;
-	using RobinHood70.Robby;
+	using RobinHood70.Robby.Design;
 	using RobinHood70.Robby.Parser;
 	using RobinHood70.WikiCommon.Parser;
 
 	public class OneOffParseJob : ParsedPageJob
 	{
-		#region Static Fields
-		private static readonly Regex JournalTable = new(@"\{\|.*?\|-\n.*?\|-\n", RegexOptions.Singleline | RegexOptions.ExplicitCapture, Globals.DefaultRegexTimeout);
-		#endregion
-
 		#region Constructors
 		[JobInfo("One-Off Parse Job")]
 		public OneOffParseJob(JobManager jobManager)
-			: base(jobManager) => this.MinorEdit = false;
+			: base(jobManager)
+		{
+		}
 		#endregion
 
 		#region Protected Override Properties
-		protected override string EditSummary => "Convert to template (bot-assisted)";
+		protected override string EditSummary => "Remove redundant savetitlename";
 		#endregion
 
 		#region Protected Override Methods
-		protected override void LoadPages()
-		{
-			this.Pages.SetLimitations(LimitationType.FilterTo, UespNamespaces.BetterCities, UespNamespaces.OblivionMod);
-			this.Pages.GetBacklinks("Template:OB Journal Entries Notes");
-		}
+		protected override void LoadPages() => this.Pages.GetBacklinks("Template:Online Achievement Summary");
 
 		protected override void ParseText(object sender, ContextualParser parsedPage)
 		{
-			var header = parsedPage.IndexOfHeader("Journal Entries");
-			var nodes = parsedPage.Nodes;
-			var journalText = WikiTextVisitor.Raw(nodes.GetRange(header + 1, nodes.Count - header - 1));
-			if (JournalTable.Match(journalText) is Match journalMatch && journalMatch.Success)
+			if (parsedPage.FindTemplate("Online Collectible Summary") is SiteTemplateNode template &&
+				template.Find("titlename") is IParameterNode title &&
+				string.Equals(title.Value.ToValue().Trim(), parsedPage.Context.LabelName(), StringComparison.Ordinal))
 			{
-				journalText = journalText[..journalMatch.Index] + "{{Journal Entries\n" + journalText[(journalMatch.Index + journalMatch.Length)..];
-				journalText = journalText
-					.Replace("|style=\"text-align:center;\"|", string.Empty, StringComparison.OrdinalIgnoreCase)
-					.Replace("|-\n", string.Empty, StringComparison.Ordinal)
-					.Replace("[[File:Check.png|Finishes quest]]", "fin", StringComparison.OrdinalIgnoreCase)
-					.Replace("||", "|", StringComparison.Ordinal)
-					.Replace("|}", "}}", StringComparison.Ordinal)
-					.Replace("| ", "|", StringComparison.Ordinal)
-					.Replace(" |", "|", StringComparison.Ordinal)
-					.Replace("||", "|   |", StringComparison.Ordinal)
-					.Replace("\n\n{{OB", "\n{{OB", StringComparison.Ordinal);
-
-				for (var i = nodes.Count - 1; i > header; i--)
-				{
-					nodes.RemoveAt(i);
-				}
-
-				parsedPage.Nodes.AddRange(new SiteNodeFactory(this.Site).Parse(journalText));
+				template.Remove("titlename");
 			}
-		}
-
-		protected override void ResultsPageLoaded(object sender, Page page)
-		{
-			page.Text = page.Text
-				.Replace(" <br", "<br", StringComparison.OrdinalIgnoreCase)
-				.Replace("=\n\n", "=\n", StringComparison.Ordinal)
-				.Replace("\n\n\n", "\n\n", StringComparison.Ordinal);
-			base.ResultsPageLoaded(sender, page);
 		}
 		#endregion
 	}
