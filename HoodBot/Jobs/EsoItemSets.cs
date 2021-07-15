4e4230009ad3706a595295189f0774e4fb92f79a
Robin Hood <robinhood70@live.ca>
2021-07-15 14:25:20 -0400

    Rewrite

--- HoodBot/Jobs/EsoItemSets.cs
+++ HoodBot/Jobs/EsoItemSets.cs
@@ -2,6 +2,7 @@
 {
 	using System;
 	using System.Collections.Generic;
+	using System.Collections.ObjectModel;
 	using System.Globalization;
 	using System.Text;
 	using System.Text.RegularExpressions;
@@ -21,20 +22,20 @@
 	internal sealed class EsoItemSets : EditJob
 	{
 		#region Static Fields
-		private static readonly HashSet<int> BadRows = new() { 854 };
+		private static readonly HashSet<int> BadRows = new(); // None atm.
 		private static readonly Regex SetBonusRegex = new(@"\(\s*(?<items>[1-6] items?)\s*\)\s*(?<text>.*?)\s*(?=(\([1-6] items?\)|\z))", RegexOptions.ExplicitCapture | RegexOptions.Singleline, DefaultRegexTimeout);
 		private static readonly Uri SetSummaryPage = new("http://esolog.uesp.net/viewlog.php?record=setSummary&format=csv");
 		private static readonly Dictionary<string, string> TitleOverrides = new(StringComparer.Ordinal)
 		{
 			// Title Overrides should only be necessary when creating new disambiguated "(set)" pages or when pages don't conform to the base/base (set) style. While this could be done programatically, it's probably best not to, so that a human has verified that the page really should be created and that the existing page isn't malformed or something.
-			["Baron Zaudrus"] = "Baron Zaudrus (set)",
-			["Bloodspawn"] = "Bloodspawn (set)",
+			["Immolator Charr"] = "Immolator Charr (set)",
+			["Zoal the Ever-Wakeful"] = "Zoal the Ever-Wakeful (set)",
 		};
 		#endregion
 
 		#region Fields
-		private readonly IDictionary<string, PageData> sets = new SortedDictionary<string, PageData>(StringComparer.Ordinal);
 		private readonly IMediaWikiClient client;
+		private readonly KeyedCollection<Page, SetData> sets = new SetCollection();
 		#endregion
 
 		#region Constructors
@@ -50,11 +51,30 @@
 		#endregion
 
 		#region Protected Override Methods
-		protected override void Main()
+		protected override void BeforeLogging()
 		{
-			this.SavePages(this.LogName, false);
-			EsoGeneral.SetBotUpdateVersion(this, "itemset");
-			this.Progress++;
+			EsoReplacer.Initialize(this);
+
+			this.StatusWriteLine("Fetching data");
+			this.GetSetPages();
+			var titles = new TitleCollection(this.Site);
+			foreach (var set in this.sets)
+			{
+				if (set.Page is not null)
+				{
+					titles.Add(set.Page);
+				}
+				else
+				{
+					throw new InvalidOperationException($"The page for set {set.Name} is null!");
+				}
+			}
+
+			this.StatusWriteLine("Updating");
+			this.Pages.PageLoaded += this.SetLoaded;
+			this.Pages.GetTitles(titles);
+			this.Pages.PageLoaded -= this.SetLoaded;
+			this.GenerateReport();
 		}
 
 		protected override void JobCompleted()
@@ -63,211 +83,171 @@
 			base.JobCompleted();
 		}
 
-		protected override void BeforeLogging()
+		protected override void Main()
 		{
-			EsoReplacer.Initialize(this);
-			this.StatusWriteLine("Fetching data");
-			var setList = GetSetsFromCsv(this.client.Get(SetSummaryPage));
-			this.StatusWriteLine("Updating");
-			this.ResolveAndPopulateSets(setList);
-			var titles = new TitleCollection(this.Site);
-			foreach (var set in this.sets)
-			{
-				titles.Add(UespNamespaces.Online, set.Key);
-			}
-
-			this.Pages.PageLoaded += this.SetLoaded;
-			this.Pages.GetTitles(titles);
-			this.Pages.PageLoaded -= this.SetLoaded;
-			this.GenerateReport();
+			this.SavePages(this.LogName, false);
+			EsoGeneral.SetBotUpdateVersion(this, "itemset");
 		}
 		#endregion
 
-		#region Private Static Methods
-		private static string BuildNewPage(string setName)
+		#region Private Methods
+		private void BuildNewPages()
 		{
-			var sb = new StringBuilder();
-			sb
-				.Append("{{Trail|Sets}}{{Online Update}}{{Minimal}}\n")
-				.Append("'''").Append(setName).Append("''' is a {{huh}}-rank [[Online:Sets|item set]] found in {{huh}}.\n\n")
-				.Append("Set pieces are {{huh}} style in {{huh}}.\n\n")
-				.Append("===Bonuses===\n")
-				.Append("<onlyinclude>\n")
-				.Append("</onlyinclude>\n")
-				.Append("===Pieces===\n")
-				.Append("The set consists of {{huh}}. For detailed weapon stats and set values, see individual item pages by clicking on the links below.\n")
-				.Append("<!--\n")
-				.Append("* {{Item Link|id=|}}\n")
-				.Append("* {{Item Link|id=|}}\n")
-				.Append("-->\n")
-				.Append("===Notes===\n\n")
-				.Append("{{Online Sets}}\n")
-				.Append("{{ESO Sets With|subtype=|source=}}\n")
-				.Append("{{Stub|Item Set}}");
-
-			return sb.ToString();
+			foreach (var set in this.sets)
+			{
+				if (set.Page is null)
+				{
+					this.Warn($"New Page: {set.Name}");
+					set.BuildNewPage(new Title(this.Site.Namespaces[UespNamespaces.Online], set.Name));
+				}
+			}
 		}
 
-		private static List<PageData> GetSetsFromCsv(string csvData)
+		private void GenerateReport()
+		{
+			var sb = new StringBuilder();
+			foreach (var item in this.sets)
+			{
+				if (item.IsNonTrivial)
+				{
+					sb
+						.Append("* {{Pl|")
+						.Append(item.Page!.FullPageName)
+						.Append('|')
+						.Append(item.Name)
+						.AppendLine("|diff=cur}}");
+				}
+			}
+
+			if (sb.Length > 0)
+			{
+				this.WriteLine("== Item Sets With Non-Trivial Updates ==");
+				this.WriteLine(sb.ToString().Replace("\r", string.Empty, StringComparison.Ordinal));
+			}
+		}
+
+		private void GetSetPages()
+		{
+			this.GetSetsFromCsv();
+			this.MatchCategoryPages();
+			this.MatchUnresolvedPages();
+			this.BuildNewPages();
+		}
+
+		private void GetSetsFromCsv()
 		{
 			var csvFile = new CsvFile();
+			var csvData = this.client.Get(SetSummaryPage);
 			csvFile.ReadText(csvData, true);
-			var setList = new List<PageData>();
 			foreach (var row in csvFile)
 			{
 				if (!BadRows.Contains(int.Parse(row["id"], CultureInfo.InvariantCulture)))
 				{
 					var setName = row["setName"].Replace(@"\'", "'", StringComparison.Ordinal);
-					/* switch (setName)
-					{
-						case "Ancient Dragonguard":
-						case "Assassin's Guile":
-						case "Daring Corsair":
-						case "Grundwulf":
-						case "Innate Axiom":
-						case "New Moon Acolyte":
-						case "Sithis' Touch":
-						case "Slimecraw":
-							break;
-						default:
-							continue;
-					} */
-
 					var bonusDescription = row["setBonusDesc"];
 					if (bonusDescription[0] != '(')
 					{
 						throw new InvalidOperationException($"Set bonus for {setName} doesn't start with a bracket:{Environment.NewLine}{bonusDescription}");
 					}
 
-					setList.Add(new PageData(setName, bonusDescription));
+					this.sets.Add(new SetData(setName, bonusDescription));
 				}
 			}
-
-			return setList;
-		}
-		#endregion
-
-		#region Private Methods
-		private void GenerateReport()
-		{
-			this.WriteLine("== Item Sets With Non-Trivial Updates ==");
-			foreach (var item in this.sets)
-			{
-				if (item.Value.IsNonTrivial)
-				{
-					this.WriteLine($"* {{{{Pl|Online:{item.Key}|{item.Value.SetName}|diff=cur}}}}");
-				}
-			}
-
-			this.WriteLine();
 		}
 
-		private void ResolveAndPopulateSets(List<PageData> dbSets)
+		private void MatchCategoryPages()
 		{
-			var loadOptions = new PageLoadOptions(PageModules.Info | PageModules.Properties, true);
-			var catMembers = new PageCollection(this.Site, loadOptions);
-			catMembers.GetCategoryMembers("Online-Sets", CategoryMemberTypes.Page, false);
+			var catPages = new PageCollection(this.Site, new PageLoadOptions(PageModules.Info, true));
+			catPages.GetCategoryMembers("Online-Sets", CategoryMemberTypes.Page, false);
+			this.UpdateSetPages(catPages);
+		}
 
-			var catTitles = new TitleCollection(this.Site);
-			foreach (var set in dbSets)
+		private void MatchUnresolvedPages()
+		{
+			var titles = new TitleCollection(this.Site);
+			foreach (var set in this.sets)
 			{
-				var setTitle = new Title(this.Site.Namespaces[UespNamespaces.Online], set.PageName);
-				if (!catMembers.Contains(setTitle))
+				if (set.Page == null)
 				{
-					catTitles.Add(setTitle);
+					foreach (var setName in set.AllNames)
+					{
+						titles.Add(UespNamespaces.Online, setName);
+					}
 				}
 			}
 
-			if (catTitles.Count > 0)
+			this.ResolveDisambiguations(titles);
+		}
+
+		private void ResolveDisambiguations(TitleCollection newTitles)
+		{
+			if (newTitles.Count == 0)
 			{
-				catMembers.GetTitles(catTitles);
+				return;
 			}
 
-#if DEBUG
-			catMembers.Sort();
-#endif
-
-			var disambigs = new Dictionary<Title, PageData>();
-			foreach (var set in dbSets)
+			var removePages = new List<Page>();
+			var addTitles = new List<Title>();
+			var pages = new PageCollection(this.Site, new PageLoadOptions(PageModules.Info | PageModules.Links | PageModules.Properties, true));
+			pages.GetTitles(newTitles);
+			foreach (var page in pages)
 			{
-				var setTitle = new Title(this.Site.Namespaces[UespNamespaces.Online], set.PageName);
-				if (!catMembers.TryGetValue(setTitle.FullPageName + " (set)", out var foundPage) && !catMembers.TryGetValue(setTitle, out foundPage))
-				{
-					throw new InvalidOperationException();
-				}
-
-				set.PageName = foundPage.PageName;
-				if (foundPage.IsDisambiguation)
-				{
-					disambigs.Add(foundPage, set);
-				}
-				else
-				{
-					this.AddToSets(set);
-				}
-			}
-
-			loadOptions = new PageLoadOptions(PageModules.Links | PageModules.Properties, true);
-			var checkNewPages = new PageCollection(this.Site, loadOptions);
-			checkNewPages.GetTitles(disambigs.Keys);
-			foreach (var title in disambigs)
-			{
-				if (checkNewPages[title.Key.FullPageName] is Page page && page.Exists)
+				if (page.IsDisambiguation == true)
 				{
 					var resolved = false;
-					if (page.IsDisambiguation)
+					foreach (var link in page.Links)
 					{
-						foreach (var link in page.Links)
+						if (link.PageName.Contains(" (set)", StringComparison.OrdinalIgnoreCase))
 						{
-							if (link.PageName.Contains(" (set)", StringComparison.OrdinalIgnoreCase))
-							{
-								title.Value.PageName = link.PageName;
-								this.AddToSets(title.Value);
-								resolved = true;
-								break;
-							}
+							resolved = true;
+							removePages.Add(page);
+							addTitles.Add(link);
+							break;
 						}
 					}
 
 					if (!resolved)
 					{
-						this.Warn($"{page.FullPageName} exists but is neither a set nor a disambiguation to one. Please check!");
+						this.Warn($"{page.FullPageName} exists but is neither a set nor a disambiguation to one. Please check this and add the title to TitleOverrides to specify the desired page name.");
 					}
 				}
 			}
-		}
 
-		private void AddToSets(PageData set)
-		{
-			try
+			if (removePages.Count > 0)
 			{
-				this.sets.Add(set.PageName, set);
+				foreach (var page in removePages)
+				{
+					pages.Remove(page);
+				}
+
+				var addPages = new PageCollection(this.Site, PageModules.Info);
+				addPages.GetTitles(addTitles);
+				pages.Add(addPages);
 			}
-			catch (ArgumentException)
-			{
-				this.Warn($"Duplicate entry for {set.SetName} in raw data.");
-			}
+
+			this.UpdateSetPages(pages);
 		}
 
 		private void SetLoaded(object sender, Page page)
 		{
-			var pageData = this.sets[page.PageName];
+			ThrowNull(this.sets, nameof(EsoItemSets), nameof(this.sets));
+			var pageData = this.sets[page];
 			if (!page.Exists || string.IsNullOrEmpty(page.Text))
 			{
-				page.Text = BuildNewPage(pageData.SetName);
+				throw new InvalidOperationException();
 			}
 
 			this.UpdatePageText(page, pageData);
 		}
 
-		private void UpdatePageText(Page page, PageData pageData)
+		private void UpdatePageText(Page page, SetData pageData)
 		{
 			var oldPage = new ContextualParser(page, InclusionType.Transcluded, false);
 			if (oldPage.Nodes.Count < 2 || !(
 					oldPage.Nodes[0] is IIgnoreNode firstNode &&
 					oldPage.Nodes[^1] is IIgnoreNode lastNode))
 			{
-				this.Warn($"Delimiters not found on page {page.FullPageName}");
+				this.Warn($"Delimiters not found on page {page.FullPageName}\n");
 				return;
 			}
 
@@ -302,51 +282,157 @@
 			// Now that we're done parsing, re-add the IgnoreNodes.
 			newPage.Nodes.Insert(0, firstNode);
 			newPage.Nodes.Add(lastNode);
-			page.Text = newPage.ToRaw();
 
 			var replacer = new EsoReplacer(this.Site);
-			if (EsoReplacer.ConstructWarning(page, replacer.CheckNewLinks(oldPage, newPage), "links") is string linkWarning)
+			var newLinks = replacer.CheckNewLinks(oldPage, newPage);
+			if (newLinks.Count > 0)
 			{
-				this.Warn(linkWarning);
+				this.Warn(EsoReplacer.ConstructWarning(oldPage, newPage, newLinks, "links"));
 			}
 
-			if (EsoReplacer.ConstructWarning(page, replacer.CheckNewTemplates(oldPage, newPage), "templates") is string templateWarning)
+			var newTemplates = replacer.CheckNewTemplates(oldPage, newPage);
+			if (newTemplates.Count > 0)
 			{
-				this.Warn(templateWarning);
+				this.Warn(EsoReplacer.ConstructWarning(oldPage, newPage, newTemplates, "templates"));
 			}
 
 			pageData.IsNonTrivial = replacer.IsNonTrivialChange(oldPage, newPage);
+			page.Text = newPage.ToRaw();
+		}
+
+		private void UpdateSetPageAnyCase(SetData set, PageCollection setMembers)
+		{
+			foreach (var setName in set.AllNames)
+			{
+				foreach (var setMember in setMembers)
+				{
+					if (string.Compare(setMember.PageName, setName, true, this.Site.Culture) == 0)
+					{
+						this.Warn($"Substituted {setMember.PageName} for {setName}");
+						set.Page = setMember;
+						break;
+					}
+				}
+
+				if (set.Page is not null)
+				{
+					break;
+				}
+			}
+		}
+
+		private void UpdateSetPages(PageCollection setMembers)
+		{
+			foreach (var set in this.sets)
+			{
+				if (set.Page is null)
+				{
+					if (TitleOverrides.TryGetValue(set.Name, out var overrideName))
+					{
+						var checkTitle = Title.FromName(this.Site, UespNamespaces.Online, overrideName);
+						set.Page = setMembers.TryGetValue(checkTitle, out var foundPage)
+							? foundPage
+							: throw new InvalidOperationException($"TitleOverride for {set.Name} => {overrideName} doesn't match any known sets.");
+					}
+					else
+					{
+						foreach (var setName in set.AllNames)
+						{
+							var checkTitle = Title.FromName(this.Site, UespNamespaces.Online, setName);
+							if (setMembers.TryGetValue(checkTitle, out var foundPage))
+							{
+								if (foundPage.Exists)
+								{
+									set.Page = foundPage;
+									break;
+								}
+							}
+						}
+
+						if (set.Page is null)
+						{
+							this.UpdateSetPageAnyCase(set, setMembers);
+						}
+					}
+				}
+			}
 		}
 		#endregion
 
 		#region Private Classes
-		private sealed class PageData
+		private sealed class SetCollection : KeyedCollection<Page, SetData>
+		{
+			public SetCollection()
+				: base(SimpleTitleEqualityComparer.Instance)
+			{
+			}
+
+			protected override Page GetKeyForItem(SetData item) => item.Page ?? throw new InvalidOperationException("Item Page is null!");
+		}
+
+		private sealed class SetData
 		{
 			#region Constructors
-			public PageData(string setName, string bonusDescription)
+			public SetData(string setName, string bonusDescription)
 			{
 				ThrowNull(setName, nameof(setName));
 				ThrowNull(bonusDescription, nameof(bonusDescription));
-				this.SetName = setName;
-				this.PageName = !TitleOverrides.TryGetValue(setName, out var pageName)
-					? setName
-					: pageName;
+				this.Name = setName;
 				this.BonusDescription = bonusDescription;
 			}
 			#endregion
 
 			#region Public Properties
+
+			public IEnumerable<string> AllNames
+			{
+				get
+				{
+					yield return this.Name + " (set)";
+					yield return this.Name;
+				}
+			}
+
 			public string BonusDescription { get; }
 
 			public bool IsNonTrivial { get; set; }
 
-			public string PageName { get; set; }
+			public string Name { get; }
 
-			public string SetName { get; }
+			public Page? Page { get; set; }
+			#endregion
+
+			#region Public Methods
+			public void BuildNewPage(Title pageName)
+			{
+				var sb = new StringBuilder();
+				sb
+					.Append("{{Trail|Sets}}{{Online Update}}{{Minimal}}\n")
+					.Append("'''").Append(this.Name).Append("''' is a {{huh}}-rank [[Online:Sets|item set]] found in {{huh}}.\n\n")
+					.Append("Set pieces are {{huh}} style in {{huh}}.\n\n")
+					.Append("===Bonuses===\n")
+					.Append("<onlyinclude>\n")
+					.Append("</onlyinclude>\n")
+					.Append("===Pieces===\n")
+					.Append("The set consists of {{huh}}. For detailed weapon stats and set values, see individual item pages by clicking on the links below.\n")
+					.Append("<!--\n")
+					.Append("* {{Item Link|id=|}}\n")
+					.Append("* {{Item Link|id=|}}\n")
+					.Append("-->\n")
+					.Append("===Notes===\n\n")
+					.Append("{{Online Sets}}\n")
+					.Append("{{ESO Sets With|subtype=|source=}}\n")
+					.Append("{{Stub|Item Set}}");
+
+				this.Page = new Page(pageName)
+				{
+					Text = sb.ToString()
+				};
+			}
 			#endregion
 
 			#region Public Override Methods
-			public override string ToString() => this.SetName;
+			public override string ToString() => $"{this.Name} ({this.Page?.FullPageName ?? "NO PAGE"})";
 			#endregion
 		}
 		#endregion
