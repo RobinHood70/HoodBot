8cd60ccae4c3e158453d591ce4926878c0f84f7a
Robin Hood <robinhood70@live.ca>
2021-07-15 11:24:54 -0400

    Add a TODO/CONSIDER

--- WikiCommon/Parser/IHeaderNode.cs
+++ WikiCommon/Parser/IHeaderNode.cs
@@ -1,5 +1,10 @@
 ﻿namespace RobinHood70.WikiCommon.Parser
 {
+	// TODO: Switch to have Title as one NodeCollection and TrailingText as another.
+	// CONSIDER: Reintroduce confirmed/possible status for constructs like:
+	//    ===Header===<includeonly>BreaksHeader</includeonly>
+	// Might be doable inline as part of TrailingText, so check this.
+
 	/// <summary>Represents a header.</summary>
 	public interface IHeaderNode : IWikiNode, IParentNode
 	{
