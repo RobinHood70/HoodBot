2f82d78aa61a8b44bc38f58203886c0fe1010c7f
Robin Hood <robinhood70@live.ca>
2021-07-21 22:35:30 -0400

    Allow custom save info

---
+++ HoodBot/Jobs/SaveInfo.cs
@@ -0,0 +1,15 @@
+﻿namespace RobinHood70.HoodBot.Jobs
+{
+	public class SaveInfo
+	{
+		public SaveInfo(string editSummary, bool isMinor)
+		{
+			this.EditSummary = editSummary;
+			this.IsMinor = isMinor;
+		}
+
+		public string EditSummary { get; }
+
+		public bool IsMinor { get; }
+	}
+}
