e8b6389965353dca36758a0d775f4fce96deed79
Robin Hood <robinhood70@live.ca>
2021-07-23 12:09:37 -0400

    Add regions

--- HoodBot/Jobs/SaveInfo.cs
+++ HoodBot/Jobs/SaveInfo.cs
@@ -2,14 +2,18 @@
 {
 	public class SaveInfo
 	{
+		#region Constructors
 		public SaveInfo(string editSummary, bool isMinor)
 		{
 			this.EditSummary = editSummary;
 			this.IsMinor = isMinor;
 		}
+		#endregion
 
+		#region Public Properties
 		public string EditSummary { get; }
 
 		public bool IsMinor { get; }
+		#endregion
 	}
 }
