31ce09597dd5cbc77bbec767416c21f21f684932
Robin Hood <robinhood70@live.ca>
2021-07-27 18:29:22 -0400

    Interim save of Photographs job - may need more work

--- HoodBot/Jobs/MovePages.cs
+++ HoodBot/Jobs/MovePages.cs
@@ -1,40 +1,94 @@
 ﻿namespace RobinHood70.HoodBot.Jobs
 {
-	using System.Diagnostics;
+	using System;
+	using System.Collections.Generic;
+	using RobinHood70.CommonCode;
+	using RobinHood70.HoodBot.Jobs.JobModels;
 	using RobinHood70.Robby;
+	using RobinHood70.Robby.Parser;
+	using RobinHood70.WikiCommon;
+	using RobinHood70.WikiCommon.Parser;
 
 	public class MovePages : MovePagesJob
 	{
+		#region Static Fields
+		private static readonly List<string> PhotoCats =
+		new()
+		{
+			"Morrowind-Photos-rpeh",
+			"Morrowind-Photos-TheEnigmaticMan",
+			"Oblivion-Photos-Arch-Mage Matt",
+			"Oblivion-Photos-rpeh",
+			"Oblivion-Photos-TheEnigmaticMan",
+			"Oblivion-Photos-Timenn",
+			"Skyrim-Photos-rpeh"
+		};
+		#endregion
+
 		#region Constructors
 		[JobInfo("Page Mover")]
 		public MovePages(JobManager jobManager)
-			: base(jobManager)
+				: base(jobManager)
 		{
 			this.DeleteStatusFile();
-			this.MoveAction = MoveAction.None;
+			this.MoveAction = MoveAction.MoveSafely;
 			this.MoveDelay = 0;
-			this.FollowUpActions = FollowUpActions.FixLinks | FollowUpActions.EmitReport | FollowUpActions.CheckLinksRemaining | FollowUpActions.UpdateCaption | FollowUpActions.UpdatePageNameCaption;
+			this.FollowUpActions = FollowUpActions.FixLinks | FollowUpActions.EmitReport | FollowUpActions.CheckLinksRemaining;
 		}
 		#endregion
 
 		#region Protected Override Methods
-		protected override void UpdateLinkText(Page page, Title oldTitle, SiteLink newLink, bool addCaption)
+		protected override void CustomEdit(Page page, Replacement replacement)
 		{
-			if (newLink.Text != null)
+			var parsedPage = new ContextualParser(page);
+			var nodes = parsedPage.Nodes;
+			for (var i = nodes.Count - 1; i >= 0; i--)
 			{
-				base.UpdateLinkText(page, oldTitle, newLink, addCaption);
-				Debug.WriteLine($"{page.PageName} / {oldTitle.PageName} / {newLink.Text}");
+				if (nodes[i] is SiteLinkNode link &&
+					link.TitleValue.Namespace == MediaWikiNamespaces.Category &&
+					PhotoCats.Contains(link.TitleValue.PageName, StringComparer.OrdinalIgnoreCase))
+				{
+					nodes.RemoveAt(i);
+					if (i < (nodes.Count - 1) && nodes[i + 1] is ITextNode text)
+					{
+						text.Text = text.Text.TrimStart();
+					}
+				}
 			}
+
+			page.Text = parsedPage.ToRaw();
 		}
 
 		protected override void PopulateReplacements()
 		{
-			this.AddReplacement("Online:Chaotic Whirlwind (Perfected)", "Online:Perfected Chaotic Whirlwind");
-			this.AddReplacement("Online:Concentrated Force (Perfected)", "Online:Perfected Concentrated Force");
-			this.AddReplacement("Online:Defensive Position (Perfected)", "Online:Perfected Defensive Position");
-			this.AddReplacement("Online:Disciplined Slash (Perfected)", "Online:Perfected Disciplined Slash");
-			this.AddReplacement("Online:Piercing Spray (Perfected)", "Online:Perfected Piercing Spray");
-			this.AddReplacement("Online:Timeless Blessing (Perfected)", "Online:Perfected Timeless Blessing");
+			foreach (var cat in PhotoCats)
+			{
+				this.GetAuthorReplacements(cat);
+			}
+
+			this.AddReplacement("Category:Photographs", "Category:User Images-Screenshots");
+			this.AddReplacement("Category:Morrowind-Photos", "Category:User Images-Screenshots-Morrowind");
+			this.AddReplacement("Category:Oblivion-Photos", "Category:User Images-Screenshots-Oblivion");
+			this.AddReplacement("Category:Skyrim-Photos", "Category:User Images-Screenshots-Skyrim");
+		}
+		#endregion
+
+		#region Private Methods
+		private void GetAuthorReplacements(string authorCat)
+		{
+			var author = authorCat.Split(TextArrays.CategorySeparators, 3)[^1];
+			var titles = new TitleCollection(this.Site);
+			titles.GetCategoryMembers(authorCat);
+			foreach (var title in titles)
+			{
+				var pageParts = title.PageName.Split(TextArrays.CategorySeparators, 3);
+				var imageName = pageParts[^1];
+				var rep = new Replacement(
+					title,
+					Title.FromName(this.Site, MediaWikiNamespaces.File, $"User-{author}-{imageName}"));
+				rep.Actions |= ReplacementActions.Edit;
+				this.Replacements.Add(rep);
+			}
 		}
 		#endregion
 	}
