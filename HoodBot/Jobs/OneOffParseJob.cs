06a0ebd406e420c79da31e901614319dc3453dbe
Robin Hood <robinhood70@live.ca>
2021-08-26 18:08:32 -0400

    Latest job

--- HoodBot/Jobs/OneOffParseJob.cs
+++ HoodBot/Jobs/OneOffParseJob.cs
@@ -1,12 +1,23 @@
 ﻿namespace RobinHood70.HoodBot.Jobs
 {
-	using System;
+	using System.Text;
+	using System.Text.RegularExpressions;
+	using RobinHood70.CommonCode;
+	using RobinHood70.Robby;
 	using RobinHood70.Robby.Design;
 	using RobinHood70.Robby.Parser;
 	using RobinHood70.WikiCommon.Parser;
 
 	public class OneOffParseJob : ParsedPageJob
 	{
+		#region Static Fields
+		private static readonly Regex DescriptionReplacer = new(@"^:\s*''(?<desc>.+?)''\s*\n?", RegexOptions.ExplicitCapture | RegexOptions.Multiline, Globals.DefaultRegexTimeout);
+		#endregion
+
+		#region Fields
+		private string? oldDescription;
+		#endregion
+
 		#region Constructors
 		[JobInfo("One-Off Parse Job")]
 		public OneOffParseJob(JobManager jobManager)
@@ -16,19 +27,45 @@
 		#endregion
 
 		#region Protected Override Properties
-		protected override string EditSummary => "Remove redundant savetitlename";
+		protected override string EditSummary => "Remove redundant description";
 		#endregion
 
 		#region Protected Override Methods
-		protected override void LoadPages() => this.Pages.GetBacklinks("Template:Online Achievement Summary");
+		protected override void LoadPages() => this.Pages.GetBacklinks("Template:Online Collectible Summary");
 
 		protected override void ParseText(object sender, ContextualParser parsedPage)
 		{
 			if (parsedPage.FindTemplate("Online Collectible Summary") is SiteTemplateNode template &&
-				template.Find("titlename") is IParameterNode title &&
-				string.Equals(title.Value.ToValue().Trim(), parsedPage.Context.LabelName(), StringComparison.Ordinal))
+				template.Find("description") is IParameterNode desc)
 			{
-				template.Remove("titlename");
+				var parsedDesc = new SiteNodeFactory(this.Site).Parse(this.oldDescription);
+				var oldDesc = new WikiPlainTextVisitor(this.Site).Build(parsedDesc);
+				var newRaw = desc.Value.ToRaw().Trim();
+				var newBytes = Encoding.GetEncoding("Windows-1252").GetBytes(newRaw);
+				var newDesc = Encoding.UTF8.GetString(newBytes);
+				if (string.Equals(oldDesc, newDesc, System.StringComparison.Ordinal))
+				{
+					desc.SetValue(this.oldDescription);
+				}
+				else
+				{
+					this.WriteLine("* " + parsedPage.Context.AsLink(true) + " - needs checked.");
+					if (!string.Equals(newDesc, newRaw, System.StringComparison.Ordinal))
+					{
+						desc.SetValue(newDesc);
+					}
+				}
+			}
+		}
+
+		protected override void ResultsPageLoaded(object sender, Page page)
+		{
+			var match = DescriptionReplacer.Match(page.Text);
+			if (match.Success)
+			{
+				this.oldDescription = match.Groups["desc"].Value;
+				page.Text = page.Text.Remove(match.Index, match.Length);
+				base.ResultsPageLoaded(sender, page);
 			}
 		}
 		#endregion
