6c51d0d88bd702577617536ac3492074ef213ca2
Robin Hood <robinhood70@live.ca>
2021-11-09 09:12:30 -0500

    Interim save

--- HoodBot/Jobs/OneOffParseJob.cs
+++ HoodBot/Jobs/OneOffParseJob.cs
@@ -10,14 +10,6 @@
 
 	public class OneOffParseJob : ParsedPageJob
 	{
-		#region Static Fields
-		private static readonly Regex DescriptionReplacer = new(@"^:\s*''(?<desc>.+?)''\s*\n?", RegexOptions.ExplicitCapture | RegexOptions.Multiline, Globals.DefaultRegexTimeout);
-		#endregion
-
-		#region Fields
-		private string? oldDescription;
-		#endregion
-
 		#region Constructors
 		[JobInfo("One-Off Parse Job")]
 		public OneOffParseJob(JobManager jobManager)
@@ -27,45 +19,19 @@
 		#endregion
 
 		#region Protected Override Properties
-		protected override string EditSummary => "Remove redundant description";
+		protected override string EditSummary => "Add explicit header";
 		#endregion
 
 		#region Protected Override Methods
-		protected override void LoadPages() => this.Pages.GetBacklinks("Template:Online Collectible Summary");
+		protected override void LoadPages() => this.Pages.GetBacklinks("Template:Similar Images");
 
 		protected override void ParseText(object sender, ContextualParser parsedPage)
 		{
-			if (parsedPage.FindTemplate("Online Collectible Summary") is SiteTemplateNode template &&
-				template.Find("description") is IParameterNode desc)
+			var templateIndex = parsedPage.Nodes.FindIndex<SiteTemplateNode>(t => t.TitleValue.PageNameEquals("Similar Images"));
+			var header = parsedPage.Nodes.Find<IHeaderNode>(h => string.Equals(h.GetInnerText(true), "Similar Iamges", System.StringComparison.Ordinal));
+			if (header == null)
 			{
-				var parsedDesc = new SiteNodeFactory(this.Site).Parse(this.oldDescription);
-				var oldDesc = new WikiPlainTextVisitor(this.Site).Build(parsedDesc);
-				var newRaw = desc.Value.ToRaw().Trim();
-				var newBytes = Encoding.GetEncoding("Windows-1252").GetBytes(newRaw);
-				var newDesc = Encoding.UTF8.GetString(newBytes);
-				if (string.Equals(oldDesc, newDesc, System.StringComparison.Ordinal))
-				{
-					desc.SetValue(this.oldDescription);
-				}
-				else
-				{
-					this.WriteLine("* " + parsedPage.Context.AsLink(true) + " - needs checked.");
-					if (!string.Equals(newDesc, newRaw, System.StringComparison.Ordinal))
-					{
-						desc.SetValue(newDesc);
-					}
-				}
-			}
-		}
-
-		protected override void ResultsPageLoaded(object sender, Page page)
-		{
-			var match = DescriptionReplacer.Match(page.Text);
-			if (match.Success)
-			{
-				this.oldDescription = match.Groups["desc"].Value;
-				page.Text = page.Text.Remove(match.Index, match.Length);
-				base.ResultsPageLoaded(sender, page);
+				parsedPage.Nodes.Insert(templateIndex, parsedPage.Factory.TextNode("== Similar Images ==\n"));
 			}
 		}
 		#endregion
