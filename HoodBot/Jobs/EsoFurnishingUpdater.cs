34bc837dc9d60f0e57fcf2c7014ed064d63a4fe3
Robin Hood <robinhood70@live.ca>
2022-01-15 16:53:02 -0500

    Update ESO furnishing updater to add categories

--- HoodBot/Jobs/EsoFurnishingUpdater.cs
+++ HoodBot/Jobs/EsoFurnishingUpdater.cs
@@ -3,47 +3,16 @@
 	using System;
 	using System.Collections.Generic;
 	using System.Diagnostics;
-	using System.IO;
-	using System.Text;
-	using System.Text.RegularExpressions;
-	using RobinHood70.CommonCode;
-	using RobinHood70.HoodBot.Uesp;
 	using RobinHood70.Robby;
 	using RobinHood70.Robby.Design;
 	using RobinHood70.Robby.Parser;
+	using RobinHood70.WikiCommon;
 	using RobinHood70.WikiCommon.Parser;
 
 	internal sealed class EsoFurnishingUpdater : TemplateJob
 	{
-		#region Static Fields
-		private static readonly Dictionary<string, string> MaterialsLookup = new(StringComparer.OrdinalIgnoreCase)
-		{
-			["bast"] = "Bast",
-			["clean pelt"] = "Clean Pelt",
-			["heartwood"] = "Heartwood",
-			["rune"] = "Mundane Rune",
-			["ochre"] = "Ochre",
-			["pelt"] = "Clean Pelt",
-			["regulus"] = "Regulus",
-			["resin"] = "Alchemical Resin",
-			["wax"] = "Decorative Wax",
-		};
-
-		private static readonly Regex OtherMat = new(@"\s*(?<material>.*?)\s*\((?<count>\d+)\)", RegexOptions.ExplicitCapture, Globals.DefaultRegexTimeout);
-		private static readonly Dictionary<string, string> SkillsLookup = new(StringComparer.OrdinalIgnoreCase)
-		{
-			["alchemy"] = "Solvent Proficiency",
-			["blacksmithing"] = "Metalworking",
-			["clothing"] = "Tailoring",
-			["enchanting"] = "Potency Improvement",
-			["jewelry"] = "Engraver",
-			["provisioning"] = "Recipe Improvement",
-			["woodworking"] = "Woodworking (skill)",
-		};
-		#endregion
-
 		#region Fields
-		private readonly Dictionary<ISimpleTitle, SiteTemplateNode> dict = new(SimpleTitleEqualityComparer.Instance);
+		private readonly Dictionary<Title, IEnumerable<string>> imageCategories = new(SimpleTitleEqualityComparer.Instance);
 		#endregion
 
 		#region Constructors
@@ -55,177 +24,89 @@
 		#endregion
 
 		#region Protected Override Properties
-		protected override string EditSummary { get; } = "Update missed info";
+		protected override string EditSummary { get; } = "Update info from ESO database";
 
 		protected override string TemplateName { get; } = "Online Furnishing Summary";
 		#endregion
 
 		#region Protected Override Methods
-		protected override void BeforeLogging()
+		protected override void Main()
 		{
-			var file = File.ReadAllText(UespSite.GetBotDataFolder("Furnishing Moves.txt")).Trim();
-			var lines = file.Split("~\n", StringSplitOptions.RemoveEmptyEntries);
-			foreach (var line in lines)
-			{
-				var fields = line.Split(TextArrays.Tab);
-				if (fields.Length > 0)
-				{
-					var template = new SiteNodeFactory(this.Site).SingleNode<SiteTemplateNode>(fields[2]);
-					var title = TitleFactory.Direct(this.Site, UespNamespaces.Online, fields[1]).ToTitle();
-					this.dict.Add(title, template);
-				}
-			}
+			var titles = new TitleCollection(this.Site, this.imageCategories.Keys);
+			PageCollection pages = PageCollection.Unlimited(this.Site, PageModules.Default, false);
+			var saveInfo = new SaveInfo("Update categories", true);
+			pages.PageLoaded += this.ImagePageLoaded;
+			pages.GetTitles(titles);
+			pages.PageLoaded -= this.ImagePageLoaded;
+			this.SavePages(pages, "Saving image categories", saveInfo, this.ImagePageLoaded);
 
-			base.BeforeLogging();
 		}
 
 		protected override void ParseTemplate(SiteTemplateNode template, ContextualParser parsedPage)
 		{
-			ISimpleTitle title = TitleFactory.Direct(parsedPage.Site, UespNamespaces.Online, parsedPage.Context.PageName + " (furnishing)").ToTitle();
-			if (!this.dict.TryGetValue(title, out var originalTemplate))
+			var isCollectible = template.TrueOrFalse("collectible");
+			var itemName = parsedPage.Context.PageName
+				.Replace("item-", string.Empty, StringComparison.Ordinal)
+				.Replace("furnishing-", string.Empty, StringComparison.Ordinal)
+				.Replace(" (furnishing)", string.Empty, StringComparison.Ordinal)
+				.Replace(".jpg", string.Empty, StringComparison.Ordinal);
+			var defaultFile = $"ON-{(isCollectible ? string.Empty : "item-")}furnishing-{itemName}.jpg";
+
+			var image = template.ValueOrDefault("image", defaultFile).Trim();
+			List<string> imgCats = new();
+			imgCats.Add("Category:Online-Furnishings");
+			var baseCat = "Category:Online-Furnishing Images-";
+			var cat = AddCategory(imgCats, template, "cat", $"{baseCat}$1");
+			AddCategory(imgCats, template, "subcat", $"{baseCat}{cat}-$1");
+			AddCategory(imgCats, template, "size", $"{baseCat}$1");
+			AddCategory(imgCats, template, "style", $"{baseCat}$1");
+			AddCategory(imgCats, template, "achievement", $"{baseCat}Achievement");
+			AddCategory(imgCats, template, "antiquity", $"{baseCat}Antiquities");
+			AddCategory(imgCats, template, "tags", $"{baseCat}By Behavior");
+			AddCategory(imgCats, template, "craft", $"{baseCat}By Profession");
+			AddCategory(imgCats, template, "collectible", $"{baseCat}Collectible");
+			AddCategory(imgCats, template, "luxury", $"{baseCat}Luxury");
+			AddCategory(imgCats, template, "master", $"{baseCat}Masterworks");
+			AddCategory(imgCats, template, "source", $"{baseCat}Sources");
+
+			Title imageTitle = TitleFactory.FromName(this.Site, MediaWikiNamespaces.File, image).ToTitle();
+			this.imageCategories.Add(imageTitle, imgCats);
+		}
+		#endregion
+
+		#region Private Static Methods
+		private static string? AddCategory(ICollection<string> imgCats, SiteTemplateNode template, string paramName, string pattern)
+		{
+			if (template.Find(paramName) is IParameterNode cat)
 			{
-				var title2 = parsedPage.Context;
-				if (!this.dict.TryGetValue(title2, out originalTemplate))
-				{
-					Debug.WriteLine("Missing Page:" + title.FullPageName());
-					return;
-				};
+				var catName = cat.Value.ToRaw().Trim();
+				var fullName = pattern.Replace("$1", catName, StringComparison.Ordinal);
+				imgCats.Add(fullName);
+				return catName;
 			}
 
-			SortedDictionary<string, string> materials = new(StringComparer.Ordinal);
-			SortedDictionary<string, string> skills = new(StringComparer.Ordinal);
-			IEnumerable<(string? Key, string Value)> originalParams = originalTemplate.Parameters.ToKeyValue();
+			return null;
+		}
+		#endregion
 
-			string? stylemat = null;
-			string? stylematcount = null;
-			foreach (var (key, value) in originalParams)
+		#region Private Methods
+
+		private void ImagePageLoaded(object sender, Page page)
+		{
+			if (page.Exists)
 			{
-				switch (key)
+				IEnumerable<string> catsToAdd = this.imageCategories[page];
+				ContextualParser parser = new(page);
+				foreach (var cat in catsToAdd)
 				{
-					case "bast":
-					case "clean pelt":
-					case "heartwood":
-					case "ochre":
-					case "pelt":
-					case "regulus":
-					case "resin":
-					case "rune":
-					case "wax":
-						// Move into catchall `materials` parameter:
-						materials[MaterialsLookup[key]] = value.Trim();
-						break;
-					case "alchemy":
-					case "blacksmithing":
-					case "clothing":
-					case "enchanting":
-					case "jewelry":
-					case "provisioning":
-					case "woodworking":
-						// Move into catchall `skills` parameter.
-						skills[SkillsLookup[key]] = value.Trim();
-						break;
-					case "stylemat":
-						stylemat = value.Trim();
-						break;
-					case "stylematcount":
-						stylematcount = value.Trim();
-						break;
-					case "othermat1":
-					case "othermat2":
-					case "othermat3":
-					case "othermat4":
-					case "othermats":
-						Match otherMatch = OtherMat.Match(value.Trim());
-						if (otherMatch.Success)
-						{
-							var otherMaterial = otherMatch.Groups["material"].Value;
-							var otherCount = otherMatch.Groups["count"].Value;
-							try
-							{
-								SiteLink siteLink = SiteLink.FromText(this.Site, otherMaterial);
-								var otherKey = siteLink.Text ?? siteLink.PageName;
-								materials[otherKey] = otherCount;
-							}
-							catch (ArgumentException)
-							{
-								materials[otherMaterial] = otherCount;
-							}
-						}
-
-						break;
-					case "achievecat":
-					case "achievecat2":
-					case "nocat":
-					case "questcat":
-					case "recipename":
-					case "style":
-						template.AddOrChange(key, value.Trim(), ParameterFormat.OnePerLine);
-						break;
-					case "achievementalt":
-					case "trainingdummy":
-					case "luxury":
-					case "materials":
-					case "species":
-						// Unused and will be discarded if nothing else changes (let me know if you want a list of values and where they're used):
-						break;
-					default:
-						break;
+					parser.AddCategory(cat, true);
 				}
+
+				page.Text = parser.ToRaw();
 			}
-
-			if (stylemat != null)
+			else
 			{
-				materials[stylemat] = stylematcount ?? throw new InvalidOperationException();
-			}
-			else if (stylematcount != null)
-			{
-				throw new InvalidOperationException();
-			}
-
-			if (materials.Count > 0)
-			{
-				StringBuilder sb = new(materials.Count * 15);
-				foreach (var material in materials)
-				{
-					sb
-						.Append(',')
-						.Append(material.Key)
-						.Append(',')
-						.Append(material.Value);
-				}
-
-				sb.Remove(0, 1);
-				if (template.Find("materials") is IParameterNode findParamMaterials)
-				{
-					findParamMaterials.SetValue(sb.ToString());
-				}
-				else
-				{
-					template.Add("materials", string.Join(", ", sb.ToString()));
-				}
-			}
-
-			if (skills.Count > 0)
-			{
-				StringBuilder sb = new(skills.Count * 15);
-				foreach (var skill in skills)
-				{
-					sb
-						.Append(',')
-						.Append(skill.Key)
-						.Append(',')
-						.Append(skill.Value);
-				}
-
-				sb.Remove(0, 1);
-				if (template.Find("skills") is IParameterNode findParamSkills)
-				{
-					findParamSkills.SetValue(sb.ToString());
-				}
-				else
-				{
-					template.Add("skills", sb.ToString());
-				}
+				Debug.WriteLine($"Missing page: {page.FullPageName}");
 			}
 		}
 		#endregion
