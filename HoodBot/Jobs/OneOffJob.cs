6a2aee494749c8c6f2f198e0c0281436d1761ca7
Robin Hood <robinhood70@live.ca>
2022-02-01 15:41:45 -0500

    Get Lore-Books word count for AKB

--- HoodBot/Jobs/OneOffJob.cs
+++ HoodBot/Jobs/OneOffJob.cs
@@ -1,9 +1,9 @@
 ﻿namespace RobinHood70.HoodBot.Jobs
 {
-	using System;
+	using System.Diagnostics;
+	using System.Text.RegularExpressions;
 	using RobinHood70.CommonCode;
-	using RobinHood70.Robby;
-	using RobinHood70.Robby.Design;
+	using RobinHood70.Robby.Parser;
 	using RobinHood70.WikiCommon;
 
 	public class OneOffJob : EditJob
@@ -19,49 +19,32 @@
 		#region Protected Override Methods
 		protected override void BeforeLogging()
 		{
-			TitleCollection allTitles = new(this.Site);
-			TitleCollection imageCats = new(this.Site);
-			imageCats.GetNamespace(MediaWikiNamespaces.Category, Filter.Any, "Online-Furnishing Images-");
-			foreach (var title in imageCats)
+			Regex wordCount = new(@"[\w-]+", RegexOptions.ExplicitCapture, Globals.DefaultRegexTimeout);
+			this.Pages.GetCategoryMembers("Lore-Books", CategoryMemberTypes.Page, true);
+			this.Pages.Sort();
+			foreach (var page in this.Pages)
 			{
-				allTitles.Add(title);
-				allTitles.Add(FurnishingTitle(title));
-			}
+				ContextualParser parsedText = new(page);
+				if (parsedText.Nodes.ToValue() is string reText &&
+					wordCount.Matches(reText) is MatchCollection matches)
+				{
+					if (matches.Count < 25)
+					{
+						Debug.WriteLine("Words:");
+						foreach (var match in matches)
+						{
+							Debug.WriteLine("  " + match);
+						}
+					}
 
-			PageCollection pages = new(this.Site, PageModules.Default | PageModules.CategoryInfo);
-			pages.GetTitles(allTitles);
-			foreach (var page in pages)
-			{
-				var newTitle = FurnishingTitle(page);
-				if (page is CategoryPage catPage && catPage.FullCount == 0)
-				{
-					if (catPage.Exists)
-					{
-						catPage.Text = "{{Proposeddeletion|bot=1|Unused category.}}";
-						this.Pages.Add(catPage);
-					}
-				}
-				else if (pages[newTitle] is CategoryPage catPageNew && catPageNew.FullCount == 0)
-				{
-					if (catPageNew.Exists)
-					{
-						catPageNew.Text = "{{Proposeddeletion|bot=1|Unused category.}}";
-						this.Pages.Add(catPageNew);
-					}
-				}
-				else if (page != newTitle && !pages[newTitle].Exists)
-				{
-					var newPage = this.Site.CreatePage(newTitle, page.Text
-						.Replace("images of ", string.Empty, StringComparison.Ordinal)
-						.Replace("Online-Furnishing Images", "Online-Furnishings", StringComparison.Ordinal));
-					this.Pages.Add(newPage);
+					Debug.WriteLine($"{page.FullPageName}: {matches.Count} words");
 				}
 			}
-
-			static ISimpleTitle FurnishingTitle(ISimpleTitle title) => TitleFactory.Direct(title.Namespace, title.PageName.Replace("-Furnishing Images-", "-Furnishings-", StringComparison.Ordinal));
 		}
 
-		protected override void Main() => this.SavePages("Create furnishing redirects", false);
+		protected override void Main()
+		{
+		}
 		#endregion
 	}
 }
