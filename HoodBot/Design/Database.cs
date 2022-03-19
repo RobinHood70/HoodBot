99a2a84d61e95e59f7531b072454d9079665cefc
Robin Hood <robinhood70@live.ca>
2022-03-19 16:06:21 -0400

    Allow paging

--- HoodBot/Design/Database.cs
+++ HoodBot/Design/Database.cs
@@ -3,32 +3,51 @@
 	using System;
 	using System.Collections.Generic;
 	using System.Data;
+	using System.Diagnostics;
 	using MySql.Data.MySqlClient;
 
 	public static class Database
 	{
-		public static IEnumerable<IDataRecord> RunQuery(string connectionString, string query)
+		public static IEnumerable<IDataRecord> RunQuery(string connectionString, string query) => RunQuery(connectionString, query, -1);
+
+		public static IEnumerable<IDataRecord> RunQuery(string connectionString, string query, long pageSize)
 		{
 			using MySqlConnection connection = new(connectionString);
 			connection.Open();
-			using MySqlCommand command = new(query, connection);
-			using var reader = command.ExecuteReader();
-			while (reader.Read())
+			if (pageSize <= 0)
 			{
-				yield return reader;
+				pageSize = long.MaxValue;
+			}
+
+			for (long offset = 0; true; offset += pageSize)
+			{
+				var limitedQuery = query + (pageSize == long.MaxValue ? string.Empty : $" LIMIT {offset}, {pageSize}");
+				Debug.WriteLine(limitedQuery.Replace('\n', ' '));
+				using MySqlCommand command = new(limitedQuery, connection);
+				using var reader = command.ExecuteReader();
+				var recordsRead = 0;
+				var readResult = reader.Read();
+				while (readResult)
+				{
+					yield return reader;
+					readResult = reader.Read();
+					recordsRead++;
+				}
+
+				if (recordsRead < pageSize)
+				{
+					yield break;
+				}
 			}
 		}
 
-		public static IEnumerable<T> RunQuery<T>(string connectionString, string query, Func<IDataRecord, T> factory)
+		public static IEnumerable<T> RunQuery<T>(string connectionString, string query, Func<IDataRecord, T> factory) => RunQuery(connectionString, query, -1, factory);
+
+		public static IEnumerable<T> RunQuery<T>(string connectionString, string query, long pageSize, Func<IDataRecord, T> factory)
 		{
-			// Could also just call non-generic RunQuery but it seemed better to not have two levels of yield return when it's so easy to have only one.
-			using MySqlConnection connection = new(connectionString);
-			connection.Open();
-			using MySqlCommand command = new(query, connection);
-			using var reader = command.ExecuteReader();
-			while (reader.Read())
+			foreach (var row in RunQuery(connectionString, query, pageSize))
 			{
-				yield return factory(reader);
+				yield return factory(row);
 			}
 		}
 	}
