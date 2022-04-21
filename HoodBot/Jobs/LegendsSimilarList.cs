d5a63b1e089652fd85df751cb42d929fd1349f85
Robin Hood <robinhood70@live.ca>
2022-04-21 18:27:15 -0400

    New jobs

---
+++ HoodBot/Jobs/LegendsSimilarList.cs
@@ -0,0 +1,126 @@
+﻿namespace RobinHood70.HoodBot.Jobs
+{
+	using System;
+	using System.Collections.Generic;
+	using RobinHood70.CommonCode;
+	using RobinHood70.HoodBot.Jobs.Design;
+	using RobinHood70.Robby;
+	using RobinHood70.Robby.Design;
+	using RobinHood70.Robby.Parser;
+	using RobinHood70.WikiCommon;
+	using RobinHood70.WikiCommon.Parser;
+
+	internal sealed class LegendsSimilarList : TemplateJob
+	{
+		#region Fields
+		private int pageNumber;
+		private int resultCount;
+		#endregion
+
+		#region Constructors
+		[JobInfo("List Legends Images", "Legends")]
+		public LegendsSimilarList(JobManager jobManager)
+				: base(jobManager)
+		{
+		}
+		#endregion
+
+		#region Protected Override Properties
+		protected override string EditSummary => "Add \"Card art\" to unlabeled gallery images";
+
+		protected override string TemplateName => "Legends Card Summary";
+		#endregion
+
+		#region Protected Override Methods
+
+		protected override void BeforeLogging()
+		{
+			this.BeforeLoadPages();
+			this.StatusWriteLine("Loading Pages");
+			this.LoadPages();
+			this.Pages.Sort();
+			foreach (var page in this.Pages)
+			{
+				this.ResultsPageLoaded(this, page);
+			}
+
+			this.WriteLine("|}");
+			this.Results?.Clear();
+			this.AfterLoadPages();
+		}
+
+		protected override void ParseTemplate(SiteTemplateNode template, ContextualParser parser)
+		{
+			if (this.resultCount == 0 && this.Results is PageResultHandler pageResults)
+			{
+				this.pageNumber++;
+				pageResults.Page = this.Site.CreatePage($"User:HoodBot/Legends Images {this.pageNumber}", string.Empty);
+				this.WriteLine("{| class=\"wikitable compress\"");
+			}
+
+			var galleryEntries = new List<SiteLink>();
+			if (template.GetValue("image") is string image)
+			{
+				image = TitleFactory.FromUnvalidated(this.Site[MediaWikiNamespaces.File], image).FullPageName;
+				image = $"[[{image}|upright=0.5|thumb|Main Image]]";
+			}
+			else
+			{
+				image = "''No Main Image''";
+			}
+
+			var factory = (SiteNodeFactory)parser.Factory;
+			var galleryNodes = new List<ITagNode>(parser.FindAll<ITagNode>(tag => string.Equals(tag.Name, "gallery", StringComparison.Ordinal)));
+			foreach (var galleryNode in galleryNodes)
+			{
+				if (galleryNode.InnerText is not null)
+				{
+					var ns = factory.NotNull().Site[MediaWikiNamespaces.File];
+					var lines = galleryNode.InnerText.Split(TextArrays.LineFeed);
+					var newLines = new List<string>();
+					foreach (var line in lines)
+					{
+						var newLine = line;
+						if (line.Trim() is var trimmedLine && trimmedLine.Length > 0)
+						{
+							var linkNode = factory.LinkNodeFromWikiText("[[" + trimmedLine + " ]]");
+							var siteLink = SiteLink.FromLinkNode(ns, linkNode);
+							if (string.IsNullOrWhiteSpace(siteLink.Text))
+							{
+								newLine += "|Card art";
+							}
+
+						}
+
+						newLines.Add(newLine);
+					}
+
+					galleryNode.InnerText = string.Join("\n", newLines);
+				}
+
+				var siteLinks = SiteLink.FromGalleryNode(factory, galleryNode);
+				foreach (var link in siteLinks)
+				{
+					galleryEntries.Add(link);
+				}
+			}
+
+			this.WriteLine("|-");
+			this.WriteLine("| " + image);
+			foreach (var entry in galleryEntries)
+			{
+				this.WriteLine($"| [[{entry.FullPageName}|upright=0.5|thumb|{entry.Text ?? "''No description''"}]]");
+			}
+
+			this.resultCount++;
+			if (this.resultCount == 500 && this.Results != null)
+			{
+				this.WriteLine("|}");
+				this.resultCount = 0;
+				// this.Results.Save();
+				this.Results.Clear();
+			}
+		}
+		#endregion
+	}
+}
---
+++ HoodBot/Jobs/RelinkCollectibles.cs
@@ -0,0 +1,173 @@
+﻿namespace RobinHood70.HoodBot.Jobs
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Text.RegularExpressions;
+	using RobinHood70.CommonCode;
+	using RobinHood70.HoodBot.Jobs.JobModels;
+	using RobinHood70.HoodBot.Uesp;
+	using RobinHood70.Robby;
+	using RobinHood70.Robby.Design;
+	using RobinHood70.WikiCommon;
+	using RobinHood70.WikiCommon.Parser;
+
+	public class RelinkCollectibles : MovePagesJob
+	{
+		#region Static Fields
+		private static readonly Regex CollectionFinder = new(@"^\*+'''\[\[(?<page>.*?)\|.*?\]\]'''$", RegexOptions.ExplicitCapture | RegexOptions.Multiline, Globals.DefaultRegexTimeout);
+		#endregion
+
+		#region Fields
+		private readonly List<Title> esoTitles = new();
+		private readonly Dictionary<Title, string> disambigs = new(SimpleTitleComparer.Instance);
+		#endregion
+
+		#region Constructors
+		[JobInfo("Relink ESO Collectibles", "ESO")]
+		public RelinkCollectibles(JobManager jobManager)
+				: base(jobManager)
+		{
+			this.MoveAction = MoveAction.None;
+			this.EditSummaryMove = "Match page name to item";
+			this.AllowFromEqualsTo = true; // Replacements are dummy replacements to trigger GetToLink();
+		}
+		#endregion
+
+		#region Protected Override Methods
+
+		protected override SiteLink GetToLink(Title page, bool isRedirectTarget, SiteLink from, Title to)
+		{
+			if (from.Interwiki == null && from.Fragment != null)
+			{
+				var newTitle =
+					this.esoTitles.Find(title => title.PageNameEquals(from.Fragment + this.disambigs[to])) ??
+					this.esoTitles.Find(title => title.PageNameEquals(from.Fragment));
+				if (newTitle == null)
+				{
+					this.WriteLine($"* Match not found for {from} on page {page.AsLink()}.");
+					return from;
+				}
+
+				var retval = from.WithTitle(TitleFactory.FromUnvalidated(to.Namespace, newTitle.PageName));
+				retval.Fragment = null;
+				if (this.GetLinkText(page, from, retval, !isRedirectTarget) is string newText)
+				{
+					retval.Text = newText;
+				}
+
+				return retval;
+			}
+
+			return from;
+		}
+
+		protected override void PopulateMoves()
+		{
+			var allTitles = new TitleCollection(this.Site);
+			allTitles.GetNamespace(UespNamespaces.Online);
+			allTitles.Sort();
+			this.esoTitles.AddRange(allTitles);
+
+			var collectionTitles = this.GetCollectionTitles();
+			var collectionPages = this.GetCollectionPages(collectionTitles);
+			this.PopulateFromCollections(collectionPages);
+		}
+
+		protected override void UpdateLinkNode(Page page, ILinkNode node, bool isRedirectTarget)
+		{
+			page.ThrowNull();
+			node.ThrowNull();
+			var from = SiteLink.FromLinkNode(this.Site, node);
+			if (this.LinkUpdateMatch(from) is Title to)
+			{
+				var toLink = this.GetToLink(page, isRedirectTarget, from, to);
+				if (!from.FullEquals(toLink))
+				{
+					toLink.UpdateLinkNode(node);
+				}
+			}
+
+			if (from.Namespace == MediaWikiNamespaces.Media)
+			{
+				Title key = TitleFactory.FromValidated(this.Site[MediaWikiNamespaces.File], from.PageName);
+				if (this.LinkUpdates.TryGetValue(key, out var toMedia))
+				{
+					var toLink = this.GetToLink(page, isRedirectTarget, from, TitleFactory.FromValidated(this.Site[MediaWikiNamespaces.Media], toMedia!.PageName));
+					if (!from.FullEquals(toLink))
+					{
+						toLink.UpdateLinkNode(node);
+					}
+				}
+			}
+		}
+		#endregion
+
+		#region Private Static Methods
+		private IEnumerable<string> GetCollectionTitles()
+		{
+			var text = this.Site.LoadPageText("Online:Collections") ?? throw new InvalidOperationException();
+			foreach (Match match in CollectionFinder.Matches(text))
+			{
+				var retval = match.Groups["page"];
+				if (retval.Success)
+				{
+					yield return retval.Value;
+				}
+			}
+		}
+		#endregion
+
+		#region Private Methods
+		private PageCollection GetCollectionPages(IEnumerable<string> collectionTitles)
+		{
+			var plm = TitleFactory.FromValidated(this.Site[MediaWikiNamespaces.Template], "PageLetterMenu");
+			var collectionPages = new PageCollection(this.Site, PageModules.Templates);
+			collectionPages.GetTitles(collectionTitles);
+
+			var allTitles = new TitleCollection(this.Site);
+			foreach (var page in collectionPages)
+			{
+				var disambig = page.PageName switch
+				{
+					"Mementos (collection)" => " (memento)",
+					"Mounts" => " (mount)",
+					"Personalities" => " (personality)",
+					"Pets" => " (pet)",
+					"Skins" => " (skin)",
+					_ => string.Empty
+				};
+
+				if (page.Templates.Contains(plm, SimpleTitleComparer.Instance))
+				{
+					for (var c = 'A'; c <= 'Z'; c++)
+					{
+						var title = page.FullPageName + ' ' + c;
+						allTitles.Add(title);
+						this.disambigs.Add(TitleFactory.FromValidated(this.Site, title), disambig);
+					}
+				}
+				else
+				{
+					allTitles.Add(page.FullPageName);
+					this.disambigs.Add(TitleFactory.FromValidated(this.Site, page.FullPageName), disambig);
+				}
+			}
+
+			var retval = new PageCollection(this.Site, PageModules.None);
+			retval.GetTitles(allTitles);
+			retval.RemoveExists(false);
+			retval.Sort();
+
+			return retval;
+		}
+
+		private void PopulateFromCollections(PageCollection collections)
+		{
+			foreach (var title in collections)
+			{
+				this.AddReplacement(title, title, ReplacementActions.None, "dummy update");
+			}
+		}
+		#endregion
+	}
+}
