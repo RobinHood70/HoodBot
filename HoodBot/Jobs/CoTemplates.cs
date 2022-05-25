08a6b1855ab48553960a6e666cd5b82113337a40
Robin Hood <robinhood70@live.ca>
2022-05-25 11:44:31 -0400

    New search for two templates on same page

---
+++ HoodBot/Jobs/CoTemplates.cs
@@ -0,0 +1,40 @@
+﻿namespace RobinHood70.HoodBot.Jobs
+{
+	using System.Diagnostics;
+	using RobinHood70.Robby;
+	using RobinHood70.Robby.Design;
+	using RobinHood70.WikiCommon;
+
+	public class CoTemplates : WikiJob
+	{
+		#region Constructors
+		[JobInfo("Co-occurring Templates")]
+		public CoTemplates(JobManager jobManager, string template1, string template2)
+			: base(jobManager)
+		{
+			this.Title1 = TitleFactory.FromUnvalidated(this.Site[MediaWikiNamespaces.Template], template1);
+			this.Title2 = TitleFactory.FromUnvalidated(this.Site[MediaWikiNamespaces.Template], template2);
+		}
+
+		public Title Title1 { get; }
+
+		public Title Title2 { get; }
+		#endregion
+
+		#region Protected Override Methods
+		protected override void Main()
+		{
+			var pages = PageCollection.Unlimited(this.Site, PageModules.Templates, true);
+			pages.GetBacklinks(this.Title1.FullPageName);
+			foreach (var page in pages)
+			{
+				var titles = new TitleCollection(this.Site, page.Templates);
+				if (titles.Contains(this.Title2))
+				{
+					Debug.WriteLine($"* {page.AsLink(LinkFormat.LabelName)}");
+				}
+			}
+		}
+		#endregion
+	}
+}
