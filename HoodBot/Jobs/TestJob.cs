aab758e6bd778728fd66eedff823e85b0353b499
Robin Hood <robinhood70@live.ca>
2022-05-27 23:18:18 -0400

    Test more things

--- HoodBot/Jobs/TestJob.cs
+++ HoodBot/Jobs/TestJob.cs
@@ -1,6 +1,9 @@
 ﻿namespace RobinHood70.HoodBot.Jobs
 {
+	using System;
 	using System.Threading;
+	using RobinHood70.WallE.Base;
+	using RobinHood70.WallE.Clients;
 
 	public class TestJob : WikiJob
 	{
@@ -15,13 +18,28 @@
 		#region Protected Override Methods
 		protected override void Main()
 		{
-			const int maxTimes = 60;
+			const int maxTimes = 30;
 			this.ProgressMaximum = maxTimes;
 			for (var i = 1; i <= maxTimes; i++)
 			{
-				this.StatusWriteLine($"Sleep: ( {i} / {maxTimes} )");
-				Thread.Sleep(1000);
+				this.Wait(TimeSpan.FromSeconds(2));
 				this.Progress++;
+				this.StatusWriteLine($"Sleep: ( {i} / {maxTimes} )");
+			}
+		}
+		#endregion
+
+		#region Private Methods
+		private void Wait(TimeSpan delay)
+		{
+			var site = this.Site;
+			if (site.AbstractionLayer is IInternetEntryPoint net && net.Client is IMediaWikiClient client)
+			{
+				client.RequestDelay(delay, DelayReason.ClientThrottled, "Just cuz.");
+			}
+			else
+			{
+				Thread.Sleep(delay);
 			}
 		}
 		#endregion
