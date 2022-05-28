fb88604e232df56443bfa6a6189bde47ff6ad479
Robin Hood <robinhood70@live.ca>
2022-05-27 23:18:04 -0400

    Job for single page moves

---
+++ HoodBot/Jobs/QuickMove.cs
@@ -0,0 +1,24 @@
+﻿namespace RobinHood70.HoodBot.Jobs
+{
+	public class QuickMove : MovePagesJob
+	{
+		#region Constructors
+		[JobInfo("Quick Move")]
+		public QuickMove(JobManager jobManager, string from, string to)
+				: base(jobManager)
+		{
+			this.MoveAction = MoveAction.MoveSafely;
+			this.FollowUpActions = FollowUpActions.FixLinks;
+			this.EditSummaryMove = $"Move to [[{to}]]";
+			this.AddMove(from, to);
+		}
+		#endregion
+
+		#region Protected Override Methods
+
+		protected override void PopulateMoves()
+		{
+		}
+		#endregion
+	}
+}
