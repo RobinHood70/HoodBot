1cc3cba62cccbbae8d3e3075863b1eb1d26485a5
Robin Hood <robinhood70@live.ca>
2022-06-01 18:39:09 -0400

    Remove empty House sections

--- HoodBot/Jobs/OneOffParseJob.cs
+++ HoodBot/Jobs/OneOffParseJob.cs
@@ -1,9 +1,7 @@
 ﻿namespace RobinHood70.HoodBot.Jobs
 {
-	using System.Collections.Generic;
+	using System;
 	using RobinHood70.CommonCode;
-	using RobinHood70.Robby;
-	using RobinHood70.Robby.Design;
 	using RobinHood70.Robby.Parser;
 	using RobinHood70.WikiCommon.Parser;
 
@@ -18,11 +16,13 @@
 		#endregion
 
 		#region Public Override Properties
+		public override string? LogDetails => this.EditSummary;
+
 		public override string LogName => "One-Off Parse Job";
 		#endregion
 
 		#region Protected Override Properties
-		protected override string EditSummary => "Convert Mod Header to CC Header";
+		protected override string EditSummary => "Remove empty Houses section from collectibles";
 		#endregion
 
 		#region Protected Override Methods
@@ -32,48 +32,22 @@
 			base.Main();
 		}
 
-		protected override void LoadPages()
-		{
-			TitleCollection templates = new(this.Site);
-			TitleCollection catPages = new(this.Site);
-			HashSet<Title> hashset = new(SimpleTitleComparer.Instance);
-			templates.GetBacklinks("Template:Mod Header");
-			catPages.GetCategoryMembers("Category:Skyrim-Creation Club", true);
-			hashset.UnionWith(templates);
-			hashset.IntersectWith(catPages);
-			this.Pages.GetTitles(hashset);
-		}
+		protected override void LoadPages() => this.Pages.GetCategoryMembers("Online-Furnishings-Empty House Section");
 
 		protected override void ParseText(object sender, ContextualParser parser)
 		{
-			parser.ThrowNull();
-			var nodes = parser;
-			var count = 0;
-			for (var nodeIndex = nodes.Count - 1; nodeIndex >= 0; nodeIndex--)
+			var sections = parser.ToSections(2);
+			for (var i = sections.Count - 1; i >= 0; i--)
 			{
-				if (nodes[nodeIndex] is SiteTemplateNode template &&
-					template.TitleValue.PageNameEquals("Mod Header") &&
-					template.FindNumberedIndex(1) is var paramIndex &&
-					paramIndex >= 0)
+				var section = sections[i];
+				var sectionHeader = section.Header?.GetInnerText(true) ?? string.Empty;
+				if (string.Equals(sectionHeader, "Houses", StringComparison.Ordinal))
 				{
-					template.Title.Clear();
-					template.Title.AddText("CC Header");
-					template.Remove("ns_base");
-					if (string.Equals(template.Parameters[paramIndex].Value.ToRaw(), "Creation Club", System.StringComparison.Ordinal))
-					{
-						if (count == 0)
-						{
-							template.Parameters.RemoveAt(paramIndex);
-						}
-						else
-						{
-							nodes.RemoveAt(nodeIndex);
-						}
-					}
-
-					count++;
+					sections.RemoveAt(i);
 				}
 			}
+
+			parser.FromSections(sections);
 		}
 		#endregion
 	}
