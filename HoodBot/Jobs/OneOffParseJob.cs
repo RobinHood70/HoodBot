d11aef47dbff0ab3037dc0a9dd42837a98b9cec1
Robin Hood <robinhood70@live.ca>
2022-06-02 15:02:17 -0400

    Update Housing/Achievments/Rewards

--- HoodBot/Jobs/OneOffParseJob.cs
+++ HoodBot/Jobs/OneOffParseJob.cs
@@ -1,12 +1,16 @@
 ﻿namespace RobinHood70.HoodBot.Jobs
 {
 	using System;
-	using RobinHood70.CommonCode;
+	using System.Collections.Generic;
 	using RobinHood70.Robby.Parser;
 	using RobinHood70.WikiCommon.Parser;
 
 	public class OneOffParseJob : ParsedPageJob
 	{
+		#region Static Fields
+		private readonly HashSet<string> achievementHeaders = new(StringComparer.Ordinal) { "Achievement", "Achievements", "Reward", "Rewards" };
+		#endregion
+
 		#region Constructors
 		[JobInfo("One-Off Parse Job")]
 		public OneOffParseJob(JobManager jobManager)
@@ -26,28 +30,53 @@
 		#endregion
 
 		#region Protected Override Methods
-		protected override void Main()
-		{
-			this.Pages.Shuffle();
-			base.Main();
-		}
-
-		protected override void LoadPages() => this.Pages.GetCategoryMembers("Online-Furnishings-Empty House Section");
+		protected override void LoadPages() => this.Pages.GetCategoryMembers("Online-Furnishings-Tagged for Bot");
 
 		protected override void ParseText(object sender, ContextualParser parser)
 		{
+			if (parser.FindSiteTemplate("Online Furnishing Summary") is not SiteTemplateNode template)
+			{
+				return;
+			}
+
 			var sections = parser.ToSections(2);
+			var skip = false;
 			for (var i = sections.Count - 1; i >= 0; i--)
 			{
-				var section = sections[i];
-				var sectionHeader = section.Header?.GetInnerText(true) ?? string.Empty;
+				var houseSection = sections[i];
+				var sectionHeader = houseSection.Header?.GetInnerText(true) ?? string.Empty;
 				if (string.Equals(sectionHeader, "Houses", StringComparison.Ordinal))
 				{
 					sections.RemoveAt(i);
 				}
+
+				if (this.achievementHeaders.Contains(sectionHeader))
+				{
+					skip = true;
+				}
 			}
 
 			parser.FromSections(sections);
+
+			var insertAt = parser.FindIndex<SiteTemplateNode>(node => node.TitleValue.PageNameEquals("NewLeft"));
+			insertAt++;
+
+			string? section = null;
+			if (!skip && template.Find("achievement") is IParameterNode achievement)
+			{
+				var achName = achievement.Value.ToRaw().Trim();
+				section = $"\n==Achievement==\nThis furnishing may be purchased after completing the following [[Online:Achievements|achievement]]: {{{{ESO Achievements List|{achName}}}}}\n";
+			}
+			else if (!skip && template.Find("reward") is IParameterNode reward)
+			{
+				var achName = reward.Value.ToRaw().Trim();
+				section = $"\n==Reward==\nThis furnishing is rewarded after completing the following [[Online:Achievements|achievement]]: {{{{ESO Achievements List|{achName}}}}}\n";
+			}
+
+			if (section is not null)
+			{
+				parser.InsertRange(insertAt, parser.Parse(section));
+			}
 		}
 		#endregion
 	}
