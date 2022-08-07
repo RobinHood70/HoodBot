5d133536abba8589c118e5835d0a83ef39ca245b
Robin Hood <robinhood70@live.ca>
2022-08-06 22:45:53 -0400

    Job to fix Lore Link parameter names

--- HoodBot/Jobs/OneOffTemplateJob.cs
+++ HoodBot/Jobs/OneOffTemplateJob.cs
@@ -1,6 +1,5 @@
 ﻿namespace RobinHood70.HoodBot.Jobs
 {
-	using RobinHood70.Robby;
 	using RobinHood70.Robby.Parser;
 	using RobinHood70.WikiCommon.Parser;
 
@@ -14,54 +13,24 @@
 		}
 		#endregion
 
-		#region Public Properties
+		#region Public Override Properties
 		public override string? LogDetails => "Update " + this.TemplateName;
 
 		public override string LogName => "One-Off Template Job";
 		#endregion
 
 		#region Protected Override Properties
-		protected override string EditSummary => "Update for changes in template";
+		protected override string EditSummary => "Fix link templates";
 
-		protected override string TemplateName => "ESO Contraband Item";
+		protected override string TemplateName => "Lore Link";
 		#endregion
 
 		#region Protected Override Methods
 		protected override void ParseTemplate(SiteTemplateNode template, ContextualParser parser)
 		{
-			var value = template.Find("value");
-			if (template.Find(5) is IParameterNode quality)
-			{
-				var qualityValue = quality.Value.ToRaw();
-				if (string.Equals(qualityValue, "2", System.StringComparison.Ordinal))
-				{
-					quality.SetValue("f", ParameterFormat.NoChange);
-				}
-				else if (
-					value is not null &&
-					(string.Equals(qualityValue, "l", System.StringComparison.Ordinal) ||
-					string.Equals(qualityValue, "t", System.StringComparison.Ordinal)) &&
-					string.Equals(value.Value.ToRaw(), "0", System.StringComparison.Ordinal))
-				{
-					template.Remove("value");
-				}
-			}
-
-			if (value is not null)
-			{
-				if (value.Value.Find<SiteTemplateNode>(n => string.Equals(n.GetTitleText(), "ESO Price", System.StringComparison.Ordinal)) is SiteTemplateNode price && price.Find(1) is IParameterNode value2)
-				{
-					value.Value.Clear();
-					value.Value.AddRange(value2.Value);
-				}
-			}
-		}
-
-		protected override void PageLoaded(EditJob job, Page page)
-		{
-			page.Text = page.Text
-				.Replace("{{icon|", "{{subst:icon|", System.StringComparison.Ordinal);
-			base.PageLoaded(job, page);
+			template.RenameParameter("ONLink", "ONlink");
+			//// template.RenameParameter("LoreLink", "LOlink");
+			template.RenameParameter("SILink", "SIlink");
 		}
 		#endregion
 	}
