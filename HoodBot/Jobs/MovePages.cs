f898d3973234f2b0d9c0e341550aba8068268d8e
Robin Hood <robinhood70@live.ca>
2023-01-10 13:53:26 -0500

    Latest move pages

--- HoodBot/Jobs/MovePages.cs
+++ HoodBot/Jobs/MovePages.cs
@@ -1,40 +1,42 @@
 ﻿namespace RobinHood70.HoodBot.Jobs
 {
-	using RobinHood70.CommonCode;
-	using RobinHood70.HoodBot.Uesp;
-	using RobinHood70.Robby;
-	using RobinHood70.Robby.Design;
+	using System.IO;
+	using RobinHood70.WikiCommon;
 
 	public class MovePages : MovePagesJob
 	{
 		#region Constructors
 		[JobInfo("Move Pages")]
-		public MovePages(JobManager jobManager)
+		public MovePages(JobManager jobManager, bool updateUserSpace)
 				: base(jobManager)
 		{
-			this.EditSummaryMove = "Move to new namespace";
+			if (updateUserSpace)
+			{
+				this.Pages.SetLimitations(
+					Robby.LimitationType.Disallow,
+					MediaWikiNamespaces.Media,
+					MediaWikiNamespaces.MediaWiki,
+					MediaWikiNamespaces.Special,
+					MediaWikiNamespaces.Template);
+			}
+
 			this.MoveAction = MoveAction.None;
-			this.FollowUpActions = FollowUpActions.FixLinks | FollowUpActions.EmitReport;
+			this.FollowUpActions = FollowUpActions.Default & ~FollowUpActions.EmitReport;
 			this.Site.WaitForJobQueue();
 		}
 		#endregion
 
 		#region Protected Override Methods
-		protected override void PageLoaded(EditJob job, Page page)
-		{
-			// TODO: Nothing to do here. May be a good candidate for a new job type.
-		}
-
 		protected override void PopulateMoves()
 		{
-			var titles = new TitleCollection(this.Site);
-			titles.GetNamespace(UespNamespaces.DFUMod, Filter.Any);
-			foreach (var title in titles)
+			var lines = File.ReadAllLines("D:\\MoveList.txt");
+			foreach (var line in lines)
 			{
-				var oldTitle = TitleFactory.FromValidated(this.Site.Namespaces[UespNamespaces.DaggerfallMod], "Daggerfall Unity/Mods/" + title.PageName);
-				this.AddMove(oldTitle, title);
+				this.AddMove(line, line.Replace("ON-item-", "ON-", System.StringComparison.Ordinal));
 			}
 		}
+
+		//// this.AddLinkUpdate("Category:Online-Furnishings", "Category:Online-Furnishing Images");
 		//// this.AddReplacement("Skyrim:Map Notes", "Skyrim:Treasure Maps");
 		//// this.LoadReplacementsFromFile(UespSite.GetBotDataFolder("Replacements5.txt"));
 		#endregion
