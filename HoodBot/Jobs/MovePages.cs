f6a68757a129bf12a7cf2372d462e9196e5fc7d5
Robin Hood <robinhood70@live.ca>
2023-01-22 15:24:46 -0500

    Navbox moves

--- HoodBot/Jobs/MovePages.cs
+++ HoodBot/Jobs/MovePages.cs
@@ -1,7 +1,9 @@
 ﻿namespace RobinHood70.HoodBot.Jobs
 {
 	using System;
+	using RobinHood70.HoodBot.Jobs.JobModels;
 	using RobinHood70.Robby;
+	using RobinHood70.Robby.Design;
 	using RobinHood70.Robby.Parser;
 	using RobinHood70.WikiCommon;
 	using RobinHood70.WikiCommon.Parser;
@@ -15,11 +17,30 @@
 		{
 			this.MoveAction = MoveAction.MoveSafely;
 			this.FollowUpActions = FollowUpActions.Default;
+			this.EditSummaryEditMovedPage = "Remove Rename template";
 			this.Site.WaitForJobQueue();
 		}
 		#endregion
 
 		#region Protected Override Methods
+		protected override void CustomEdit(ContextualParser parser, Title from)
+		{
+			parser.RemoveAll<SiteTemplateNode>(template => template.TitleValue.Namespace == MediaWikiNamespaces.Template && template.TitleValue.PageNameEquals("Rename"));
+			if (parser.Count > 1)
+			{
+				for (var i = parser.Count - 2; i >= 0; i--)
+				{
+					if (
+						parser[i] is IIgnoreNode ignoreOpen && string.Equals(ignoreOpen.Value, "<noinclude>", StringComparison.Ordinal) &&
+						parser[i + 1] is IIgnoreNode ignoreClose && string.Equals(ignoreClose.Value, "</noinclude>", StringComparison.Ordinal))
+					{
+						parser.RemoveAt(i + 1);
+						parser.RemoveAt(i);
+					}
+				}
+			}
+		}
+
 		protected override void PopulateMoves()
 		{
 			var templatePages = new PageCollection(this.Site);
@@ -29,9 +50,14 @@
 			{
 				var parsed = new ContextualParser(page, InclusionType.CurrentPage, false);
 				var rename = parsed.FindSiteTemplate("Rename");
-				if (rename != null && rename.Find(1) is IParameterNode renameTo)
+				if (rename != null && rename.Find(1) is IParameterNode renameParam)
 				{
-					this.AddMove(page.FullPageName, renameTo.Value.ToRaw().Replace(" Gods", " Religion", StringComparison.Ordinal));
+					var renameTo = renameParam.Value.ToRaw().Replace(" Gods", " Religion", StringComparison.Ordinal);
+					this.AddReplacement(
+						TitleFactory.FromUnvalidated(this.Site, page.FullPageName),
+						TitleFactory.FromUnvalidated(this.Site, renameTo),
+						ReplacementActions.Move | ReplacementActions.Edit,
+						null);
 				}
 			}
 		}
