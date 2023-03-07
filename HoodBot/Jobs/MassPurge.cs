f6edd3e7c5b9f8ab7a908292ae9bf4689004be94
Robin Hood <robinhood70@live.ca>
2023-03-07 16:49:36 -0500

    Add dummy progress

--- HoodBot/Jobs/MassPurge.cs
+++ HoodBot/Jobs/MassPurge.cs
@@ -1,7 +1,6 @@
 ﻿namespace RobinHood70.HoodBot.Jobs
 {
 	using System;
-	using RobinHood70.CommonCode;
 	using RobinHood70.Robby;
 	using RobinHood70.WikiCommon;
 
@@ -18,7 +17,8 @@
 		public MassPurge(JobManager jobManager, string pages, bool linkUpdate, bool recursive)
 			: base(jobManager, JobType.UnloggedWrite)
 		{
-			pages = pages.NotNull().Replace("\r\n", "\n", StringComparison.Ordinal);
+			ArgumentNullException.ThrowIfNull(pages);
+			pages = pages.Replace("\r\n", "\n", StringComparison.Ordinal);
 			var values = pages.Split('\n', StringSplitOptions.RemoveEmptyEntries);
 			this.titles = new TitleCollection(this.Site, values);
 			this.purgeMethod =
@@ -29,7 +29,12 @@
 		#endregion
 
 		#region Protected Override Methods
-		protected override void Main() => PageCollection.Purge(this.Site, this.titles, this.purgeMethod, 5);
+		protected override void Main()
+		{
+			this.ProgressMaximum = 1;
+			_ = PageCollection.Purge(this.Site, this.titles, this.purgeMethod, 5);
+			this.Progress++;
+		}
 		#endregion
 	}
 }
