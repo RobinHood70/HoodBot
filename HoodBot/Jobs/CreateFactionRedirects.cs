5a4b7310e81944334041cab0844aafcf32ac245a
Robin Hood <robinhood70@live.ca>
2023-08-21 16:25:11 -0400

    New jobs

---
+++ HoodBot/Jobs/CreateFactionRedirects.cs
@@ -0,0 +1,102 @@
+﻿namespace RobinHood70.HoodBot.Jobs
+{
+	using RobinHood70.CommonCode;
+	using RobinHood70.HoodBot.Uesp;
+	using RobinHood70.Robby;
+	using RobinHood70.Robby.Design;
+	using RobinHood70.Robby.Parser;
+	using RobinHood70.WikiCommon.Parser;
+
+	internal sealed class CreateFactionRedirects : EditJob
+	{
+		#region Private Constants
+		private const string SearchPrefix = "Beyond Skyrim:Cyrodiil/Factions ";
+		#endregion
+
+		#region Fields
+		private readonly PageCollection allTitles;
+		private string? nsFull;
+		#endregion
+
+		#region Constructors
+		[JobInfo("Create Faction Redirects")]
+		public CreateFactionRedirects(JobManager jobManager)
+			: base(jobManager)
+		{
+			this.allTitles = new PageCollection(this.Site, PageModules.Info);
+		}
+		#endregion
+
+		#region Protected Override Properties
+		protected override string EditSummary => "Create faction redirect";
+		#endregion
+
+		#region Protected Override Methods
+		protected override void LoadPages()
+		{
+			var searchTitle = TitleFactory.FromUnvalidated(this.Site, SearchPrefix);
+			var fakeNs = new UespNamespaceList(this.Site).FromTitle(searchTitle);
+			this.nsFull = fakeNs.Full;
+
+			var baseTitle = TitleFactory.FromUnvalidated(this.Site, fakeNs.Base);
+			this.allTitles.GetNamespace(baseTitle.Namespace.Id, Filter.Any, fakeNs.IsPseudoNamespace ? baseTitle.PageName + "/" : string.Empty);
+
+			var factionPages = new PageCollection(this.Site);
+			factionPages.GetNamespace(searchTitle.Namespace.Id, Filter.Any, searchTitle.PageName);
+			foreach (var factionPage in factionPages)
+			{
+				var parser = new ContextualParser(factionPage);
+				var sections = parser.ToSections(2);
+				foreach (var section in sections)
+				{
+					if (section.Header is null)
+					{
+						continue;
+					}
+
+					var headerTitle = section.Header.GetTitle(true);
+					this.CreatePage(factionPage, headerTitle, headerTitle);
+					var factionTemplates = section.Content.FindAll<SiteTemplateNode>(template => template.TitleValue.PageNameEquals("Factions"));
+					foreach (var node in factionTemplates)
+					{
+						var edid = node.GetRaw("edid");
+						if (edid != null)
+						{
+							this.CreatePage(factionPage, edid, headerTitle);
+						}
+					}
+				}
+			}
+		}
+
+		protected override void PageLoaded(Page page)
+		{
+		}
+		#endregion
+
+		#region Private Methods
+		private void CreatePage(Page factionPage, string factionName, string section)
+		{
+			if (factionName.Contains("{{", System.StringComparison.Ordinal))
+			{
+				this.StatusWriteLine("Ignoring " + factionName);
+				return;
+			}
+
+			var pageName = this.nsFull + factionName;
+			var site = factionPage.Site;
+			if (this.allTitles.TryGetValue(pageName, out var existingPage) && !existingPage.IsRedirect)
+			{
+				pageName += " (faction)";
+			}
+
+			if (!this.Pages.Contains(pageName))
+			{
+				var page = site.CreatePage(pageName);
+				page.Text = $"#REDIRECT [[{factionPage.FullPageName}#{section}]] [[Category:Redirects to Broader Subjects]] [[Category:Beyond Skyrim-Cyrodiil-Factions]]";
+				this.Pages.Add(page);
+			}
+		}
+		#endregion
+	}
+}
---
+++ HoodBot/Jobs/SFStars.cs
@@ -0,0 +1,59 @@
+﻿namespace RobinHood70.HoodBot.Jobs
+{
+	using System.Collections.Generic;
+	using RobinHood70.CommonCode;
+	using RobinHood70.HoodBot.Jobs.JobModels;
+	using RobinHood70.Robby;
+
+	internal sealed class SFStars : EditJob
+	{
+		#region Fields
+		private readonly Dictionary<string, CsvRow> stars = new(System.StringComparer.Ordinal);
+		#endregion
+
+		#region Constructors
+		[JobInfo("SF Stars")]
+		public SFStars(JobManager jobManager)
+			: base(jobManager)
+		{
+		}
+		#endregion
+
+		#region Protected Override Methods
+		protected override string EditSummary => "Create star";
+
+		protected override void LoadPages()
+		{
+			var fileName = LocalConfig.BotDataSubPath("stars.csv");
+			var stars = new CsvFile();
+			stars.ReadFile(fileName, true);
+			foreach (var star in stars)
+			{
+				this.stars.Add(star["proper"], star);
+			}
+		}
+
+		protected override void PageMissing(Page page)
+		{
+			page.Text = $"{{{{System Infobox\n" +
+			$"|eid={star["id"]}\n" +
+			$"|name={star["proper"]}\n" +
+			$"|class={star["spect"]}\n" +
+			$"|id={star["gl"]}\n" +
+			$"|temp={star["Temp"]}\n" +
+			$"|magnitude={star["absmag"]}\n" +
+			"|image=\n" +
+			"|level=\n" +
+			"|mass=\n" +
+			"|moon=\n" +
+			"|planet=\n" +
+			"|radius=\n" +
+			$"}}}}";
+		}
+
+		protected override void PageLoaded(Page page)
+		{
+		}
+		#endregion
+	}
+}
