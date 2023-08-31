ffb79933f854eaf5fb4f29653570766e2e558c7d
Robin Hood <robinhood70@live.ca>
2023-08-31 10:55:21 -0400

    Factions job

---
+++ HoodBot/Jobs/SFFactions.cs
@@ -0,0 +1,211 @@
+﻿namespace RobinHood70.HoodBot.Jobs
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Globalization;
+	using System.Text;
+	using RobinHood70.CommonCode;
+	using RobinHood70.HoodBot.Jobs.JobModels;
+	using RobinHood70.Robby;
+	using RobinHood70.Robby.Design;
+	using RobinHood70.Robby.Parser;
+
+	internal sealed class SFFactions : CreateOrUpdateJob<SFFactions.Redirect>
+	{
+		#region Constructors
+		[JobInfo("SF Factions")]
+		public SFFactions(JobManager jobManager)
+			: base(jobManager)
+		{
+			Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+		}
+		#endregion
+
+		#region Protected Override Properties
+		protected override string? Disambiguator => "faction";
+
+		protected override string EditSummary => "Create faction page";
+		#endregion
+
+		#region Protected Override Methods
+		protected override bool IsValid(ContextualParser parser, Redirect item) => parser.Page.Text.StartsWith("#REDIRECT", StringComparison.Ordinal);
+
+		protected override IDictionary<Title, Redirect> LoadItems()
+		{
+			var csv = new CsvFile();
+			csv.Load(LocalConfig.BotDataSubPath("Starfield/Factions.csv"), true);
+			var items = this.ParseFactions(csv);
+			var entries = this.GetFactionEntries(csv);
+			var first = entries.First();
+			var letter = char.ToUpper(first.Key[0], CultureInfo.CurrentCulture);
+			var pageName = TitleFactory.FromUnvalidated(this.Site, "Starfield:Factions " + letter);
+			var page = this.Site.CreatePage(pageName);
+			foreach (var kvp in entries)
+			{
+				if (letter != char.ToUpper(kvp.Key[0], CultureInfo.CurrentCulture))
+				{
+					this.Pages.Add(page);
+					letter = char.ToUpper(kvp.Key[0], CultureInfo.CurrentCulture);
+					pageName = TitleFactory.FromUnvalidated(this.Site, "Starfield:Factions " + letter);
+					page = this.Site.CreatePage(pageName);
+				}
+
+				page.Text += $"=={kvp.Key}==\n";
+				var entryList = kvp.Value;
+				entryList.Sort((x, y) => string.Compare(x.EditorId, y.EditorId, StringComparison.Ordinal));
+				foreach (var entry in entryList)
+				{
+					if (entryList.Count > 1)
+					{
+						page.Text += $"==={entry.Header}===\n";
+					}
+
+					var sb = new StringBuilder();
+					sb
+						.Append("{{Factions\n")
+						.Append($"|edid={entry.EditorId}\n")
+						.Append($"|formid={entry.FormId}\n");
+					if (entry.Members.Count > 0)
+					{
+						sb
+							.Append("|members={{Factions/Members\n")
+							.Append("  |members={{List|sep=,&#32;\n");
+						entry.Members.Sort();
+						foreach (var member in entry.Members)
+						{
+							sb
+								.Append("    |[[")
+								.Append(member.FullPageName())
+								.Append("|]]\n");
+						}
+
+						sb.Append("}}}}");
+					}
+
+					sb.Append("}}\n\n");
+					page.Text += sb.ToString();
+				}
+			}
+
+			if (page.Text.Length > 0)
+			{
+				page.Text = "{{Faction Header}}\n";
+				this.Pages.Add(page);
+			}
+
+			return items;
+		}
+
+		protected override string NewPageText(Title title, Redirect item)
+		{
+			var retval = $"#REDIRECT [[Starfield:Factions {item.Letter}#{item.Section}]] [[Category:Redirects to Broader Subjects]] [[Category:Starfield-Factions]]";
+			var origName = (this.Disambiguator is not null && title.PageName.EndsWith(" (" + this.Disambiguator + ")", StringComparison.Ordinal))
+				? title.PageName[..(this.Disambiguator.Length + 3)]
+				: title.PageName;
+			var data = string.Empty;
+			if (item.AltName is not null)
+			{
+				data += "|altname=" + item.AltName;
+			}
+
+			if (origName.Contains('(', StringComparison.Ordinal))
+			{
+				data += "|catname=" + origName;
+			}
+
+			if (data.Length > 0)
+			{
+				retval += "\n{{Faction Data" + data + "}}";
+			}
+
+			return retval;
+		}
+		#endregion
+
+		#region Private Classes
+		private IReadOnlyDictionary<string, TitleCollection> GetNPCs()
+		{
+			var npcs = new TitleCollection(this.Site);
+			npcs.GetCategoryMembers("Starfield-NPCs");
+			var members = new Dictionary<string, TitleCollection>(StringComparer.Ordinal);
+			var npcsFile = new CsvFile();
+			npcsFile.Load(LocalConfig.BotDataSubPath("Starfield/Npcs.csv"), true, Encoding.GetEncoding(1252));
+			{
+				foreach (var row in npcsFile)
+				{
+					var name = row["Name"];
+					if (name.Length > 0)
+					{
+						var factionText = row["Factions"];
+						var factions = factionText.Length == 0
+							? Array.Empty<string>()
+							: factionText.Split(TextArrays.Comma);
+						var title =
+							npcs.TryGetValue("Starfield:" + name + " (NPC)", out var page) ? page :
+							npcs.TryGetValue("Starfield:" + name, out page) ? page :
+							throw new KeyNotFoundException();
+						foreach (var faction in factions)
+						{
+							var memberList = members.TryGetValue(faction, out var list) ? list : new TitleCollection(this.Site);
+							memberList.TryAdd(title);
+							members[faction] = memberList;
+						}
+					}
+				}
+			}
+
+			return members;
+		}
+
+		private IDictionary<Title, Redirect> ParseFactions(CsvFile csv)
+		{
+			var items = new Dictionary<Title, Redirect>();
+			foreach (var row in csv)
+			{
+				var name = row["Name"]
+					.Replace("[", string.Empty, StringComparison.Ordinal)
+					.Replace("]", string.Empty, StringComparison.Ordinal);
+				var edid = row["EditorID"];
+				var prefName = name.Length > 0 ? name : edid;
+				var altName = name.Length > 0 && !string.Equals(name, edid, StringComparison.Ordinal) ? name : null;
+				if (name.Length > 0)
+				{
+					items.TryAdd(TitleFactory.FromUnvalidated(this.Site, "Starfield:" + name), new Redirect(name[0], name, null));
+				}
+
+				items.TryAdd(TitleFactory.FromUnvalidated(this.Site, "Starfield:" + edid), new Redirect(prefName[0], edid, altName));
+			}
+
+			return items;
+		}
+
+		private IReadOnlyDictionary<string, List<Entry>> GetFactionEntries(CsvFile csv)
+		{
+			var npcs = this.GetNPCs();
+			var entries = new SortedDictionary<string, List<Entry>>(StringComparer.OrdinalIgnoreCase);
+			foreach (var row in csv)
+			{
+				var name = row["Name"]
+					.Replace("[", string.Empty, StringComparison.Ordinal)
+					.Replace("]", string.Empty, StringComparison.Ordinal);
+				var edid = row["EditorID"];
+				var factionMembers = npcs.TryGetValue(edid, out var membs) ? membs : new TitleCollection(this.Site);
+				var entry = new Entry(edid, row["FormID"][2..], edid, factionMembers);
+				var prefName = name.Length > 0 ? name : edid;
+				var entryList = entries.TryGetValue(prefName, out var list) ? list : new List<Entry>();
+				entryList.Add(entry);
+				entries[prefName] = entryList;
+			}
+
+			return entries;
+		}
+
+		#endregion
+
+		#region Internal Classes
+		internal sealed record Entry(string Header, string FormId, string EditorId, TitleCollection Members);
+
+		internal sealed record Redirect(char Letter, string Section, string? AltName);
+		#endregion
+	}
+}
