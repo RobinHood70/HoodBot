32a4c31a0290cd68c768cec1fd3f28c89c988898
Robin Hood <robinhood70@live.ca>
2023-08-31 11:48:05 -0400

    Old version of planets job

---
+++ HoodBot/Jobs/SFGalaxy_Old.cs
@@ -0,0 +1,89 @@
+﻿namespace RobinHood70.HoodBot.Jobs
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Text;
+	using RobinHood70.CommonCode;
+	using RobinHood70.HoodBot.Jobs.JobModels;
+	using RobinHood70.Robby;
+	using RobinHood70.Robby.Design;
+	using RobinHood70.Robby.Parser;
+
+	internal sealed class SFGalaxy_Old : CreateOrUpdateJob<CsvRow>
+	{
+		#region Fields
+		private readonly Dictionary<string, string> stars = new(StringComparer.Ordinal);
+		#endregion
+
+		#region Constructors
+		[JobInfo("SF Galaxy")]
+		public SFGalaxy_Old(JobManager jobManager)
+			: base(jobManager)
+		{
+			Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+		}
+		#endregion
+
+		#region Protected Override Properties
+		protected override string? Disambiguator => "planet";
+
+		protected override string EditSummary => "Create planet page";
+		#endregion
+
+		#region Protected Override Methods
+		protected override bool IsValid(ContextualParser parser, CsvRow data) => parser.FindSiteTemplate("Planet Infobox") is not null;
+
+		protected override IDictionary<Title, CsvRow> LoadItems()
+		{
+			var csv = new CsvFile();
+			csv.Load(LocalConfig.BotDataSubPath("Starfield/stars.csv"), true, Encoding.GetEncoding(1252));
+			foreach (var row in csv)
+			{
+				this.stars.Add(row["FormID"], row["Name"]);
+			}
+
+			csv = new CsvFile()
+			{
+				SkipLines = 1
+			};
+			var items = new Dictionary<Title, CsvRow>();
+			csv.Load(LocalConfig.BotDataSubPath("Starfield/galaxy.csv"), true, Encoding.GetEncoding(1252));
+			foreach (var item in csv)
+			{
+				var name = "Starfield:" + item["Name"];
+				items.Add(TitleFactory.FromUnvalidated(this.Site, name), item);
+			}
+
+			return items;
+		}
+
+		protected override string NewPageText(Title title, CsvRow item)
+		{
+			var starName = this.stars[item["Star ID"]];
+			var starType = item["Type"]
+				.Replace("G.", "Giant", StringComparison.Ordinal)
+				.Replace("Aster.", "Asteroid", StringComparison.Ordinal);
+			var magnetosphere = item["Mag. Field"];
+			if (string.IsNullOrWhiteSpace(magnetosphere))
+			{
+				magnetosphere = "Unknown";
+			}
+
+			return "{{Planet Infobox\n" +
+			"|image=\n" +
+			$"|system={starName}\n" +
+			$"|type={starType}\n" +
+			$"|gravity={item["Gravity"]}\n" +
+			"|temp=\n" +
+			"|atmosphere=\n" +
+			$"|magnetosphere={magnetosphere}\n" +
+			"|fauna=\n" +
+			"|flora=\n" +
+			"|water=\n" +
+			"|resource=\n" +
+			"|trait=\n" +
+			"}}\n\n{{Stub|Planet}}";
+		}
+		#endregion
+	}
+}
