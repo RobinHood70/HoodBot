5f559de7ebbbbf0552df552036a9046d127b7018
Robin Hood <robinhood70@live.ca>
2023-11-07 20:45:04 -0500

    Remove Endless Archive

--- HoodBot/Jobs/OneOffParseJob.cs
+++ HoodBot/Jobs/OneOffParseJob.cs
@@ -1,7 +1,9 @@
 ﻿namespace RobinHood70.HoodBot.Jobs
 {
+	using System;
 	using System.Diagnostics;
 	using RobinHood70.CommonCode;
+	using RobinHood70.HoodBot.Uesp;
 	using RobinHood70.Robby;
 	using RobinHood70.Robby.Parser;
 	using RobinHood70.WikiCommon;
@@ -18,7 +20,7 @@
 		#endregion
 
 		#region Public Override Properties
-		public override string LogDetails => "Remove scrollboxes on short entries";
+		public override string LogDetails => "Remove Endless Archive header";
 
 		public override string LogName => "One-Off Parse Job";
 		#endregion
@@ -26,39 +28,36 @@
 		#region Protected Override Methods
 		protected override string GetEditSummary(Page page) => this.LogDetails;
 
-		protected override void LoadPages() => this.Pages.GetBacklinks("Template:Scrollbox", BacklinksTypes.EmbeddedIn, true, Filter.Exclude);
+		protected override void LoadPages() => this.Pages.GetBacklinks("Template:Mod Header", BacklinksTypes.EmbeddedIn, true, Filter.Exclude, UespNamespaces.Online);
 
 		protected override void ParseText(ContextualParser parser)
 		{
-			parser.Replace(node => Replacer(node, parser), false);
-			parser.MergeText(true);
-			parser.ReplaceText("\n\n\n", "\n\n", System.StringComparison.Ordinal);
-		}
-
-		private static NodeCollection? Replacer(IWikiNode node, ContextualParser parser)
-		{
-			if (node is not SiteTemplateNode template ||
-			!template.TitleValue.PageNameEquals("Scrollbox") ||
-			template.Find("content") is not IParameterNode contentNode)
+			for (var templateOffset = parser.Count - 1; templateOffset >= 0; templateOffset--)
 			{
-				return null;
-			}
-
-			var content = contentNode.Value.ToRaw().Trim();
-			var contentLines = content.Split(TextArrays.NewLineChars);
-			var numOccurrences = (contentLines.Length - 1).ToStringInvariant();
-			if (!contentLines[0].Contains("Appearances: " + numOccurrences, System.StringComparison.Ordinal) &&
-				!contentLines[0].Contains("Discounts: " + numOccurrences, System.StringComparison.Ordinal))
-			{
-				Debug.WriteLine("Possible appearances mismatch on " + parser.Page.Title.FullPageName());
-			}
-
-			return (contentLines.Length > 5 || content.Length > 1000)
-				? null
-				: new NodeCollection(parser.Factory)
+				if (parser[templateOffset] is SiteTemplateNode template &&
+					template.TitleValue.PageNameEquals("Mod Header") &&
+				string.Equals(template.GetValue(1), "Endless Archive", StringComparison.Ordinal))
 				{
-					parser.Factory.TextNode(content)
-				};
+					// var archive = ((SiteTemplateNode)parser[templateOffset]).GetValue(1);
+					Debug.WriteLine(parser.Page.Title.FullPageName());
+					parser.RemoveAt(templateOffset);
+					if (parser.Count >= templateOffset && parser[templateOffset] is ITextNode textNode)
+					{
+						textNode.Text = textNode.Text.TrimStart(' ');
+						if (textNode.Text.Length > 0 && textNode.Text[0] == '\n')
+						{
+							if (textNode.Text.Length > 1)
+							{
+								textNode.Text = textNode.Text[1..];
+							}
+							else
+							{
+								parser.RemoveAt(templateOffset);
+							}
+						}
+					}
+				}
+			}
 		}
 		#endregion
 	}
