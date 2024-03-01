8a0bcbfe02ee0fdecb56c1fdcadc3e0b4fefa5b1
Robin Hood <robinhood70@live.ca>
2024-03-01 17:10:31 -0500

    Check if page only links to Lore:Names derivatives

---
+++ HoodBot/Jobs/OrphanedNames.cs
@@ -0,0 +1,69 @@
+﻿namespace RobinHood70.HoodBot.Jobs
+{
+	using System.Diagnostics;
+	using RobinHood70.CommonCode;
+	using RobinHood70.HoodBot.Uesp;
+	using RobinHood70.Robby;
+	using RobinHood70.Robby.Design;
+
+	[method: JobInfo("Orphaned Names")]
+	internal sealed class OrphanedNames(JobManager jobManager) : WikiJob(jobManager, JobType.ReadOnly)
+	{
+		#region Protected Override Methods
+		protected override void Main()
+		{
+			var namesPages = new TitleCollection(
+				  this.Site,
+				  this.Site[UespNamespaces.Lore].Id,
+				  "Names",
+				  "Altmer Names",
+				  "Argonian Names",
+				  "Bosmer Names",
+				  "Breton Names",
+				  "Daedra Names",
+				  "Dunmer Names",
+				  "Imperial Names",
+				  "Khajiit Names",
+				  "Nord Names",
+				  "Orc Names",
+				  "Reachman Names",
+				  "Redguard Names");
+			var namePages = new PageCollection(this.Site, PageModules.Links);
+			namePages.GetTitles(namesPages);
+			var nameLinks = new TitleCollection(this.Site);
+			foreach (var page in namePages)
+			{
+				foreach (var link in page.Links)
+				{
+					nameLinks.TryAdd(link);
+				}
+			}
+
+			nameLinks.Remove(namesPages);
+			var nameBackLinkPages = new PageCollection(this.Site, PageModules.Info | PageModules.Backlinks)
+			{
+				AllowRedirects = Filter.Exclude
+			};
+
+			nameBackLinkPages.GetTitles(nameLinks);
+			foreach (var page in nameBackLinkPages)
+			{
+				var hasOtherLinks = false;
+				foreach (var link in page.Backlinks)
+				{
+					if (link.Value == WikiCommon.BacklinksTypes.Backlinks && !namesPages.Contains(link.Key))
+					{
+						hasOtherLinks = true;
+						break;
+					}
+				}
+
+				if (!hasOtherLinks)
+				{
+					Debug.WriteLine(page.Title.FullPageName());
+				}
+			}
+		}
+		#endregion
+	}
+}
