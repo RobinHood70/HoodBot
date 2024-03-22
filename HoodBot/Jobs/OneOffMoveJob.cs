baeebb10c2f1702c6819a0770ea9076f73e2cb31
Robin Hood <robinhood70@live.ca>
2024-03-21 21:27:00 -0400

    Battleaxe moves

--- HoodBot/Jobs/OneOffMoveJob.cs
+++ HoodBot/Jobs/OneOffMoveJob.cs
@@ -1,38 +1,33 @@
 ﻿namespace RobinHood70.HoodBot.Jobs
 {
+	using RobinHood70.CommonCode;
 	using RobinHood70.Robby;
-	using RobinHood70.Robby.Design;
+	using RobinHood70.WikiCommon;
 
-	public class OneOffMoveJob : MovePagesJob
+	[method: JobInfo("One-Off Move Job")]
+	public class OneOffMoveJob(JobManager jobManager, bool updateUserSpace) : MovePagesJob(jobManager, updateUserSpace)
 	{
-		#region Constructors
-		[JobInfo("One-Off Move Job")]
-		public OneOffMoveJob(JobManager jobManager, bool updateUserSpace)
-				: base(jobManager, updateUserSpace)
-		{
-			this.MoveAction = MoveAction.None;
-			this.SuppressRedirects = false;
-			this.FollowUpActions =
-				FollowUpActions.CheckLinksRemaining |
-				FollowUpActions.EmitReport |
-				FollowUpActions.FixLinks |
-				FollowUpActions.RetainDirectLinkText;
-			// this.Site.WaitForJobQueue();
-		}
-		#endregion
-
 		#region Protected Override Methods
-		protected override void PopulateMoves() => this.AddLinkUpdate("Online:Pets", "Online:Non-Combat Pets");
-
-		protected override SiteLink GetToLink(Page page, bool isRedirectTarget, SiteLink from, Title to)
+		protected override void BeforeMain()
 		{
-			if (from.Fragment is not null)
-			{
-				to = TitleFactory.FromValidated(to.Namespace, from.Fragment);
-				from.Fragment = null;
-			}
+			//// this.MoveAction = MoveAction.MoveSafely;
+			//// this.SuppressRedirects = false;
+			//// this.FollowUpActions = FollowUpActions.FixLinks | FollowUpActions.RetainDirectLinkText;
+			this.Site.WaitForJobQueue();
+			base.BeforeMain();
+		}
 
-			return base.GetToLink(page, isRedirectTarget, from, to);
+		protected override string GetEditSummary(Page page) => "Harmonize Battle Axe spelling";
+
+		protected override void PopulateMoves()
+		{
+			var titles = new TitleCollection(this.Site);
+			titles.GetNamespace(MediaWikiNamespaces.File, Filter.Exclude, "ON-icon-weapon-Battleaxe");
+			foreach (var title in titles)
+			{
+				var newName = title.FullPageName().Replace("Battleaxe", "Battle Axe", false, this.Site.Culture);
+				this.AddMove(title, newName);
+			}
 		}
 
 		//// this.AddLinkUpdate("Category:Online-Furnishings", "Category:Online-Furnishing Images");
