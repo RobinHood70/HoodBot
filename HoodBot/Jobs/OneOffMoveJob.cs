e6d46b6cfb71677e354db9444fb6a364dd57e917
Robin Hood <robinhood70@live.ca>
2024-04-29 14:12:46 -0400

    Link past and delete multiple redirects

--- HoodBot/Jobs/OneOffMoveJob.cs
+++ HoodBot/Jobs/OneOffMoveJob.cs
@@ -1,6 +1,10 @@
 ﻿namespace RobinHood70.HoodBot.Jobs
 {
+	using RobinHood70.CommonCode;
 	using RobinHood70.Robby;
+	using RobinHood70.Robby.Design;
+	using RobinHood70.WallE.Base;
+	using RobinHood70.WikiCommon;
 
 	[method: JobInfo("One-Off Move Job")]
 	public class OneOffMoveJob(JobManager jobManager, bool updateUserSpace) : MovePagesJob(jobManager, updateUserSpace)
@@ -9,17 +13,27 @@
 		protected override void BeforeMain()
 		{
 			this.MoveAction = MoveAction.None;
-			//// this.SuppressRedirects = false;
-			this.FollowUpActions = FollowUpActions.FixLinks | FollowUpActions.UpdateCategoryMembers;
-			this.Site.WaitForJobQueue();
 			base.BeforeMain();
 		}
 
-		protected override string GetEditSummary(Page page) => "Fix incorrect category";
+		protected override string GetEditSummary(Page page) => "Link past redirect";
+
+		protected override void OnFromPageOrphaned(Page page)
+		{
+			if (page.Exists)
+			{
+				page.Title.Delete("Unnecessary redirect");
+			}
+		}
 
 		protected override void PopulateMoves()
 		{
-			this.AddLinkUpdate("Category:Redirects to Alternate Names", "Category:Redirects from Alternate Names");
+			var redirects = new PageCollection(this.Site, PageModules.Links);
+			redirects.GetNamespace(MediaWikiNamespaces.File, Filter.Only, "ON-icon-lead-");
+			foreach (var page in redirects)
+			{
+				this.AddLinkUpdate(page.Title, page.Links[0]);
+			}
 		}
 
 		//// this.AddLinkUpdate("Category:Online-Furnishings", "Category:Online-Furnishing Images");
