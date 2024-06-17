19b875ec1779b5e34f1a622c89e6382d63653731
Robin Hood <robinhood70@live.ca>
2024-06-17 12:35:05 -0400

    Suppress warning

--- HoodBot/Jobs/JobParameterAttribute.cs
+++ HoodBot/Jobs/JobParameterAttribute.cs
@@ -1,8 +1,10 @@
 ﻿namespace RobinHood70.HoodBot.Jobs
 {
 	using System;
+	using System.Diagnostics.CodeAnalysis;
 
 	[AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = true)]
+	[SuppressMessage("Performance", "CA1813:Avoid unsealed attributes", Justification = "Inherited.")]
 	public class JobParameterAttribute : Attribute
 	{
 		public object? DefaultValue { get; set; }
