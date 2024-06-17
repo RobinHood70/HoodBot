945623f2461aa3455c1f7193d42e66f11f6bb00a
Robin Hood <robinhood70@live.ca>
2024-06-17 12:34:12 -0400

    New job

---
+++ HoodBot/Jobs/PurgeSpecialList.cs
@@ -0,0 +1,16 @@
+﻿namespace RobinHood70.HoodBot.Jobs
+{
+	using RobinHood70.Robby;
+	using RobinHood70.WikiCommon;
+
+	[method: JobInfo("Purge List")]
+	internal sealed class PurgeSpecialList(JobManager jobManager) : WikiJob(jobManager, JobType.UnloggedWrite)
+	{
+		protected override void Main()
+		{
+			var titles = new TitleCollection(this.Site);
+			titles.GetQueryPage("Uncategorizedimages");
+			_ = PageCollection.Purge(this.Site, titles, PurgeMethod.LinkUpdate, 50);
+		}
+	}
+}
