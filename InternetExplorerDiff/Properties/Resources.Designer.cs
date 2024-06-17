fdaf659ac2b8aa76684c43433733c22584215367
Robin Hood <robinhood70@live.ca>
2024-06-17 12:31:40 -0400

    Update from generated code

--- InternetExplorerDiff/Properties/Resources.Designer.cs
+++ InternetExplorerDiff/Properties/Resources.Designer.cs
@@ -19,10 +19,10 @@ namespace RobinHood70.InternetExplorerDiff.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal sealed class Resources {
+    internal class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
