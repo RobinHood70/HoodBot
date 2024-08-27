de9e23ac06a7179b90d3ef4bc361add6c278ca4e
Robin Hood <robinhood70@live.ca>
2024-08-26 21:07:53 -0400

    Fix namespace and minor updates

--- HoodBot/Jobs/UploadAntiquityLeads.cs
+++ HoodBot/Jobs/UploadAntiquityLeads.cs
@@ -1,11 +1,13 @@
-﻿namespace RobinHood70.HoodBot.Jobs.JobModels
+﻿namespace RobinHood70.HoodBot.Jobs
 {
 	using System;
 	using System.Collections.Generic;
 	using System.Data;
 	using System.Diagnostics;
+	using System.Globalization;
 	using RobinHood70.CommonCode;
 	using RobinHood70.HoodBot.Design;
+	using RobinHood70.HoodBot.Jobs.JobModels;
 	using RobinHood70.HoodBot.Uesp;
 	using RobinHood70.Robby;
 	using RobinHood70.Robby.Design;
@@ -13,11 +15,16 @@
 	using RobinHood70.WikiCommon;
 	using RobinHood70.WikiCommon.Parser;
 
-	[method: JobInfo("Upload Antiquity Leads")]
-	internal sealed class UploadAntiquityLeads(JobManager jobManager) : EditJob(jobManager)
+	[method: JobInfo("Upload Antiquity Leads", "ESO Update")]
+	internal sealed class UploadAntiquityLeads(JobManager jobManager) : TemplateJob(jobManager)
 	{
 		#region Fields
-		private readonly Dictionary<string, Page> originalFileNames = [];
+		private readonly Dictionary<string, Page> filePages = [];
+		private readonly Dictionary<int, Lead> leads = [];
+		#endregion
+
+		#region Protected Override Properties
+		protected override string TemplateName => "Online Furnishing Antiquity/Row";
 		#endregion
 
 		#region Protected Override Methods
@@ -25,33 +32,23 @@
 
 		protected override void LoadPages()
 		{
-			this.Shuffle = true;
-			var uesp = (UespSite)this.Site;
-			var filePages = uesp.CreateMetaPageCollection(PageModules.Default | PageModules.Custom, true, false, "originalfile");
-			filePages.SetLimitations(LimitationType.OnlyAllow, UespNamespaces.File);
-			filePages.GetCustomGenerator(new VariablesInput() { Variables = ["originalfile"] });
-			foreach (var page in filePages)
-			{
-				var varPage = (VariablesPage)page;
-				if (varPage.GetVariable("originalfile") is string origFile)
-				{
-					var split = origFile.Split(TextArrays.Comma);
-					foreach (var name in split)
-					{
-						var trimmed = name.Trim();
-						this.originalFileNames.TryAdd(trimmed, page);
-					}
-				}
-			}
+			var patchVersion = new EsoVersion(43, true);
+			this.GetIcons(patchVersion.Text, false);
+			this.GetFilePages();
 
 			var leadFiles = new TitleCollection(this.Site);
 			leadFiles.GetNamespace(MediaWikiNamespaces.File, Filter.Only, Lead.Prefix);
 
-			var leads = Database.RunQuery(EsoLog.Connection, "SELECT name, icon, setName FROM antiquityLeads", row => new Lead(row));
-			var redirects = new Dictionary<Title, Page>();
-			foreach (var lead in leads)
+			var query = Database.RunQuery(EsoLog.Connection, "SELECT icon, id, name, setName FROM antiquityLeads", row => new Lead(row));
+			foreach (var item in query)
 			{
-				if (this.originalFileNames.TryGetValue(lead.Icon, out var page))
+				this.leads.Add(item.Id, item);
+			}
+
+			var redirects = new Dictionary<Title, Page>();
+			foreach (var (key, lead) in this.leads)
+			{
+				if (this.filePages.TryGetValue(lead.Icon, out var page))
 				{
 					var leadTitle = TitleFactory.FromUnvalidated(this.Site, lead.FileTitle);
 					if (!leadFiles.Contains(leadTitle))
@@ -65,7 +62,8 @@
 				}
 				else
 				{
-					this.UploadFile(lead);
+					var filePage = this.UploadFile(lead);
+					this.filePages.Add(lead.Icon, filePage);
 				}
 			}
 
@@ -73,28 +71,53 @@
 			{
 				if (this.Pages.TryGetValue(kvp.Value, out var page))
 				{
-					this.CreateRedirect(kvp.Key, page);
+					UpdateFilePage(kvp.Key, page);
 				}
 				else
 				{
-					this.CreateRedirect(kvp.Key, kvp.Value);
+					UpdateFilePage(kvp.Key, kvp.Value);
 					this.Pages.Add(kvp.Value);
 				}
 			}
+
+			this.Pages.GetBacklinks("Template:Online Furnishing Antiquity/Row", BacklinksTypes.EmbeddedIn, false, Filter.Exclude);
 		}
 
-		protected override void PageLoaded(Page page) => throw new NotSupportedException();
+		protected override void ParseTemplate(SiteTemplateNode template, ContextualParser parser)
+		{
+			string? idText = template.GetValue("id");
+			if (idText is not null)
+			{
+				var id = int.Parse(idText, CultureInfo.CurrentCulture);
+				var lead = this.leads[id];
+				var iconRemote = lead.Icon;
+				if (!this.filePages.TryGetValue(iconRemote, out var icon))
+				{
+					return;
+				}
+
+				var title = icon.Title;
+				var defaultTitle = $"ON-icon-lead-{parser.Page.Title.PageName}.png";
+				if (!string.Equals(title.PageName, defaultTitle, StringComparison.Ordinal))
+				{
+					template.Update("icon", title.PageName);
+				}
+			}
+		}
 		#endregion
 
-		#region Private Methods
-		private void CreateRedirect(Title from, Page to)
+		#region Private Static Methods
+		private static int PairedComparer((string Key, string Value) x, (string Key, string Value) y)
 		{
-			var page = this.Site.CreatePage(from);
-			page.Text = "#REDIRECT [[" + to.Title.FullPageName() + "]][[Category:Redirects to Alternate Names]][[Category:Online-Icons-Antiquity Leads]]";
-			this.Pages.Add(page);
+			var compare = string.Compare(x.Key, y.Key, StringComparison.OrdinalIgnoreCase);
+			return compare == 0
+				? string.Compare(x.Value, y.Value, StringComparison.OrdinalIgnoreCase)
+				: compare;
+		}
 
+		private static void UpdateFilePage(Title from, Page to)
+		{
 			var parser = new ContextualParser(to);
-			var anons = new List<(IParameterNode Key, IParameterNode Value)>();
 			if (parser.FindSiteTemplate("Online File") is not SiteTemplateNode template)
 			{
 				Debug.WriteLine("Template not found on " + to.Title.FullPageName());
@@ -109,6 +132,7 @@
 
 			var i = 0;
 			IParameterNode? single = null;
+			var anons = new List<(string Key, string Value)>();
 			while (i < template.Parameters.Count)
 			{
 				var param = template.Parameters[i];
@@ -116,7 +140,9 @@
 				{
 					if (i < (template.Parameters.Count - 1))
 					{
-						anons.Add((param, template.Parameters[i + 1]));
+						var key = param.Value.ToRaw();
+						var value = template.Parameters[i + 1].Value.ToRaw().Trim();
+						anons.Add((key, value));
 					}
 					else
 					{
@@ -136,10 +162,13 @@
 				}
 			}
 
-			anons.Add((
-				parser.Factory.ParameterNodeFromParts("Lead"),
-				parser.Factory.ParameterNodeFromParts(Lead.ReverseTitle(from.PageName) + '\n')));
-			anons.Sort(this.PairedComparer);
+			var trimmed = Lead.TrimCruft(from.PageName);
+			if (!anons.Contains(("Lead", trimmed)))
+			{
+				anons.Add(("Lead", trimmed));
+			}
+
+			anons.Sort(PairedComparer);
 
 			for (i = template.Parameters.Count - 1; i >= 0; i--)
 			{
@@ -151,8 +180,8 @@
 
 			foreach (var (key, value) in anons)
 			{
-				template.Parameters.Add(key);
-				template.Parameters.Add(value);
+				template.Add(key, ParameterFormat.Packed);
+				template.Add(value, ParameterFormat.OnePerLine);
 			}
 
 			if (single is not null)
@@ -162,16 +191,31 @@
 
 			parser.UpdatePage();
 		}
+		#endregion
 
-		private int PairedComparer((IParameterNode Key, IParameterNode Value) x, (IParameterNode Key, IParameterNode Value) y)
+		#region Private Methods
+		private void GetFilePages()
 		{
-			var compare = string.Compare(x.Key.Value.ToRaw(), y.Key.Value.ToRaw(), StringComparison.OrdinalIgnoreCase);
-			return compare == 0
-				? string.Compare(x.Value.Value.ToRaw(), y.Value.Value.ToRaw(), StringComparison.OrdinalIgnoreCase)
-				: compare;
+			var uesp = (UespSite)this.Site;
+			var pages = uesp.CreateMetaPageCollection(PageModules.Default | PageModules.Custom, true, false, "originalfile");
+			pages.SetLimitations(LimitationType.OnlyAllow, UespNamespaces.File);
+			pages.GetCustomGenerator(new VariablesInput() { Variables = ["originalfile"] });
+			foreach (var page in pages)
+			{
+				var varPage = (VariablesPage)page;
+				if (varPage.GetVariable("originalfile") is string origFile)
+				{
+					var split = origFile.Split(TextArrays.Comma);
+					foreach (var name in split)
+					{
+						var trimmed = name.Trim();
+						this.filePages.TryAdd(trimmed, page);
+					}
+				}
+			}
 		}
 
-		private void UploadFile(Lead lead)
+		private Page UploadFile(Lead lead)
 		{
 			var pageText =
 				$"{{{{Online File\n" +
@@ -188,7 +232,7 @@
 				this.StatusWriteLine("File not found: " + fileName);
 			}
 
-			this.originalFileNames.Add(lead.Icon, page);
+			return page;
 		}
 		#endregion
 
@@ -204,13 +248,22 @@
 
 			public string Icon { get; } = ((string)row["icon"])[1..^4];
 
+			public int Id { get; } = (int)row["id"];
+
 			public string Name { get; } = (string)row["name"];
 
 			public string SetName { get; } = (string)row["setName"];
 			#endregion
 
 			#region Public Static Functions
-			public static string ReverseTitle(string title) => title[Prefix.Length..^4];
+			public static string TrimCruft(string title)
+			{
+				title = title[Prefix.Length..];
+				var revIndex = title.LastIndexOf('.');
+				return revIndex == -1
+					? throw new InvalidOperationException()
+					: title[..revIndex];
+			}
 			#endregion
 		}
 		#endregion
