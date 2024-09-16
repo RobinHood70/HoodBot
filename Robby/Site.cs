f32c5d7c39b5c721d22e3bb968104de20ca2cd19
Robin Hood <robinhood70@live.ca>
2024-09-15 21:30:27 -0400

    Stop auto-ignoring upload errors

--- Robby/Site.cs
+++ Robby/Site.cs
@@ -833,7 +833,7 @@
 		/// <remarks>The destination filename will be the same as the local filename.</remarks>
 		/// <exception cref="ArgumentException">Path contains an invalid character.</exception>
 		/// <returns>A value indicating the change status of the upload.</returns>
-		public ChangeStatus Upload(string fileName, string editSummary) => this.Upload(fileName, null, editSummary, null);
+		public ChangeStatus Upload(string fileName, string editSummary) => this.Upload(fileName, null, editSummary, null, false);
 
 		/// <summary>Upload a file to the wiki.</summary>
 		/// <param name="fileName">The full path and filename of the file to upload.</param>
@@ -842,16 +842,16 @@
 		/// <remarks>The destination filename will be the same as the local filename.</remarks>
 		/// <exception cref="ArgumentException">Path contains an invalid character.</exception>
 		/// <returns>A value indicating the change status of the upload.</returns>
-		public ChangeStatus Upload(string fileName, string destinationName, string editSummary) => this.Upload(fileName, destinationName, editSummary, null);
+		public ChangeStatus Upload(string fileName, string destinationName, string editSummary) => this.Upload(fileName, destinationName, editSummary, null, false);
 
 		/// <summary>Upload a file to the wiki.</summary>
 		/// <param name="fileName">The full path and filename of the file to upload.</param>
 		/// <param name="destinationName">The bare name (i.e., do not include "File:") of the file to upload to on the wiki. Set to null to use the filename from the <paramref name="fileName" /> parameter.</param>
 		/// <param name="editSummary">The edit summary for the upload.</param>
-		/// <param name="pageText">Full page text for the File page. This should include the license, categories, and anything else required. Set to null to allow the wiki to generate the page text (normally just the <paramref name="editSummary" />).</param>
+		/// <param name="pageText">Full page text for the File page. This should include the license, categories, and anything else required. Set to null  if this is a new version of an existing file or to allow the wiki to generate the page text (normally just the <paramref name="editSummary" />).</param>
 		/// <exception cref="ArgumentException">Path contains an invalid character.</exception>
 		/// <returns>A value indicating the change status of the upload.</returns>
-		public ChangeStatus Upload(string fileName, string? destinationName, string editSummary, string? pageText)
+		public ChangeStatus Upload(string fileName, string? destinationName, string editSummary, string? pageText, bool ignoreWarnings)
 		{
 			ArgumentNullException.ThrowIfNull(fileName);
 			ArgumentNullException.ThrowIfNull(editSummary);
@@ -880,7 +880,7 @@
 				using FileStream upload = new(fileName, FileMode.Open, FileAccess.Read, FileShare.Read);
 				UploadInput uploadInput = new(destinationName!, upload)
 				{
-					IgnoreWarnings = true,
+					IgnoreWarnings = ignoreWarnings,
 					Comment = editSummary,
 				};
 
