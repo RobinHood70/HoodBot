f62a870a15ebefefd2cd3db79605555ea27363e3
Robin Hood <robinhood70@live.ca>
2024-09-30 16:43:04 -0400

    Under construction

---
+++ HoodBot/Jobs/FindAllEsoIcons.cs
@@ -0,0 +1,38 @@
+﻿namespace RobinHood70.HoodBot.Jobs
+{
+	using System.Diagnostics;
+	using RobinHood70.HoodBot.Jobs.JobModels;
+	using RobinHood70.Robby;
+	using RobinHood70.Robby.Design;
+	using RobinHood70.WikiCommon;
+
+	[method: JobInfo("Find All ESO Icons", "ESO")]
+	internal sealed class FindAllEsoIcons(JobManager jobManager) : EditJob(jobManager)
+	{
+		protected override string GetEditSummary(Page page) => "Add Online File";
+
+		protected override void LoadPages()
+		{
+			var checksums = EsoSpace.GetIconChecksums();
+			var pages = new PageCollection(this.Site, new PageLoadOptions(PageModules.Info | PageModules.FileInfo));
+			pages.GetNamespace(MediaWikiNamespaces.File, CommonCode.Filter.Exclude, "ON-icon-achievement-R");
+			foreach (var page in pages)
+			{
+				if (page is FilePage file)
+				{
+					foreach (var fileRev in file.FileRevisions)
+					{
+						if (fileRev.Sha1 is not null && checksums.TryGetValue(fileRev.Sha1, out var fileList))
+						{
+							Debug.WriteLine(file.Title.FullPageName() + " matched " + string.Join(", ", fileList));
+						}
+					}
+				}
+			}
+		}
+
+		protected override void PageLoaded(Page page)
+		{
+		}
+	}
+}
---
+++ HoodBot/Jobs/SFPlanets.cs
@@ -0,0 +1,38 @@
+﻿namespace RobinHood70.HoodBot.Jobs
+{
+	using System;
+	using System.Collections.Generic;
+	using RobinHood70.CommonCode;
+	using RobinHood70.Robby;
+	using RobinHood70.Robby.Parser;
+
+	internal sealed class SFPlanets : CreateOrUpdateJob<CsvRow>
+	{
+		public SFPlanets(JobManager jobManager)
+			: base(jobManager)
+		{
+		}
+
+		protected override string? Disambiguator { get; }
+
+		protected override string GetEditSummary(Page page)
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override bool IsValid(ContextualParser parser, CsvRow item)
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override IDictionary<Title, CsvRow> LoadItems()
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override string NewPageText(Title title, CsvRow item)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
