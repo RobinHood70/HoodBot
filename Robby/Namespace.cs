333ca90f82a4361682295b7a465d2a5031ad8568
Robin Hood <robinhood70@live.ca>
2024-10-09 00:17:45 -0400

    Use custom comparer

--- Robby/Namespace.cs
+++ Robby/Namespace.cs
@@ -2,7 +2,6 @@
 {
 	using System;
 	using System.Collections.Generic;
-	using System.Diagnostics.CodeAnalysis;
 	using RobinHood70.CommonCode;
 	using RobinHood70.Robby.Design;
 	using RobinHood70.Robby.Properties;
@@ -182,13 +181,10 @@
 		/// <param name="x">This first page name to compare.</param>
 		/// <param name="y">The second page name to compare.</param>
 		/// <returns>An integer that indicates whether the first page name is less than the second (-1), equal to the second (0), or greater than the second (1).</returns>
-		[SuppressMessage("Globalization", "CA1309:Use ordinal string comparison", Justification = "False hit, culture is specified.")]
-		public int ComparePageNames(string x, string y)
+		public int ComparePageNames(string? x, string? y)
 		{
-			var siteCulture = this.Site.Culture;
-			return this.CaseSensitive
-				? string.Compare(x, y, false, siteCulture)
-				: string.Compare(x.UpperFirst(siteCulture), y.UpperFirst(siteCulture), false, siteCulture);
+			var comparer = this.Site.GetPageNameComparer(this.CaseSensitive);
+			return comparer.Compare(x, y);
 		}
 
 		/// <summary>Determines whether the name specified is in the list of names for this namespace.</summary>
