4f6b7949feda8eb9d7eda87ee2f1e4da8ba58a6f
Robin Hood <robinhood70@live.ca>
2024-10-29 14:51:19 -0400

    Add ModTemplate

--- HoodBot/Jobs/JobModels/Starfield.cs
+++ HoodBot/Jobs/JobModels/Starfield.cs
@@ -5,5 +5,7 @@
 		public static string BaseFolder { get; } = LocalConfig.BotDataSubPath(@"Starfield\");
 
 		public static string ModFolder { get; } = BaseFolder + @"ShatteredSpaceData2\ShatteredSpace\";
+
+		public static string ModTemplate => "{{SS}}";
 	}
 }
