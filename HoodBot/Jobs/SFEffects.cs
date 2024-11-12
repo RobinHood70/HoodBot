da7537de812774348b515a0f284fb3e20449949c
Robin Hood <robinhood70@live.ca>
2024-11-12 15:57:38 -0500

    Full job

--- HoodBot/Jobs/SFEffects.cs
+++ HoodBot/Jobs/SFEffects.cs
@@ -3,7 +3,6 @@
 	using System;
 	using System.Collections.Generic;
 	using System.Collections.ObjectModel;
-	using System.Diagnostics;
 	using System.Linq;
 	using System.Text;
 	using System.Text.RegularExpressions;
@@ -13,6 +12,7 @@
 	using RobinHood70.Robby.Design;
 	using RobinHood70.Robby.Parser;
 
+	// CONSIDER: Should redirects from EDID pages be to EDID targets with Anchor including both Name and EDID?
 	[method: JobInfo("Effects", "Starfield")]
 	internal sealed partial class SFEffects(JobManager jobManager) : CreateOrUpdateJob<SFEffects.Effect>(jobManager)
 	{
@@ -30,31 +30,39 @@
 
 		protected override IDictionary<Title, Effect> LoadItems()
 		{
-			var effects = LoadEffects();
+			this.NewPageText = this.GetNewPageText;
+			var effects = new Effects();
 			var effectsPage = this.Site.LoadPage("Starfield:Effects") ?? throw new InvalidOperationException();
-			effectsPage.Text = effectsPage.Text.Replace("|-\n|}", "|}", StringComparison.Ordinal);
-			var rows = EffectsRowFinder().Matches(effectsPage.Text);
-			FilterEffects(effects, rows);
-			effects.Sort();
-
-			var pos = 0;
-			if (rows.Count > 0)
+			var rows = (IReadOnlyList<Match>)EffectsRowFinder().Matches(effectsPage.Text);
+			LoadExistingEffects(effects, rows);
+			var newEfects = LoadNewEffects(effects);
+			var retval = new Dictionary<Title, Effect>();
+			foreach (var effect in newEfects)
 			{
-				var lastRow = rows[^1];
-				pos = lastRow.Index + lastRow.Length;
+				if (effect.Link.Length > 0)
+				{
+					var siteLink = SiteLink.FromText(this.Site, effect.Link);
+					retval.TryAdd(siteLink.Title, effect);
+				}
+
+				if (effect.EditorId.Length > 0)
+				{
+					var title = TitleFactory.FromUnvalidated(this.Site, "Starfield:" + effect.EditorId);
+					retval.TryAdd(title, effect);
+				}
 			}
 
-			var insertPos = effectsPage.Text.IndexOf("|}", pos, StringComparison.Ordinal);
+			effects.Sort();
+
+			var startPos = rows[0].Index;
+			var endPos = effectsPage.Text.IndexOf("|}", startPos, StringComparison.Ordinal);
+			effectsPage.Text = effectsPage.Text.Remove(startPos, endPos - startPos);
 
 			var sb = new StringBuilder();
-			var retval = new Dictionary<Title, Effect>();
 			foreach (var effect in effects)
 			{
-				var title = TitleFactory.FromUnvalidated(this.Site, "Starfield:" + effect.Name);
-				retval.TryAdd(title, effect);
-				sb
-					.Append("|-\n");
-				if (effect.Name.Length == 0)
+				sb.Append("|-\n");
+				if (effect.Link.Length == 0)
 				{
 					sb.Append("|NO NAME");
 				}
@@ -62,76 +70,112 @@
 				{
 					sb
 						.Append("|{{Anchor|")
-						.Append(title.ToSiteLink().AsLink(LinkFormat.PipeTrick))
+						.Append(effect.Link)
 						.Append("}}");
 				}
 
 				sb
-					.Append(GameInfo.Starfield.ModTemplate)
-					.Append(" || ")
+					.Append(effect.ModTemplate)
+					.Append(" || {{ID|")
 					.Append(effect.FormId)
-					.Append(" || ")
+					.Append("}} || ")
 					.Append(effect.EditorId)
 					.Append(" || ")
 					.Append(effect.Description)
 					.Append('\n');
 			}
 
-			effectsPage.Text = effectsPage.Text.Insert(insertPos, sb.ToString());
+			effectsPage.Text = effectsPage.Text.Insert(startPos, sb.ToString());
 			this.Pages.Add(effectsPage);
 
 			return retval;
 		}
-
-		private static void FilterEffects(Effects effects, IReadOnlyList<Match> rows)
-		{
-			foreach (var row in rows)
-			{
-				var key = row.Groups["EditorId"].Value;
-				if (effects.Remove(row.Groups["EditorId"].Value))
-				{
-					Debug.WriteLine(key);
-				}
-			}
-		}
 		#endregion
 
 		#region Private Static Methods
-		[GeneratedRegex(@"\|-\s+\|\s*(NO NAME|{{Anchor\|.*?}})\s*\|\|\s*0x[x0-9A-F]{8}.*?\s*\|\|\s*(?<EditorId>.*?)\s*\|\|.*?", RegexOptions.ExplicitCapture, 10000)]
+		[GeneratedRegex(@"\|-\s+\|\s*(NO NAME|{{Anchor\|(?<Link>\[\[.*?\]\])(?<Mod>{{.*?}})?}})\s*\|\|\s*0x(?<FormId>[x0-9A-F]{8}.*?)\s*\|\|\s*(?<EditorId>.*?)\s*\|\|(?<Comment>.*?)$", RegexOptions.ExplicitCapture | RegexOptions.Multiline, 10000)]
 		private static partial Regex EffectsRowFinder();
 
-		private static Effects LoadEffects()
+		private static void LoadExistingEffects(Effects effects, IReadOnlyList<Match> rows)
 		{
-			var retval = new Effects();
+			foreach (var row in rows)
+			{
+				var link = row.Groups["Link"].Value;
+				var mod = row.Groups["Mod"].Value;
+				var formid = row.Groups["FormId"].Value.Trim();
+				var edid = row.Groups["EditorId"].Value.Trim();
+				var desc = row.Groups["Comment"].Value.Trim();
+				effects.Add(new Effect(formid, edid, link, mod, desc));
+			}
+		}
+
+		private static List<Effect> LoadNewEffects(Effects effects)
+		{
+			var retval = new List<Effect>();
 			var effectsFile = new CsvFile(GameInfo.Starfield.ModFolder + "Effects.csv");
 			foreach (var row in effectsFile.ReadRows())
 			{
+				var link = row["Name"];
+				if (link.Length > 0)
+				{
+					link = $"[[SF:{link}|{link}]]";
+				}
+
 				var effect = new Effect(
-				row["FormID"][2..],
-				row["EditorID"],
-				row["Name"],
-				row["Description"]);
-				retval.Add(effect);
+					row["FormID"][2..].TrimEnd(),
+					row["EditorID"].Trim(),
+					link,
+					GameInfo.Starfield.ModTemplate,
+					row["Description"].Trim());
+				if (!effects.Remove(effect))
+				{
+					retval.Add(effect);
+				}
+
+				effects.Add(effect);
 			}
 
 			return retval;
+		}
+		#endregion
+
+		#region Private Methods
+		private string GetNewPageText(Title title, Effect effect)
+		{
+			var siteLink = SiteLink.FromText(this.Site, effect.Link);
+			return "#REDIRECT [[Starfield:Effects#" + siteLink.Text + "]]\n[[Category:Redirects to Broader Subjects]]\n[[Category:Starfield-Effects]]";
 		}
 		#endregion
 
 		#region Internal Records
 
-		internal record Effect(string FormId, string EditorId, string Name, string Description);
+		internal record Effect(string FormId, string EditorId, string Link, string ModTemplate, string Description);
 		#endregion
 
 		#region Private Classes
 		private sealed class Effects() : KeyedCollection<string, Effect>(StringComparer.Ordinal)
 		{
-			public void Sort() => ((List<Effect>)this.Items).Sort((effect1, effect2) => effect1.Name.CompareTo(effect2.Name));
+			public void Sort()
+			{
+				var list = (List<Effect>)this.Items;
+				list.Sort(EffectSorter);
+			}
 
 			protected override string GetKeyForItem(Effect item)
 			{
 				ArgumentNullException.ThrowIfNull(item);
 				return item.EditorId;
+			}
+
+			private static int EffectSorter(Effect effect1, Effect effect2)
+			{
+				var retval = effect1.Link.CompareTo(effect2.Link);
+				if (retval == 0)
+				{
+					retval = effect1.EditorId.CompareTo(effect2.EditorId);
+				}
+
+				return retval;
 			}
 		}
 		#endregion
