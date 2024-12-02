6c7075fa2e36e31001289007d8e694fd8de081bb
Robin Hood <robinhood70@live.ca>
2024-12-02 15:08:47 -0500

    Aid Items job with base description checks

---
+++ HoodBot/Jobs/SFAidItems.cs
@@ -0,0 +1,192 @@
+﻿namespace RobinHood70.HoodBot.Jobs;
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Text;
+using RobinHood70.CommonCode;
+using RobinHood70.HoodBot.Jobs.JobModels;
+using RobinHood70.HoodBot.Uesp;
+using RobinHood70.Robby;
+using RobinHood70.Robby.Design;
+using RobinHood70.Robby.Parser;
+using RobinHood70.WikiCommon.Parser;
+
+internal sealed class SFAidItems : CreateOrUpdateJob<SFItem>
+{
+	#region Static Fields
+	private static readonly Dictionary<string, string> ReplacementNames = new(StringComparer.Ordinal)
+	{
+		["NOCLUTTER_ResearchUI_Aid_Affl_Multi_RepairingImmobilizer"] = "Repairing Immobilizer (no clutter)",
+		["NOCLUTTER_ResearchUI_Chem_Craft_Amp"] = "Amp (no clutter)",
+		["NOCLUTTER_ResearchUI_Aid_Affl_Multi_AntibioticPaste"] = "Antibiotic Paste (no clutter)",
+		["NOCLUTTER_ResearchUI_Aid_Craft_Boudicca"] = "Boudicca (no clutter)",
+		["NOCLUTTER_ResearchUI_Chem_Craft_DwarfHeart"] = "Dwarf Star Heart (no clutter)",
+		["NOCLUTTER_ResearchUI_Chem_Craft_Frostwolf"] = "Frostwolf (no clutter)",
+		["NOCLUTTER_ResearchUI_Chem_Craft_GiantHeart"] = "Giant Heart (no clutter)",
+		["NOCLUTTER_ResearchUI_Chem_Craft_Neurajack"] = "Neurajack (no clutter)",
+		["NOCLUTTER_ResearchUI_Aid_Craft_Panacea"] = "Panacea (no clutter)",
+		["NOCLUTTER_ResearchUI_Chem_Craft_Panopticon"] = "Panopticon (no clutter)",
+		["NOCLUTTER_ResearchUI_Chem_Craft_SuperMassiveHeart"] = "Supermassive Black Heart (no clutter)",
+		["PEO_Sus_Drink_Tier_1_Potion"] = "Hydrated (Tier 1)",
+		["PEO_Sus_Drink_Tier_2_Potion"] = "Hydrated (Tier 2)",
+		["PEO_Sus_Drink_Tier_3_Potion"] = "Hydrated (Tier 3)",
+		["PEO_Sus_Food_Tier_1_Potion"] = "Fed (Tier 1)",
+		["PEO_Sus_Food_Tier_2_Potion"] = "Fed (Tier 2)",
+		["PEO_Sus_Food_Tier_3_Potion"] = "Fed (Tier 3)",
+	};
+	#endregion
+
+	#region Private Fields
+	private readonly Dictionary<string, SFItem> oldItems = new(StringComparer.Ordinal);
+	private readonly Dictionary<string, SFItem> newItems = new(StringComparer.Ordinal);
+	#endregion
+
+	#region Constructors
+	[JobInfo("Aid Items", "Starfield")]
+	public SFAidItems(JobManager jobManager)
+		: base(jobManager)
+	{
+		Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+		this.NewPageText = GetNewPageText;
+		this.OnUpdate = this.UpdateAidItem;
+		this.Shuffle = true;
+	}
+	#endregion
+
+	#region Protected Override Properties
+	protected override string? Disambiguator => "aid item";
+	#endregion
+
+	#region Protected Override Methods
+	protected override string GetEditSummary(Page page) => "Update Aid Items";
+
+	protected override bool IsValid(SiteParser parser, SFItem item) =>
+		parser.FindTemplate("Item Summary") is ITemplateNode template &&
+		string.Equals(template.Find("editorid")?.GetValue() ?? throw new InvalidOperationException(), item.EditorId, StringComparison.Ordinal);
+
+	protected override IDictionary<Title, SFItem> LoadItems()
+	{
+		foreach (var item in ReadItems(GameInfo.Starfield.BaseFolder))
+		{
+			this.oldItems.Add(item.EditorId, item);
+		}
+
+		var items = new Dictionary<Title, SFItem>();
+		foreach (var item in ReadItems(GameInfo.Starfield.ModFolder))
+		{
+			this.newItems.Add(item.EditorId, item);
+			var name = ReplacementNames.TryGetValue(item.EditorId, out var replacement)
+				? replacement
+				: item.Name;
+			if (name.Length != 0)
+			{
+				var title = TitleFactory.FromUnvalidated(this.Site[StarfieldNamespaces.Starfield], name);
+				if (!items.TryAdd(title, item))
+				{
+					var existing = items[title];
+					Debug.WriteLine($"Dupe - {title.FullPageName()}: {existing.EditorId} / {item.EditorId}");
+				}
+			}
+		}
+
+		return items;
+	}
+
+	private static List<SFItem> ReadItems(string folder)
+	{
+		var retval = new List<SFItem>();
+		var csvFile = new CsvFile(folder + "Alchemy.csv")
+		{
+			Encoding = Encoding.GetEncoding(1252)
+		};
+
+		foreach (var row in csvFile.ReadRows())
+		{
+			var item = new SFItem(row);
+			retval.Add(item);
+		}
+
+		return retval;
+	}
+	#endregion
+
+	#region Private Static Methods
+	private static string GetNewPageText(Title title, SFItem item) => $$$"""
+		{{Trail|Items|Aid Items}}{{Item Summary
+		|objectid=
+		|editorid={{{item.EditorId}}}
+		|type=Aid Item
+		|image=
+		|weight=
+		|value=
+		|effect=
+		|description=
+		}}
+		[[{{{title.FullPageName()}}}|{{{item.Name}}}]] is an [[Starfield:Aid Items|aid]] [[Starfield:Items|item]].
+		""";
+
+	private void UpdateAidItem(SiteParser parser, SFItem item)
+	{
+		var template = parser.FindTemplate("Item Summary") ?? throw new InvalidOperationException();
+		var pagenameParam = template.Find("pagename");
+		var pagename = pagenameParam?.GetValue();
+		if (parser.Title.PageNameEquals(pagename))
+		{
+			pagename = null;
+			template.Remove("pagename");
+		}
+
+		var titleParam = template.Find("title");
+		var title = titleParam?.GetValue();
+		if ((pagename is null && parser.Title.PageNameEquals(title)) || string.Equals(pagename, title, StringComparison.Ordinal))
+		{
+			title = null;
+			template.Remove("title");
+		}
+
+		template.Update("objectid", item.FormId, ParameterFormat.OnePerLine, false);
+		template.Update("editorid", item.EditorId, ParameterFormat.OnePerLine, false); // For trimming
+		template.Update("weight", item.Weight.ToStringInvariant(), ParameterFormat.OnePerLine, false);
+		template.Update("value", item.Value.ToStringInvariant(), ParameterFormat.OnePerLine, false);
+		template.UpdateIfEmpty("image", $"<!--SF-item-{item.Name}.png-->", ParameterFormat.OnePerLine);
+		var defaultImgdesc = title ?? pagename ?? parser.Title.PageName;
+		if (template.Find("imgdesc") is IParameterNode imgdesc &&
+			string.Equals(imgdesc.GetValue(), defaultImgdesc, StringComparison.OrdinalIgnoreCase))
+		{
+			template.Remove("imgdesc");
+		}
+
+		if (template.Find("effect") is IParameterNode effectParam)
+		{
+			var effectText = effectParam.GetValue();
+			this.oldItems.TryGetValue(item.EditorId, out var oldItem);
+			var oldDesc = oldItem?.Description ?? string.Empty;
+			if (oldDesc.Length > 0)
+			{
+				effectText = effectText.Replace(oldDesc, string.Empty, StringComparison.OrdinalIgnoreCase);
+			}
+
+			if (item.Description.Length > 0)
+			{
+				effectText = effectText.Replace(item.Description, string.Empty, StringComparison.OrdinalIgnoreCase);
+			}
+
+			effectText = effectText.Replace("''''", string.Empty, StringComparison.Ordinal).Trim();
+			effectParam.SetValue(effectText, ParameterFormat.OnePerLine);
+
+			if (!string.IsNullOrEmpty(oldDesc) && !string.Equals(oldDesc, item.Description, StringComparison.Ordinal))
+			{
+				Debug.WriteLine(string.Empty);
+				Debug.WriteLine("Differing descs for " + parser.Title.FullPageName());
+				Debug.WriteLine(oldDesc);
+				Debug.WriteLine(item.Description);
+			}
+		}
+
+		template.Update("description", item.Description, ParameterFormat.OnePerLine, false);
+		template.Sort("objectid", "editorid", "type", "weight", "value", "image", "imgdesc", "effect", "description");
+	}
+	#endregion
+}
---
+++ HoodBot/Jobs/SFItem.cs
@@ -0,0 +1,60 @@
+﻿namespace RobinHood70.HoodBot.Jobs;
+
+using System;
+using System.Globalization;
+using RobinHood70.CommonCode;
+
+internal sealed class SFItem
+{
+	#region Constructors
+	public SFItem(CsvRow row)
+	{
+		var formId = row["FormID"];
+		if (formId.StartsWith("0x", StringComparison.Ordinal))
+		{
+			formId = formId[2..];
+		}
+
+		if (formId.Length != 8)
+		{
+			throw new InvalidOperationException();
+		}
+
+		if (!formId.StartsWith("00", StringComparison.Ordinal))
+		{
+			formId = "xx" + formId[2..];
+		}
+
+		this.FormId = formId;
+
+		this.EditorId = row["EditorID"];
+		this.Name = row["Name"];
+		this.Description = row["Description"];
+		if (double.TryParse(row["Weight"], CultureInfo.CurrentCulture, out var weight))
+		{
+			this.Weight = weight;
+		}
+
+		_ = row.TryGetValue("Value", out var value) || row.TryGetValue("Unknown1", out value);
+		this.Value = int.Parse(value ?? "0", CultureInfo.CurrentCulture);
+	}
+	#endregion
+
+	#region Public Properties
+	public string Description { get; }
+
+	public string EditorId { get; }
+
+	public string FormId { get; }
+
+	public string Name { get; }
+
+	public int Value { get; }
+
+	public double Weight { get; }
+	#endregion
+
+	#region Public Override Properties
+	public override string ToString() => this.Name;
+	#endregion
+}
