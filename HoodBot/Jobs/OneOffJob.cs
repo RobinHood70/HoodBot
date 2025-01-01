624de7198b1e1120e92c6d88f4a9357b7491ef00
Robin Hood <robinhood70@live.ca>
2024-12-31 22:35:56 -0500

    Project Cyrodiil jobs

--- HoodBot/Jobs/OneOffJob.cs
+++ HoodBot/Jobs/OneOffJob.cs
@@ -1,24 +1,35 @@
 ﻿namespace RobinHood70.HoodBot.Jobs;
 
-using System.IO;
+using System.Collections.Generic;
 using RobinHood70.Robby;
+using RobinHood70.WikiCommon;
 
 [method: JobInfo("One-Off Job")]
-internal sealed class OneOffJob(JobManager jobManager) : WikiJob(jobManager, JobType.ReadOnly)
+internal sealed class OneOffJob(JobManager jobManager) : EditJob(jobManager)
 {
 	#region Public Override Properties
-	//// public override string LogDetails => "Get category";
-
 	public override string LogName => "One-Off Job";
 	#endregion
 
 	#region Protected Override Methods
-	protected override void Main()
+	protected override string GetEditSummary(Page page) => "Update project name";
+
+	protected override void LoadPages()
 	{
-		var titles = new TitleCollection(this.Site);
-		titles.GetCategoryMembers("Online-Furnishings", true);
-		titles.Sort();
-		File.WriteAllText("D:\\Online Furnishings.txt", string.Join("\r\n", titles));
+		var namespaces = new Uesp.UespNamespaceList(this.Site);
+		var gamespaces = new List<int>(namespaces.Count);
+		foreach (var ns in namespaces)
+		{
+			if (ns.Value.IsGamespace)
+			{
+				gamespaces.Add(ns.Value.BaseNamespace.Id);
+			}
+		}
+
+		this.Pages.GetSearchResults("\"Province: Cyrodiil\"", WhatToSearch.Text, gamespaces);
+		this.Pages.GetSearchResults("insource:\"Province: Cyrodiil\"", WhatToSearch.Text, gamespaces);
 	}
+
+	protected override void PageLoaded(Page page) => page.Text = page.Text.Replace("Province: Cyrodiil", "Project Cyrodiil", System.StringComparison.OrdinalIgnoreCase);
 	#endregion
 }
--- HoodBot/Jobs/OneOffMoveJob.cs
+++ HoodBot/Jobs/OneOffMoveJob.cs
@@ -1,9 +1,9 @@
 ﻿namespace RobinHood70.HoodBot.Jobs;
 
+using System;
 using RobinHood70.CommonCode;
 using RobinHood70.Robby;
-using RobinHood70.Robby.Design;
-using RobinHood70.WikiCommon;
+using RobinHood70.WikiCommon.Parser;
 
 [method: JobInfo("One-Off Move Job")]
 public class OneOffMoveJob(JobManager jobManager, bool updateUserSpace) : MovePagesJob(jobManager, updateUserSpace)
@@ -12,32 +12,65 @@ public class OneOffMoveJob(JobManager jobManager, bool updateUserSpace) : MovePa
 	protected override bool BeforeMain()
 	{
 		this.MoveAction = MoveAction.None;
+		this.ParameterReplacers.AddTemplateReplacers("PC3", this.StirkReplace);
 		return base.BeforeMain();
 	}
 
-	protected override string GetEditSummary(Page page) => "Link past redirect";
 
-	protected override void OnFromPageOrphaned(Page page)
+	protected void StirkReplace(Page page, ITemplateNode template)
 	{
-		if (page.Exists)
+		if (template.Find(1) is not IParameterNode param1)
 		{
-			page.Title.Delete("Unnecessary redirect");
+			return;
+		}
+
+		if (!param1.GetValue().Replace('_', ' ').OrdinalEquals("Stirk (city)"))
+		{
+			return;
+		}
+
+		param1.SetValue("Charach", ParameterFormat.Copy);
+		this.ParameterReplacers.PageNameReplace(page.Title.Namespace, param1);
+		if (template.Find(2) is not IParameterNode param2)
+		{
+			return;
+		}
+
+		var param2Value = param2
+			.GetValue()
+			.Replace("Sitrk", "Charach", StringComparison.OrdinalIgnoreCase)
+			.Replace("Stirk", "Charach", StringComparison.OrdinalIgnoreCase);
+
+		if (string.Compare(param1.GetValue(), param2Value, false, this.Site.Culture) == 0)
+		{
+			template.Remove("2");
+		}
+		else
+		{
+			param2.SetValue(param2Value, ParameterFormat.Copy);
 		}
 	}
 
+
+	protected override string GetEditSummary(Page page) => "Link past redirect";
+
 	protected override void PopulateMoves()
 	{
-		var redirects = new PageCollection(this.Site, PageModules.Links);
-		redirects.GetNamespace(MediaWikiNamespaces.File, Filter.Only, "ON-icon-lead-");
-		foreach (var page in redirects)
-		{
-			this.AddLinkUpdate(page.Title, page.Links[0]);
-		}
+		this.AddLinkUpdate("Project Tamriel:Cyrodiil/Stirk (city)", "Project Tamriel:Cyrodiil/Charach");
 	}
 
 	//// this.AddLinkUpdate("Category:Online-Furnishings", "Category:Online-Furnishing Images");
 	//// this.AddMove(title, newName);
 	//// this.AddReplacement("Skyrim:Map Notes", "Skyrim:Treasure Maps");
 	//// this.LoadReplacementsFromFile(LocalConfig.BotDataSubPath("Replacements5.txt"));
+	protected override void UpdateLinkText(ITitle page, SiteLink from, SiteLink toLink, bool addCaption)
+	{
+		base.UpdateLinkText(page, from, toLink, addCaption);
+		if (from.Text.OrdinalICEquals("Stirk") ||
+			from.Text.OrdinalICEquals("Stirk (city)"))
+		{
+			toLink.Text = "Charach";
+		}
+	}
 	#endregion
 }
