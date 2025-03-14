41f7db853dfc5a3f46a6ce12ea83fad535770b7e
Robin Hood <robinhood70@live.ca>
2025-03-14 17:20:11 -0400

    Temporarily ignore some invalid ids

--- HoodBot/Jobs/JobModels/EsoLog.cs
+++ HoodBot/Jobs/JobModels/EsoLog.cs
@@ -165,7 +165,8 @@ internal static class EsoLog
 		var query = "SELECT id, name, gender, difficulty, ppDifficulty, ppClass, reaction FROM uesp_esolog.npc WHERE level != -1 AND reaction != 6";
 		foreach (var npcData in Database.RunQuery(Connection, query, row => new NpcData(row)))
 		{
-			if (!ColourCode.IsMatch(npcData.DataName) &&
+			if ((npcData.Id < 322827 || npcData.Id > 322924) && // Remove temporary(?) Polish names
+				!ColourCode.IsMatch(npcData.DataName) &&
 				!TrailingDigits.IsMatch(npcData.DataName) &&
 				!ReplacementData.NpcNameSkips.Contains(npcData.DataName))
 			{
