60ae401a45e2206f53a2cf895c5e7a4d3b774e37
Robin Hood <robinhood70@live.ca>
2025-04-23 16:11:04 -0400

    Add remastered NPC templates

--- HoodBot/Jobs/OneOffParseJob.cs
+++ HoodBot/Jobs/OneOffParseJob.cs
@@ -1,23 +1,61 @@
 ﻿namespace RobinHood70.HoodBot.Jobs;
 
 using RobinHood70.Robby;
+using RobinHood70.Robby.Design;
 using RobinHood70.Robby.Parser;
 using RobinHood70.WikiCommon;
+using RobinHood70.WikiCommon.Parser;
 
 [method: JobInfo("One-Off Parse Job")]
 public class OneOffParseJob(JobManager jobManager) : ParsedPageJob(jobManager)
 {
+	#region Constants
+	private const string TemplateText = """
+		{{Cleanup-obrp-npc
+		|image=
+		|imageChecked=
+		|dialogue=
+		|dialogueChecked=
+		|quests=
+		|questsChecked=
+		|services=
+		|servicesChecked=
+		|inventory=
+		|inventoryChecked=
+		|house=
+		|houseChecked=
+		|schedule=
+		|scheduleChecked=
+		}}
+		""";
+	#endregion
+
 	#region Public Override Properties
-	public override string LogDetails => "Remove Trails";
+	public override string LogDetails => "Add remaster NPC template";
 
 	public override string LogName => "One-Off Parse Job";
 	#endregion
 
 	#region Protected Override Methods
-	protected override string GetEditSummary(Page page) => "Remove trail already handled by template";
+	protected override string GetEditSummary(Page page) => "Add remaster template";
 
-	protected override void LoadPages() => this.Pages.GetBacklinks("Template:Game Book", BacklinksTypes.EmbeddedIn);
+	protected override void LoadPages()
+	{
+		this.Shuffle = true;
+		this.Pages.AllowRedirects = CommonCode.Filter.Exclude;
+		this.Pages.GetCategoryMembers("Oblivion-NPCs", CategoryMemberTypes.Page, false);
+		this.Pages.GetCategoryMembers("Shivering-NPCs", CategoryMemberTypes.Page, false);
+	}
 
-	protected override void ParseText(SiteParser parser) => parser.RemoveTemplates("Trail");
+	protected override void ParseText(SiteParser parser)
+	{
+		var newTemplate = TitleFactory.FromUnvalidated(this.Site[MediaWikiNamespaces.Template], "Cleanup-obrp-npc");
+		if (parser.FindTemplate(newTemplate) is null)
+		{
+			var oldTemplate = TitleFactory.FromValidated(this.Site[MediaWikiNamespaces.Template], "Cleanup-obnpcrp");
+			var location = parser.IndexOf<ITemplateNode>(t => t.GetTitle(this.Site) == oldTemplate);
+			parser.InsertText(location + 1, TemplateText);
+		}
+	}
 	#endregion
 }
