95682ecc0326af0fa7b353c097c3748be2fc6e25
Robin Hood <robinhood70@live.ca>
2025-05-24 13:36:59 -0400

    Blades ID update

--- HoodBot/Jobs/OneOffTemplateJob.cs
+++ HoodBot/Jobs/OneOffTemplateJob.cs
@@ -1,12 +1,20 @@
 ﻿namespace RobinHood70.HoodBot.Jobs;
 
+using System.Collections.Generic;
+using System.IO;
+using RobinHood70.HoodBot.Jobs.JobModels;
 using RobinHood70.Robby;
+using RobinHood70.Robby.Design;
 using RobinHood70.Robby.Parser;
 using RobinHood70.WikiCommon.Parser;
 
 [method: JobInfo("One-Off Template Job")]
 public class OneOffTemplateJob(JobManager jobManager) : TemplateJob(jobManager)
 {
+	#region Fields
+	private readonly Dictionary<Title, string> ids = [];
+	#endregion
+
 	#region Public Override Properties
 	public override string LogDetails => "Update " + this.TemplateName;
 
@@ -14,26 +22,28 @@ public class OneOffTemplateJob(JobManager jobManager) : TemplateJob(jobManager)
 	#endregion
 
 	#region Protected Override Properties
-	protected override string TemplateName => "Online NPC Summary";
+	protected override string TemplateName => "Blades Item Summary";
 	#endregion
 
 	#region Protected Override Methods
-	protected override string GetEditSummary(Page page) => "Revert to health=1";
+	protected override string GetEditSummary(Page page) => "Add/update ID";
+
+	protected override void BeforeLoadPages()
+	{
+		foreach (var line in File.ReadAllLines(LocalConfig.BotDataSubPath("BladesIds.txt")))
+		{
+			var split = line.Split('\t');
+			this.ids.Add(TitleFactory.FromUnvalidated(this.Site, split[0]), split[1]);
+		}
+
+		base.BeforeLoadPages();
+	}
+
+	protected override void LoadPages() => this.Pages.GetTitles(this.ids.Keys);
 
 	protected override void ParseTemplate(ITemplateNode template, SiteParser parser)
 	{
-		if (template.Find("health") is IParameterNode health &&
-			string.Equals(health.GetValue(), "{{ESO Health|w}}", System.StringComparison.OrdinalIgnoreCase))
-		{
-			parser.Page.LoadPreviousRevision();
-			var prevParser = new SiteParser(parser.Page, parser.Page.Revisions[1].Text);
-			if (prevParser.FindTemplate("Online NPC Summary") is ITemplateNode prevTemplate &&
-				prevTemplate.Find("health") is IParameterNode prevHealth &&
-				string.Equals(prevHealth.GetValue(), "1", System.StringComparison.Ordinal))
-			{
-				health.SetValue("1", ParameterFormat.Copy);
-			}
-		}
+		template.Update("id", this.ids[parser.Title]);
 	}
 	#endregion
 }
