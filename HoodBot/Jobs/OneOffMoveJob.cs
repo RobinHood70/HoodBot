ed3d1d749b14fc15b6ae7691364d160a9148ae08
Robin Hood <robinhood70@live.ca>
2025-05-26 13:54:47 -0400

    Recat LotD images

--- HoodBot/Jobs/OneOffMoveJob.cs
+++ HoodBot/Jobs/OneOffMoveJob.cs
@@ -7,7 +7,7 @@ public class OneOffMoveJob(JobManager jobManager, bool updateUserSpace) : MovePa
 	#region Protected Override Methods
 	protected override bool BeforeMain()
 	{
-		this.FollowUpActions |= FollowUpActions.NeedsCategoryMembers;
+		this.FollowUpActions = FollowUpActions.CheckLinksRemaining | FollowUpActions.EmitReport | FollowUpActions.FixLinks | FollowUpActions.UpdateSameNamedText | FollowUpActions.NeedsCategoryMembers;
 		this.MoveAction = MoveAction.None;
 		return base.BeforeMain();
 	}
@@ -16,28 +16,7 @@ public class OneOffMoveJob(JobManager jobManager, bool updateUserSpace) : MovePa
 
 	protected override void PopulateMoves()
 	{
-		this.AddLinkUpdate("Category:Online-Sets with Weapon Damage", "Category:Online-Sets with Weapon/Spell Damage");
-		this.AddLinkUpdate("Category:Online-Sets with Spell Damage", "Category:Online-Sets with Weapon/Spell Damage");
-		this.AddLinkUpdate("Category:Online-Sets with Double Weapon Damage", "Category:Online-Sets with Double Weapon/Spell Damage");
-		this.AddLinkUpdate("Category:Online-Sets with Double Spell Damage", "Category:Online-Sets with Double Weapon/Spell Damage");
-		this.AddLinkUpdate("Category:Online-Sets with Triple Weapon Damage", "Category:Online-Sets with Triple Weapon/Spell Damage");
-		this.AddLinkUpdate("Category:Online-Sets with Triple Spell Damage", "Category:Online-Sets with Triple Weapon/Spell Damage");
-		this.AddLinkUpdate("Category:Online-Sets with Weapon Critical", "Category:Online-Sets with Weapon/Spell Critical");
-		this.AddLinkUpdate("Category:Online-Sets with Spell Critical", "Category:Online-Sets with Weapon/Spell Critical");
-		this.AddLinkUpdate("Category:Online-Sets with Double Weapon Critical", "Category:Online-Sets with Double Weapon/Spell Critical");
-		this.AddLinkUpdate("Category:Online-Sets with Double Spell Critical", "Category:Online-Sets with Double Weapon/Spell Critical");
-		this.AddLinkUpdate("Category:Online-Sets with Triple Weapon Critical", "Category:Online-Sets with Triple Weapon/Spell Critical");
-		this.AddLinkUpdate("Category:Online-Sets with Triple Spell Critical", "Category:Online-Sets with Triple Weapon/Spell Critical");
-		this.AddLinkUpdate("Category:Online-Sets with Physical Penetration", "Category:Online-Sets with Physical/Spell Penetration");
-		this.AddLinkUpdate("Category:Online-Sets with Spell Penetration", "Category:Online-Sets with Physical/Spell Penetration");
-		this.AddLinkUpdate("Category:Online-Sets with Double Physical Penetration", "Category:Online-Sets with Double Physical/Spell Penetration");
-		this.AddLinkUpdate("Category:Online-Sets with Double Spell Penetration", "Category:Online-Sets with Double Physical/Spell Penetration");
-		this.AddLinkUpdate("Category:Online-Sets with Triple Physical Penetration", "Category:Online-Sets with Triple Physical/Spell Penetration");
-		this.AddLinkUpdate("Category:Online-Sets with Triple Spell Penetration", "Category:Online-Sets with Triple Physical/Spell Penetration");
-		this.AddLinkUpdate("Category:Online-Sets with Physical Resistance", "Category:Online-Sets with Physical/Spell Resistance");
-		this.AddLinkUpdate("Category:Online-Sets with Spell Resistance", "Category:Online-Sets with Physical/Spell Resistance");
-		this.AddLinkUpdate("Category:Online-Sets with Double Physical Resistance", "Category:Online-Sets with Double Physical/Spell Resistance");
-		this.AddLinkUpdate("Category:Online-Sets with Double Spell Resistance", "Category:Online-Sets with Double Physical/Spell Resistance");
+		this.AddLinkUpdate("Category:Legacy of the Dragonborn", "Category:Skyrim Mod-Legacy of the Dragonborn-Images");
 	}
 	#endregion
 }
