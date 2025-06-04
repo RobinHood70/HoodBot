55953fd3b1c7977b750801d16fa2b89596127eae
Robin Hood <robinhood70@live.ca>
2025-06-03 21:17:57 -0400

    Mod Header - Season of the Worm Cult

--- HoodBot/Jobs/OneOffTemplateJob.cs
+++ HoodBot/Jobs/OneOffTemplateJob.cs
@@ -1,49 +1,53 @@
 ﻿namespace RobinHood70.HoodBot.Jobs;
 
+using System;
 using System.Collections.Generic;
-using System.IO;
-using RobinHood70.HoodBot.Jobs.JobModels;
+using RobinHood70.CommonCode;
+using RobinHood70.HoodBot.Uesp;
 using RobinHood70.Robby;
 using RobinHood70.Robby.Design;
 using RobinHood70.Robby.Parser;
+using RobinHood70.WikiCommon;
 using RobinHood70.WikiCommon.Parser;
 
 [method: JobInfo("One-Off Template Job")]
 public class OneOffTemplateJob(JobManager jobManager) : TemplateJob(jobManager)
 {
-	#region Fields
-	private readonly Dictionary<Title, string> ids = [];
+	#region Static Fields
+	private static readonly HashSet<string> SeasonVariants = new(StringComparer.OrdinalIgnoreCase)
+	{
+		"2025 Content Pass", "Seasons of the Worm Cult", "Solstice"
+	};
 	#endregion
 
 	#region Public Override Properties
-	public override string LogDetails => "Update " + this.TemplateName;
+	public override string LogDetails => "Update " + this.TemplateName + " to use Season of the Worm Cult";
 
 	public override string LogName => "One-Off Template Job";
 	#endregion
 
 	#region Protected Override Properties
-	protected override string TemplateName => "Blades Item Summary";
+	protected override string TemplateName => "Mod Header";
 	#endregion
 
 	#region Protected Override Methods
-	protected override string GetEditSummary(Page page) => "Add/update ID";
+	protected override string GetEditSummary(Page page) => "Season of the Worm Cult";
 
-	protected override void BeforeLoadPages()
+	protected override void LoadPages()
 	{
-		foreach (var line in File.ReadAllLines(LocalConfig.BotDataSubPath("BladesIds.txt")))
-		{
-			var split = line.Split('\t');
-			this.ids.Add(TitleFactory.FromUnvalidated(this.Site, split[0]), split[1]);
-		}
-
-		base.BeforeLoadPages();
+		var title = TitleFactory.FromUnvalidated(this.Site[MediaWikiNamespaces.Template], this.TemplateName);
+		this.Pages.GetBacklinks(title.FullPageName(), BacklinksTypes.EmbeddedIn, true, Filter.Exclude, UespNamespaces.Online);
 	}
-
-	protected override void LoadPages() => this.Pages.GetTitles(this.ids.Keys);
 
 	protected override void ParseTemplate(ITemplateNode template, SiteParser parser)
 	{
-		template.Update("id", this.ids[parser.Title]);
+		foreach (var param in template.Parameters)
+		{
+			if (param.Anonymous && SeasonVariants.Contains(param.GetValue()))
+			{
+				param.SetValue("Season of the Worm Cult", ParameterFormat.Copy);
+			}
+		}
 	}
 	#endregion
 }
