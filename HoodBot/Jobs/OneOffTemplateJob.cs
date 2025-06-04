85f97cda625733ccc2aecef898817327d8bd592b
Robin Hood <robinhood70@live.ca>
2025-06-03 21:59:31 -0400

    Rename ESO DLC for Season of the Worm Cult

--- HoodBot/Jobs/OneOffTemplateJob.cs
+++ HoodBot/Jobs/OneOffTemplateJob.cs
@@ -2,12 +2,8 @@
 
 using System;
 using System.Collections.Generic;
-using RobinHood70.CommonCode;
-using RobinHood70.HoodBot.Uesp;
 using RobinHood70.Robby;
-using RobinHood70.Robby.Design;
 using RobinHood70.Robby.Parser;
-using RobinHood70.WikiCommon;
 using RobinHood70.WikiCommon.Parser;
 
 [method: JobInfo("One-Off Template Job")]
@@ -21,32 +17,24 @@ public class OneOffTemplateJob(JobManager jobManager) : TemplateJob(jobManager)
 	#endregion
 
 	#region Public Override Properties
-	public override string LogDetails => "Update " + this.TemplateName + " to use Season of the Worm Cult";
+	public override string LogDetails => "Update " + this.TemplateName;
 
 	public override string LogName => "One-Off Template Job";
 	#endregion
 
 	#region Protected Override Properties
-	protected override string TemplateName => "Mod Header";
+	protected override string TemplateName => "ESO DLC";
 	#endregion
 
 	#region Protected Override Methods
-	protected override string GetEditSummary(Page page) => "Season of the Worm Cult";
-
-	protected override void LoadPages()
-	{
-		var title = TitleFactory.FromUnvalidated(this.Site[MediaWikiNamespaces.Template], this.TemplateName);
-		this.Pages.GetBacklinks(title.FullPageName(), BacklinksTypes.EmbeddedIn, true, Filter.Exclude, UespNamespaces.Online);
-	}
+	protected override string GetEditSummary(Page page) => "Update template name";
 
 	protected override void ParseTemplate(ITemplateNode template, SiteParser parser)
 	{
-		foreach (var param in template.Parameters)
+		if (string.Equals(template.Find(1)?.GetValue(), "Seasons of the Worm Cult", StringComparison.OrdinalIgnoreCase))
 		{
-			if (param.Anonymous && SeasonVariants.Contains(param.GetValue()))
-			{
-				param.SetValue("Season of the Worm Cult", ParameterFormat.Copy);
-			}
+			template.SetTitle("ESO S-WC");
+			template.Parameters.Clear();
 		}
 	}
 	#endregion
