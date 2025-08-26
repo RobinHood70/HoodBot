332a84665a807020b983b72e1a775c4e941bd046
Robin Hood <robinhood70@live.ca>
2025-08-26 13:32:35 -0400

    New job

---
+++ HoodBot/Jobs/CaseSensitiveSearch.cs
@@ -0,0 +1,35 @@
+﻿namespace RobinHood70.HoodBot.Jobs;
+
+using System;
+using System.Collections.Generic;
+using RobinHood70.Robby;
+using RobinHood70.WikiCommon;
+
+[method: JobInfo("Case-Sensitive Search")]
+internal class CaseSensitiveSearch(JobManager jobManager, string search) : WikiJob(jobManager, JobType.ReadOnly)
+{
+	protected override void Main()
+	{
+		var subjectSpaces = new List<int>();
+		foreach (var ns in this.Site.Namespaces)
+		{
+			if (ns.IsSubjectSpace && ns.CanTalk)
+			{
+				subjectSpaces.Add(ns.Id);
+			}
+		}
+
+		this.WriteLine("== Case-Sensitive Search Term: " + search + " ==");
+		var pages = new PageCollection(this.Site);
+		pages.GetSearchResults(search, WhatToSearch.Title | WhatToSearch.Text, subjectSpaces);
+		pages.Sort();
+		foreach (var page in pages)
+		{
+			if (page.Text.Contains(search, StringComparison.Ordinal))
+			{
+				var siteLink = new SiteLink(page.Title);
+				this.WriteLine("* " + siteLink.AsLink(LinkFormat.ForcedLink));
+			}
+		}
+	}
+}
