8d038c1cf10a0242ae6f286bdeb2a68f1a34de22
Robin Hood <robinhood70@live.ca>
2025-09-18 23:09:24 -0400

    Primitive removal of bulleted location links

--- HoodBot/Jobs/OneOffJob.cs
+++ HoodBot/Jobs/OneOffJob.cs
@@ -1,13 +1,59 @@
 ﻿namespace RobinHood70.HoodBot.Jobs;
+
+using System.Collections.Generic;
+using RobinHood70.CommonCode;
 using RobinHood70.Robby;
+using RobinHood70.Robby.Parser;
 using RobinHood70.WikiCommon;
 
-[method: JobInfo("One-Off Job")]
-internal sealed class OneOffJob(JobManager jobManager) : EditJob(jobManager)
+internal sealed class OneOffJob : EditJob
 {
-	protected override string GetEditSummary(Page page) => "Replace incorrect Paradise link (bot assisted)";
+	#region Fields
+	private readonly TitleCollection searchTitles;
+	#endregion
 
-	protected override void LoadPages() => this.Pages.GetBacklinks("Oblivion:Paradise (quest)", BacklinksTypes.All);
+	#region Constructors
+	[JobInfo("One-Off Job")]
+	public OneOffJob(JobManager jobManager)
+		: base(jobManager)
+	{
+		this.searchTitles = new TitleCollection(this.Site, "Tamriel Rebuilt:Firewatch Library", "Tamriel Rebuilt:Helnim Hall", "Tamriel Rebuilt:Telvanni Library");
+	}
+	#endregion
 
-	protected override void PageLoaded(Page page) => page.Text = page.Text.Replace("Paradise (quest)", "Paradise (place)", System.StringComparison.OrdinalIgnoreCase);
+	#region Protected Override Methods
+	protected override string GetEditSummary(Page page) => "Remove references to deprecated locations";
+
+	protected override void LoadPages()
+	{
+		this.Pages.GetBacklinks("Tamriel Rebuilt:Firewatch Library", BacklinksTypes.Backlinks);
+		this.Pages.GetBacklinks("Tamriel Rebuilt:Helnim Hall", BacklinksTypes.Backlinks);
+		this.Pages.GetBacklinks("Tamriel Rebuilt:Telvanni Library", BacklinksTypes.Backlinks);
+	}
+
+	protected override void PageLoaded(Page page)
+	{
+		var lines = page.Text.Split(TextArrays.LineFeed);
+		var newLines = new List<string>(lines.Length);
+		foreach (var line in lines)
+		{
+			var parser = new SiteParser(page, line);
+			var found = false;
+			foreach (var title in this.searchTitles)
+			{
+				if (line.Length > 0 && line[0] == '*')
+				{
+					found |= parser.FindLink(title) is not null;
+				}
+			}
+
+			if (!found)
+			{
+				newLines.Add(line);
+			}
+		}
+
+		page.Text = string.Join('\n', newLines);
+	}
+	#endregion
 }
