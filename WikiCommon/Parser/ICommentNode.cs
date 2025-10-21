a4a33f998d043b5aaede7520cc3ae334def2c59f
Robin Hood <robinhood70@live.ca>
2025-10-21 17:07:36 -0400

    Explanatory comments

--- WikiCommon/Parser/ICommentNode.cs
+++ WikiCommon/Parser/ICommentNode.cs
@@ -4,6 +4,6 @@
 public interface ICommentNode : IWikiNode
 {
 	/// <summary>Gets or sets the comment text.</summary>
-	/// <value>The comment text.</value>
+	/// <value>The comment text, including leading and trailing markers as well as trailing spaces and tabs.</value>
 	string Comment { get; set; }
 }
