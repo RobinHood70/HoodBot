abd6293344848c04762dad273dc24bf5603f1095
Robin Hood <robinhood70@live.ca>
2025-10-23 22:13:28 -0400

    Generated regexs for common wiki searches

---
+++ HoodBot/Jobs/JobModels/WikiRegexes.cs
@@ -0,0 +1,9 @@
+﻿namespace RobinHood70.HoodBot.Jobs.JobModels;
+using System.Text.RegularExpressions;
+using RobinHood70.CommonCode;
+
+public static partial class WikiRegexes
+{
+	[GeneratedRegex(@"^==\s*(?<title>.*?)\s*==", RegexOptions.Multiline, Globals.DefaultGeneratedRegexTimeout)]
+	public static partial Regex HeaderFinder();
+}
