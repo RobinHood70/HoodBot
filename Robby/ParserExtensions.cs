42dd18bd738c8598874a7031576efca6df099dfb
Robin Hood <robinhood70@live.ca>
2025-10-29 12:00:26 -0400

    Remove unused one-liner extension

--- Robby/ParserExtensions.cs
+++ Robby/ParserExtensions.cs
@@ -8,17 +8,6 @@ public static class ParserExtensions
 {
 	#region ILinkNode Methods
 
-	/// <summary>Parses the title and returns the trimmed value.</summary>
-	/// <param name="link">The backlink to get the title for.</param>
-	/// <param name="site">The site the title is from.</param>
-	/// <returns>The title.</returns>
-	public static SiteLink GetLink(this ILinkNode link, Site site)
-	{
-		ArgumentNullException.ThrowIfNull(link);
-		ArgumentNullException.ThrowIfNull(site);
-		return SiteLink.FromLinkNode(site, link);
-	}
-
 	/// <summary>Parses the link and returns the title portion as a <see cref="Title"/>.</summary>
 	/// <param name="link">The link to get the title for.</param>
 	/// <param name="site">The site the link is from.</param>
@@ -27,8 +16,7 @@ public static class ParserExtensions
 	{
 		ArgumentNullException.ThrowIfNull(link);
 		ArgumentNullException.ThrowIfNull(site);
-		var titleText = link.TitleNodes.ToValue();
-		return TitleFactory.FromUnvalidated(site, titleText).Title;
+		return TitleFactory.FromUnvalidated(site, link.TitleNodes.ToValue()).Title;
 	}
 	#endregion
 
