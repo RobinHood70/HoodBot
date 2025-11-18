c31ef0637f26ee01a299f7c781c3125e0296d5d5
Robin Hood <robinhood70@live.ca>
2025-11-18 18:12:35 -0500

    Em dash job (not yet run)

--- HoodBot/Jobs/OneOffParseJob.cs
+++ HoodBot/Jobs/OneOffParseJob.cs
@@ -2,6 +2,8 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
 using RobinHood70.CommonCode;
 using RobinHood70.Robby;
 using RobinHood70.Robby.Parser;
@@ -10,52 +12,33 @@ using RobinHood70.WikiCommon.Parser;
 
 internal sealed class OneOffParseJob : ParsedPageJob
 {
-	#region Static Fields
-	private static readonly HashSet<string> Replacements = new(StringComparer.OrdinalIgnoreCase)
-	{
-		"Burning",
-		"Chilled",
-		"Concussion",
-		"Diseased",
-		"Hemorrhaging",
-		"Overcharged",
-		"Poisoned",
-		"Sundered",
-	};
-	#endregion
+	private static readonly Regex EmDashes = new(@"\s*—\s*", RegexOptions.None, Globals.DefaultRegexTimeout);
 
-	#region Constructors
 	[JobInfo("One-Off Parse Job")]
 	public OneOffParseJob(JobManager jobManager)
 		: base(jobManager)
 	{
 	}
-	#endregion
 
-	#region Public Override Properties
-	public override string LogDetails => "Replace links";
-	#endregion
+	protected override string GetEditSummary(Page page) => "Replace em dashes";
 
-	#region Protected Override Methods
-	protected override string GetEditSummary(Page page) => this.LogDetails;
-
-	protected override void LoadPages() => this.Pages.GetBacklinks("ON:Combat", BacklinksTypes.Backlinks);
+	protected override void LoadPages() => this.Pages.GetBacklinks("Template:Online NPC Summary", BacklinksTypes.EmbeddedIn, true, Filter.Exclude);
 
 	protected override void ParseText(SiteParser parser)
 	{
-		for (var i = parser.Count - 1; i >= 0; i--)
+		foreach (var template in parser.FindTemplates("Online NPC Summary"))
 		{
-			var node = parser[i];
-			if (node is ILinkNode linkNode && linkNode.GetTitle(this.Site) == "ON:Combat")
+			foreach (var parameter in template.Parameters)
 			{
-				var siteLink = SiteLink.FromLinkNode(this.Site, linkNode);
-				if (siteLink.Fragment is not null && Replacements.Contains(siteLink.Fragment))
+				foreach (var node in parameter.Value)
 				{
-					var ns = siteLink.OriginalTitle!.Split(TextArrays.Colon, 2)[0];
-					parser[i] = parser.Factory.LinkNodeFromParts($"{ns}:{siteLink.Fragment}", siteLink.Text ?? string.Empty);
+					if (node is ITextNode textNode && textNode.Text.Contains('—', StringComparison.Ordinal))
+					{
+						textNode.Text = EmDashes.Replace(textNode.Text, ", ");
+						Debug.WriteLine($"{parser.Title}\t{parameter.ToRaw().Trim()}");
+					}
 				}
 			}
 		}
 	}
-	#endregion
 }
