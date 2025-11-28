8c7c35c7926d92462c4e62b48a85306733d09974
Robin Hood <robinhood70@live.ca>
2025-11-28 12:20:38 -0500

    Remove comment - done

--- HoodBot/Jobs/EsoCollectibles.cs
+++ HoodBot/Jobs/EsoCollectibles.cs
@@ -43,7 +43,6 @@ internal sealed class EsoCollectibles : ParsedPageJob
 		var title = TitleFactory.FromUnvalidated(this.Site, jobManager.WikiInfo.ResultsPage + "/ESO Collectibles");
 		this.SetTemporaryResultHandler(new PageResultHandler(title, false));
 
-		// TODO: Rewrite Mod Header handling to be more intelligent.
 		this.StatusWriteLine("DON'T FORGET TO UPDATE MOD HEADER!");
 	}
 	#endregion
