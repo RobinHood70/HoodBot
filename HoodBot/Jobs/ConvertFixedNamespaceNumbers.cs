0c7a5084a0bf3490802eeae25627491b41bd6c02
Robin Hood <robinhood70@live.ca>
2025-12-22 16:45:11 -0500

    Convert to full job

---
+++ HoodBot/Jobs/ConvertFixedNamespaceNumbers.cs
@@ -0,0 +1,46 @@
+﻿namespace RobinHood70.HoodBot.Jobs;
+
+using System;
+using System.Text.RegularExpressions;
+using RobinHood70.CommonCode;
+using RobinHood70.HoodBot.Uesp;
+using RobinHood70.Robby;
+using RobinHood70.Robby.Design;
+using RobinHood70.Robby.Parser;
+
+[method: JobInfo("Convert Fixed Namespace Numbers")]
+internal sealed class ConvertFixedNamespaceNumbers(JobManager jobManager) : ParsedPageJob(jobManager)
+{
+	#region Protected Override Methods
+	protected override string GetEditSummary(Page page) => "Update ID to xx";
+
+	protected override void LoadPages()
+	{
+		var title = TitleFactory.FromUnvalidated(this.Site, "Oblivion Mod:Oscuro's Oblivion Overhaul/Items");
+
+		var nsList = new UespNamespaceList(this.Site);
+		if (nsList.FromTitle(title) is not UespNamespace uespNs)
+		{
+			throw new InvalidOperationException("Title is invalid.");
+		}
+
+		var basePageName = uespNs.GetTitle(string.Empty).PageName;
+		this.Pages.GetPageLinks([title]);
+		for (var i = this.Pages.Count - 1; i >= 0; i--)
+		{
+			if (!this.Pages[i].Title.PageName.StartsWith(basePageName, StringComparison.CurrentCulture))
+			{
+				this.Pages.RemoveAt(i);
+			}
+		}
+	}
+
+	protected override void ParseText(SiteParser parser)
+	{
+		foreach (var textNode in parser.TextNodes)
+		{
+			textNode.Text = Regex.Replace(textNode.Text, @"([|(\n]\s*)01([0-9A-Fa-f]{6})", "$1xx$2", RegexOptions.None, Globals.DefaultRegexTimeout);
+		}
+	}
+	#endregion
+}
