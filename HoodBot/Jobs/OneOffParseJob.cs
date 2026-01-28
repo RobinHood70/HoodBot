bbce01bce4a56596a2a2f7afe55e48daef5463a0
Robin Hood <robinhood70@live.ca>
2026-01-28 10:18:43 -0500

    Blades Item Stats

--- HoodBot/Jobs/OneOffParseJob.cs
+++ HoodBot/Jobs/OneOffParseJob.cs
@@ -1,37 +1,117 @@
 ﻿namespace RobinHood70.HoodBot.Jobs;
 
 using System;
-using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Diagnostics;
 using RobinHood70.CommonCode;
+using RobinHood70.HoodBot.Jobs.JobModels;
 using RobinHood70.Robby;
 using RobinHood70.Robby.Design;
 using RobinHood70.Robby.Parser;
+using RobinHood70.WikiCommon.Parser;
 
 [method: JobInfo("One-Off Parse Job")]
 internal sealed class OneOffParseJob(JobManager jobManager) : ParsedPageJob(jobManager)
 {
+	#region Fields
+	private readonly TitleDictionary<IDictionary<string, string>> data = [];
+	#endregion
+
 	#region Protected Override Methods
-	protected override string GetEditSummary(Page page) => "Update ID to xx";
+	protected override string GetEditSummary(Page page) => "Update Item";
 
 	protected override void LoadPages()
 	{
-		var title = TitleFactory.FromUnvalidated(this.Site, "Oblivion Mod:Order of the Dragon/Items");
-		this.Pages.GetPageLinks([title]);
-		for (var i = this.Pages.Count - 1; i >= 0; i--)
+		var fileName = LocalConfig.BotDataSubPath("Blades - Item Stats.csv");
+		var csv = new CsvFile(fileName);
+		foreach (var row in csv.ReadRows())
 		{
-			if (!this.Pages[i].Title.PageName.StartsWith("Order of the Dragon/", StringComparison.CurrentCulture))
+			var page = row["page"];
+			if (page.StartsWith("Divine ", StringComparison.Ordinal))
 			{
-				this.Pages.RemoveAt(i);
+				page = page[7..] + " (divine)";
 			}
+
+			var fields = row.ToDictionary();
+			var newDict = TrimDictionary(fields);
+			var title = TitleFactory.FromUnvalidated(this.Site, "Blades:" + page);
+			this.data.Add(title, newDict);
 		}
+
+		this.Pages.GetTitles(this.data.ToTitleCollection(this.Site));
 	}
 
 	protected override void ParseText(SiteParser parser)
 	{
-		foreach (var textNode in parser.TextNodes)
+		if (parser.Page.IsMissing)
 		{
-			textNode.Text = Regex.Replace(textNode.Text, @"([|(\n]\s*)01([0-9A-Fa-f]{6})", "$1xx$2", RegexOptions.None, Globals.DefaultRegexTimeout);
+			Debug.WriteLine("Page does not exist: " + parser.Title);
+			return;
 		}
+
+		parser.RemoveTemplates("Stub");
+		parser.RemoveTemplates("Minimal");
+		this.InsertItemStats(parser);
+		TrimBeforeNoinclude(parser);
+	}
+	#endregion
+
+	#region Private Static Methods
+	private static Section CreateItemStatsSection(SiteParser parser, IDictionary<string, string> fields)
+	{
+		var template = parser.Factory.TemplateNodeFromParts("Blades Item Stats Table");
+		foreach (var (key, value) in fields)
+		{
+			template.Add(key, value, ParameterFormat.OnePerLine);
+		}
+
+		var itemStats = Section.FromText(parser.Factory, 3, "Item Stats", "\n");
+		itemStats.Content.Add(template);
+		itemStats.Content.AddText("\n\n");
+
+		return itemStats;
+	}
+
+	private static void TrimBeforeNoinclude(SiteParser parser)
+	{
+		if (parser.Count >= 2 &&
+			parser[^1] is IIgnoreNode &&
+			parser[^2] is ITextNode text)
+		{
+			text.Text = text.Text.TrimEnd();
+		}
+	}
+
+	private static Dictionary<string, string> TrimDictionary(IDictionary<string, string> fields)
+	{
+		fields.Remove("page");
+		var newDict = new Dictionary<string, string>(fields.Count, StringComparer.Ordinal);
+		foreach (var (key, value) in fields)
+		{
+			if (value.Trim().Length != 0)
+			{
+				newDict.Add(key, value);
+			}
+		}
+
+		return newDict;
+	}
+	#endregion
+
+	#region Private Methods
+	private void InsertItemStats(SiteParser parser)
+	{
+		var sections = parser.ToSections();
+		var index = sections.IndexOf("Tempering Recipe");
+		if (index == -1)
+		{
+			index = 1;
+		}
+
+		var fields = this.data[parser.Title];
+		var itemStats = CreateItemStatsSection(parser, fields);
+		sections.Insert(index, itemStats);
+		parser.FromSections(sections);
 	}
 	#endregion
 }
