ae7e34bd209095172215a7cd7db2b61554580592
Robin Hood <robinhood70@live.ca>
2026-03-05 16:11:19 -0500

    Blank

--- HoodBot/Jobs/OneOffParseJob.cs
+++ HoodBot/Jobs/OneOffParseJob.cs
@@ -4,47 +4,14 @@ using System;
 using RobinHood70.Robby;
 using RobinHood70.Robby.Parser;
 
-internal sealed class OneOffParseJob : ParsedPageJob
+[method: JobInfo("One-Off Parse Job")]
+internal sealed class OneOffParseJob(JobManager jobManager) : ParsedPageJob(jobManager)
 {
-	#region Constants
-	private const string BaseWiki = "en";
-	private const string OtherWiki = "fr";
-	#endregion
-
-	#region Fields
-	private readonly string category = BaseWiki switch
-	{
-		"en" => "Morrowind-Books",
-		"fr" => "Morrowind-Livres",
-		_ => throw new NotSupportedException()
-	};
-
-	private readonly string editSummary = BaseWiki switch
-	{
-		"en" => "Add interlanguage link",
-		"fr" => "Ajouter un lien interlangue",
-		_ => throw new NotSupportedException()
-	};
-	#endregion
-
-	#region Constructors
-	public OneOffParseJob(JobManager jobManager)
-		: base(jobManager)
-	{
-		if (jobManager.Site.AbstractionLayer == null)
-		{
-		}
-	}
-	#endregion
-
 	#region Protected Override Methods
-	protected override string GetEditSummary(Page page) => this.editSummary;
+	protected override string GetEditSummary(Page page) => throw new NotImplementedException();
 
-	protected override void LoadPages() => this.Pages.GetCategoryMembers(this.category, false);
+	protected override void LoadPages() => throw new NotImplementedException();
 
-	protected override void ParseText(SiteParser parser)
-	{
-		return;
-	}
+	protected override void ParseText(SiteParser parser) => throw new NotImplementedException();
 	#endregion
 }
