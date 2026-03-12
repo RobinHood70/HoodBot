777ee03efddec6e3981468bb08a99c32cfd7f6c2
Robin Hood <robinhood70@live.ca>
2026-03-12 16:17:04 -0400

    Correct naming of a few Face Flats

--- HoodBot/Jobs/OneOffMoveJob.cs
+++ HoodBot/Jobs/OneOffMoveJob.cs
@@ -1,6 +1,8 @@
 ﻿namespace RobinHood70.HoodBot.Jobs;
 
+using RobinHood70.HoodBot.Jobs.JobModels;
 using RobinHood70.Robby;
+using RobinHood70.Robby.Parser;
 
 public class OneOffMoveJob : MovePagesJob
 {
@@ -9,14 +11,29 @@ public class OneOffMoveJob : MovePagesJob
 	public OneOffMoveJob(JobManager jobManager, bool updateUserSpace)
 		: base(jobManager, updateUserSpace)
 	{
-		this.MoveAction = MoveAction.None;
-		this.FollowUpActions = FollowUpActions.CheckLinksRemaining | FollowUpActions.EmitReport | FollowUpActions.FixLinks | FollowUpActions.RetainDirectLinkText;
+		this.MoveAction = MoveAction.MoveSafely;
+		this.FollowUpActions = FollowUpActions.CheckLinksRemaining | FollowUpActions.EmitReport | FollowUpActions.FixLinks;
 	}
 	#endregion
 
 	#region Protected Override Methods
-	protected override string GetEditSummary(Page page) => "Update Lore Links (bot-assisted)";
+	protected override void CustomEdit(SiteParser parser, Title from) => parser.AddCategory("Daggerfall-NPC Face Flats", true);
 
-	protected override void PopulateMoves() => this.AddMove("Lore:Herne", "Lore:Herne (island)");
+	protected override string GetEditSummary(Page page) => "Fix name";
+
+	protected override void PopulateMoves()
+	{
+		for (var i = 163; i <= 212; i++)
+		{
+			this.AddReplacement($"File:DF-Face-Flat-{i}.png", $"File:DF-Face-Flat {i}.png", ReplacementActions.Move | ReplacementActions.Edit, null);
+		}
+
+		this.AddReplacement($"File:DF-Face-Flat-360.png", $"File:DF-Face-Flat 360.png", ReplacementActions.Move | ReplacementActions.Edit, null);
+		this.AddReplacement($"File:DF-Face-Flat-396-0.png", $"File:DF-Face-Flat 396-0.png", ReplacementActions.Move | ReplacementActions.Edit, null);
+		this.AddReplacement($"File:DF-Face-Flat-415.png", $"File:DF-Face-Flat 415.png", ReplacementActions.Move | ReplacementActions.Edit, null);
+		this.AddReplacement($"File:DF-Face-Flat-444.png", $"File:DF-Face-Flat 444.png", ReplacementActions.Move | ReplacementActions.Edit, null);
+		this.AddReplacement($"File:DF-Face-Flat-445.png", $"File:DF-Face-Flat 445.png", ReplacementActions.Move | ReplacementActions.Edit, null);
+		this.AddReplacement($"File:DF-Face-Flat-492.png", $"File:DF-Face-Flat 492.png", ReplacementActions.Move | ReplacementActions.Edit, null);
+	}
 	#endregion
 }
