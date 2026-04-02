1c2d13e745de9ba4838b489ce806482e988e9270
Robin Hood <robinhood70@live.ca>
2026-04-01 21:22:33 -0400

    Fix most remaining links

--- HoodBot/Jobs/OneOffMoveJob.cs
+++ HoodBot/Jobs/OneOffMoveJob.cs
@@ -1,8 +1,5 @@
 ﻿namespace RobinHood70.HoodBot.Jobs;
 
-using System;
-using RobinHood70.Robby;
-
 public class OneOffMoveJob : MovePagesJob
 {
 	#region Constructors
@@ -10,9 +7,8 @@ public class OneOffMoveJob : MovePagesJob
 	public OneOffMoveJob(JobManager jobManager, bool updateUserSpace)
 		: base(jobManager, updateUserSpace)
 	{
-		this.MoveAction = MoveAction.MoveOverExisting;
+		this.MoveAction = MoveAction.None;
 		this.FollowUpActions = FollowUpActions.Default; // FollowUpActions.CheckLinksRemaining | FollowUpActions.EmitReport | FollowUpActions.FixLinks;
-		this.SuppressRedirects = false;
 	}
 	#endregion
 
@@ -23,20 +19,8 @@ public class OneOffMoveJob : MovePagesJob
 	#region Protected Override Methods
 	protected override void PopulateMoves()
 	{
-		this.AddMove($"Online:Battlegrounds Merchants", $"Online:Battleground Merchants");
-		this.AddMove($"Online:Battlegrounds Supplies Merchants", $"Online:Battleground Supplies Merchants");
-	}
-
-	protected override void UpdateLinkText(ITitle page, SiteLink from, SiteLink toLink, bool addCaption)
-	{
-		base.UpdateLinkText(page, from, toLink, addCaption);
-		toLink.Text = toLink.Text?
-			.Replace("Battlegrounds Supplies", "Battleground Supplies", StringComparison.Ordinal)
-			.Replace("Battlegrounds supplies", "Battleground supplies", StringComparison.Ordinal)
-			.Replace("battlegrounds supplies", "battleground supplies", StringComparison.Ordinal)
-			.Replace("Battlegrounds Merch", "Battleground Merch", StringComparison.Ordinal)
-			.Replace("Battlegrounds merch", "Battleground merch", StringComparison.Ordinal)
-			.Replace("battlegrounds merch", "battleground merch", StringComparison.Ordinal);
+		this.AddLinkUpdate($"Online:Battlegrounds Merchants", $"Online:Battleground Merchants");
+		this.AddLinkUpdate($"Online:Battlegrounds Supplies Merchants", $"Online:Battleground Supplies Merchants");
 	}
 	#endregion
 }
