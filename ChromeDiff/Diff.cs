4b1f7ff703307faa3a6dd0fd1b2a6ee037afde15
Robin Hood <robinhood70@live.ca>
2026-04-27 11:37:33 -0400

    Updates after testing

--- ChromeDiff/Diff.cs
+++ ChromeDiff/Diff.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.ComponentModel.Composition;
+using System.Diagnostics;
 using ChromeDiff.Properties;
 using Microsoft.Win32;
 using OpenQA.Selenium;
@@ -11,16 +12,23 @@ using RobinHood70.HoodBotPlugins;
 
 [Export(typeof(IPlugin))]
 [ExportMetadata("DisplayName", "Chrome")]
-public class Diff : IDiffViewer
+public class Diff : IDiffViewer, IDisposable
 {
+	// This version does things the "right way" by filling in the edit form and clicking the diff button. This is more tolerant of changes in the wiki form, but it requires waiting for the form to load. Note, however, that it does not tolerate changes massive changes, like  Another approach would be to craft the POST request directly, like IeDiff does, which is faster but more brittle. It's unclear if this will be possible with Selenium.
+	#region Fields
+	private bool disposed;
+	private ChromeDriver? driver;
+	private int windowCount;
+	#endregion
+
 	#region Public Properties
 	public string Name => Resources.Name;
 	#endregion
 
 	#region Public Methods
-	[STAThread]
 	public void Compare(DiffContent diff)
 	{
+		ObjectDisposedException.ThrowIf(this.disposed, this);
 		ArgumentNullException.ThrowIfNull(diff);
 		if (diff.EditPath is null)
 		{
@@ -29,47 +37,122 @@ public class Diff : IDiffViewer
 
 		// Launch Chrome maximized.
 		var options = new ChromeOptions();
+		options.AddArgument(Path.Combine($"--user-data-dir={diff.UserFolder}", "Chrome"));
 		options.AddArgument("--start-maximized");
-		using var driver = new ChromeDriver(options);
+		if (diff.UserAgent is not null)
+		{
+			options.AddArgument($"--user-agent={diff.UserAgent}");
+		}
+
+		this.driver = new ChromeDriver(options);
 
 		// Navigate to the edit page.
-		driver.Navigate().GoToUrl(diff.EditPath);
-		var windowCount = driver.WindowHandles.Count;
+		this.driver.Navigate().GoToUrl(diff.EditPath);
+		this.windowCount = this.driver.WindowHandles.Count;
 
-		// Fill in the edit form.
-		var text = driver.FindElement(By.Name("wpTextbox1"));
+		// Fill in the form.
+		var text = this.WaitForElement(By.Id("wpTextbox1"));
 		text.Clear();
 		text.SendKeys(diff.Text);
-		text = driver.FindElement(By.Name("wpSummary"));
+		text = this.WaitForElement(By.Id("wpSummary"));
 		text.Clear();
 		text.SendKeys(diff.EditSummary);
 		if (diff.IsMinor)
 		{
-			text = driver.FindElement(By.Name("wpMinoredit"));
-			text.Click();
+			try
+			{
+				text = this.driver.FindElement(By.Id("wpMinoredit"));
+				text.Click();
+			}
+			catch (NoSuchElementException)
+			{
+				// This is possible if the bot is logged out currently, in which case the minor edit checkbox is not available. Ignore it and continue.
+			}
 		}
 
+		text = driver.FindElement(By.Id("wpDiff"));
 		text.Submit();
-
-		var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-		wait.Until(driver2 => driver2.WindowHandles.Count < windowCount);
-		driver.Quit();
 	}
 
 	public bool Validate()
 	{
 		if (!OperatingSystem.IsWindows())
 		{
-			return true; // Blindly trust it's installed on non-Windows platforms.
+			return true; // Blindly trust Chrome is installed on non-Windows platforms.
 		}
 
 		var registryPath = @"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe";
-		using var key = Registry.LocalMachine.OpenSubKey(registryPath) ?? Registry.CurrentUser.OpenSubKey(registryPath);
-		return key is not null;
+		using var machineKey = Registry.LocalMachine.OpenSubKey(registryPath);
+		if (machineKey is not null)
+		{
+			return true;
+		}
+
+		using var userKey = Registry.CurrentUser.OpenSubKey(registryPath);
+		return userKey is not null;
 	}
 
 	public void Wait()
 	{
+		if (this.driver is null)
+		{
+			return;
+		}
+
+		var pageWait = new WebDriverWait(this.driver, TimeSpan.FromDays(1));
+		try
+		{
+			pageWait.Until(drv => drv.WindowHandles.Count < this.windowCount);
+			this.driver.Quit();
+		}
+		catch (Exception)
+		{
+		}
+	}
+	#endregion
+
+	#region Protected Virtual Methods
+	protected virtual void Dispose(bool disposing)
+	{
+		if (!this.disposed)
+		{
+			if (disposing)
+			{
+				this.driver?.Dispose();
+			}
+
+			this.driver = null;
+			this.disposed = true;
+		}
+	}
+
+	public void Dispose()
+	{
+		// Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+		this.Dispose(disposing: true);
+		GC.SuppressFinalize(this);
+	}
+	#endregion
+
+	#region Private Methods
+	private IWebElement WaitForElement(By condition)
+	{
+		Debug.Assert(condition is not null);
+		Debug.Assert(this.driver is not null);
+
+		var wait = new WebDriverWait(this.driver, TimeSpan.FromSeconds(10));
+		return wait.Until(drv =>
+		{
+			try
+			{
+				var el = drv.FindElement(condition);
+				return el.Displayed ? el : null;
+			}
+			catch (NoSuchElementException)
+			{
+				return null;
+			}
+		});
 	}
 	#endregion
 }
--- ChromeDiff/Properties/Resources.Designer.cs
+++ ChromeDiff/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace RobinHood70.ChromeDiff.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ChromeDiff.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("RobinHood70.ChromeDiff.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
