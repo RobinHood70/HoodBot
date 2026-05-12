8144e06fd776321d6bf5961941ef2f1a6b4b937f
Robin Hood <robinhood70@live.ca>
2026-05-11 20:18:22 -0400

    Daggerfall Interactive Preview

--- HoodBot/Jobs/OneOffParseJob.cs
+++ HoodBot/Jobs/OneOffParseJob.cs
@@ -1,50 +1,39 @@
 ﻿namespace RobinHood70.HoodBot.Jobs;
 
-using System.Diagnostics;
+using System;
+using System.Collections.Generic;
 using RobinHood70.Robby;
 using RobinHood70.Robby.Parser;
 using RobinHood70.WikiCommon;
-using RobinHood70.WikiCommon.Parser;
 
 [method: JobInfo("One-Off Parse Job")]
 internal sealed class OneOffParseJob(JobManager jobManager) : ParsedPageJob(jobManager)
 {
 	#region Protected Override Methods
-	protected override string GetEditSummary(Page page) => "Update license";
+	protected override string GetEditSummary(Page page) => "Update Daggerfall Preview text";
 
 	protected override void LoadPages()
 	{
-		this.Pages.GetCategoryMembers("Starfield-Icons");
-		this.Pages.GetCategoryMembers("Starfield-Icons-Planet Traits");
-		this.Pages.GetCategoryMembers("Starfield-Logo Images");
-		this.Pages.GetCategoryMembers("Starfield-Skill Images");
+		var nses = new List<int>();
+		foreach (var ns in this.Site.Namespaces)
+		{
+			if (ns.Id < 300 && !ns.IsTalkSpace)
+			{
+				nses.Add(ns.Id);
+			}
+		}
+
+		this.Pages.GetSearchResults("Daggerfall Preview", WhatToSearch.Text, nses);
+		this.Pages.GetSearchResults("Daggerfall_Preview", WhatToSearch.Text, nses);
+		this.Pages.Remove("UESPWiki:Administrator Noticeboard/Archives/Images");
 	}
 
 	protected override void ParseText(SiteParser parser)
 	{
-		foreach (var template in parser.TemplateNodes)
+		foreach (var textNode in parser.TextNodes)
 		{
-			var title = template.GetTitle(this.Site);
-			if (title.Namespace != MediaWikiNamespaces.Template)
-			{
-				continue;
-			}
-
-			if (title.PageNameEquals("Sfwimage"))
-			{
-				template.SetTitle("sfimage");
-			}
-			else if (
-				title.PageNameEquals("cc-by-sa-2.5") ||
-				title.PageNameEquals("Cc-by-sa-3.0") ||
-				title.PageNameEquals("Public Domain") ||
-				title.PageNameEquals("sfimage"))
-			{
-			}
-			else
-			{
-				Debug.WriteLine(parser.Title + ": No license found");
-			}
+			textNode.Text = textNode.Text.Replace("Daggerfall Preview", "Daggerfall Interactive Preview", StringComparison.Ordinal);
+			textNode.Text = textNode.Text.Replace("Daggerfall_Preview", "Daggerfall_Interactive_Preview", StringComparison.Ordinal);
 		}
 	}
 	#endregion
