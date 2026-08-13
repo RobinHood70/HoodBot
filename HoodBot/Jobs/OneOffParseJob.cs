3b6454da46a0a661976a817bf9a291f2bdd0c56d
Robin Hood <robinhood70@live.ca>
2026-08-13 16:06:50 -0400

    Standardize mod parameters in PC3

--- HoodBot/Jobs/OneOffParseJob.cs
+++ HoodBot/Jobs/OneOffParseJob.cs
@@ -1,39 +1,30 @@
 ﻿namespace RobinHood70.HoodBot.Jobs;
 
-using System;
-using System.Collections.Generic;
+using RobinHood70.CommonCode;
+using RobinHood70.HoodBot.Uesp;
 using RobinHood70.Robby;
 using RobinHood70.Robby.Parser;
-using RobinHood70.WikiCommon;
+using RobinHood70.WikiCommon.Parser;
 
 [method: JobInfo("One-Off Parse Job")]
 internal sealed class OneOffParseJob(JobManager jobManager) : ParsedPageJob(jobManager)
 {
 	#region Protected Override Methods
-	protected override string GetEditSummary(Page page) => "Update Daggerfall Preview text";
+	protected override string GetEditSummary(Page page) => "Standardize mod parameter";
 
-	protected override void LoadPages()
-	{
-		var nses = new List<int>();
-		foreach (var ns in this.Site.Namespaces)
-		{
-			if (ns.Id < 300 && !ns.IsTalkSpace)
-			{
-				nses.Add(ns.Id);
-			}
-		}
-
-		this.Pages.GetSearchResults("Daggerfall Preview", WhatToSearch.Text, nses);
-		this.Pages.GetSearchResults("Daggerfall_Preview", WhatToSearch.Text, nses);
-		this.Pages.Remove("UESPWiki:Administrator Noticeboard/Archives/Images");
-	}
+	protected override void LoadPages() => this.Pages.GetNamespace(UespNamespaces.ProjectTamriel, Filter.Any, "Cyrodiil/");
 
 	protected override void ParseText(SiteParser parser)
 	{
-		foreach (var textNode in parser.TextNodes)
+		foreach (var template in parser.TemplateNodes)
 		{
-			textNode.Text = textNode.Text.Replace("Daggerfall Preview", "Daggerfall Interactive Preview", StringComparison.Ordinal);
-			textNode.Text = textNode.Text.Replace("Daggerfall_Preview", "Daggerfall_Interactive_Preview", StringComparison.Ordinal);
+			foreach (var parameter in template.Parameters)
+			{
+				if (parameter.GetName().OrdinalICEquals("mod"))
+				{
+					parameter.SetValue("[[Project Tamriel:Cyrodiil/Main Page|Project Cyrodiil]]", ParameterFormat.Copy);
+				}
+			}
 		}
 	}
 	#endregion
