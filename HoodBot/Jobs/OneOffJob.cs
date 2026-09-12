8e1852383456a6db7ea887ed0c091462448bb00b
Robert Morley <robinhood70@live.ca>
2026-09-11 22:31:00 -0400

    Lore image furnishings for AKB

--- HoodBot/Jobs/OneOffJob.cs
+++ HoodBot/Jobs/OneOffJob.cs
@@ -3,39 +3,28 @@
 using RobinHood70.CommonCode;
 using RobinHood70.HoodBot.Uesp;
 using RobinHood70.Robby;
-using RobinHood70.Robby.Parser;
-using RobinHood70.WikiCommon;
+using RobinHood70.Robby.Design;
 
 [method: JobInfo("One-Off Job")]
 internal sealed class OneOffJob(JobManager jobManager) : WikiJob(jobManager, JobType.ReadOnly)
 {
 	protected override void Main()
 	{
-		var pages = new PageCollection(this.Site);
-		pages.GetBacklinks("Template:NPC Summary", BacklinksTypes.EmbeddedIn, true, Filter.Exclude, UespNamespaces.ProjectTamriel);
+		var pages = PageCollection.Unlimited(this.Site, PageModules.Backlinks, true);
+		pages.GetCategoryMembers("Online-Furnishing Images", true);
 		pages.Sort();
+		this.StatusWriteLine(pages.Count.ToStringInvariant() + " pages");
 		foreach (var page in pages)
 		{
-			if (!page.Title.PageName.StartsWith("Skyrim/", System.StringComparison.InvariantCultureIgnoreCase))
+			foreach (var link in page.Backlinks)
 			{
-				continue;
-			}
-
-			var parser = new SiteParser(page);
-			var found = false;
-			foreach (var template in parser.TemplateNodes)
-			{
-				if (template.GetTitle(this.Site).PageNameEquals("MW Racial Note"))
+				if (link.Key.Namespace == UespNamespaces.Lore)
 				{
-					found = true;
-					break;
+					this.WriteLine($"* [[:{page.Title}]] => [[{link.Key}]]");
 				}
 			}
-
-			if (!found)
-			{
-				this.WriteLine($"* Missing MW Racial Note template on [[{page.Title}]]");
-			}
 		}
+
+		this.Results?.Save();
 	}
 }
